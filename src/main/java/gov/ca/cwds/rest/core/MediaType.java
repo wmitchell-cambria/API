8db1ea38634e7402c487a53f4d22403ed4f5dd76
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-15 12:09:53 -0700

    Created a version 1 media type and integrated into the ApplicationResource

--- src/main/java/gov/ca/cwds/rest/core/MediaType.java
+++ src/main/java/gov/ca/cwds/rest/core/MediaType.java
@@ -5,6 +5,6 @@ public class MediaType {
     /**
      * A {@code String} constant representing {@value #APPLICATION_JSON} media type.
      */
-    public final static String APPLICATION_JSON_APPLICATION_V1 = "application/json;x.api.application+v1";
+    public final static String APPLICATION_JSON_V1 = "application/gov.ca.cwds.rest-v1+json";
 }
 
--- src/main/java/gov/ca/cwds/rest/resources/ApplicationResource.java
+++ src/main/java/gov/ca/cwds/rest/resources/ApplicationResource.java
@@ -1,15 +1,18 @@
 package gov.ca.cwds.rest.resources;
 
 import static gov.ca.cwds.rest.core.Api.APPLICATION;
+import static gov.ca.cwds.rest.core.MediaType.APPLICATION_JSON_V1;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 
 @Api(APPLICATION)
 @Path(APPLICATION)
+@Produces(APPLICATION_JSON_V1)
 public class ApplicationResource {
 	
 	private String applicationName;
