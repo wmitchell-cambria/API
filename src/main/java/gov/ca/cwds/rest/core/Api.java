98ac9ef1ea3c1417efa5b488832305351209bf6c
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-15 11:14:58 -0700

    Abstracted out hard coded resource name in ApplicationResource

---
+++ src/main/java/gov/ca/cwds/rest/core/Api.java
@@ -0,0 +1,10 @@
+package gov.ca.cwds.rest.core;
+
+public class Api {
+	
+    /**
+     * A {@code String} constant representing {@value #APPLICATION} resource.
+     */
+    public final static String APPLICATION = "application";
+    
+}
