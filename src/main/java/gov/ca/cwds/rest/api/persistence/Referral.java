98e00cc9816559669c8a0d94317d66a85f78b201
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-18 14:52:44 -0700

    https://www.pivotaltracker.com/story/show/126321037 - fixed a couple ReferralService tests and complete the 2 TODO's on tests that were in ReferralResourceImpl test

--- src/main/java/gov/ca/cwds/rest/api/persistence/Referral.java
+++ src/main/java/gov/ca/cwds/rest/api/persistence/Referral.java
@@ -55,5 +55,52 @@ public class Referral {
 	public Date getReceivedDate() {
 		return receivedDate;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((receivedDate == null) ? 0 : receivedDate.hashCode());
+		result = prime * result
+				+ ((referralName == null) ? 0 : referralName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Referral other = (Referral) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (receivedDate == null) {
+			if (other.receivedDate != null)
+				return false;
+		} else if (!receivedDate.equals(other.receivedDate))
+			return false;
+		if (referralName == null) {
+			if (other.referralName != null)
+				return false;
+		} else if (!referralName.equals(other.referralName))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
