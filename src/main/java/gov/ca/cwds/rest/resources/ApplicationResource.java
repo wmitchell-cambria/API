30e23706ba6434e5b974704a0e581c020fd4265d
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-18 11:40:06 -0700

    moved a couple annotations below javadoc in ApplicationResource

--- src/main/java/gov/ca/cwds/rest/resources/ApplicationResource.java
+++ src/main/java/gov/ca/cwds/rest/resources/ApplicationResource.java
@@ -4,19 +4,21 @@ import static gov.ca.cwds.rest.core.Api.RESOURCE_APPLICATION;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 
-@Api(RESOURCE_APPLICATION)
-@Path(RESOURCE_APPLICATION)
-@Produces(gov.ca.cwds.rest.core.Api.MEDIA_TYPE_JSON_V1)
 /**
  * A resource providing a basic information about the CWDS API
  * 
  * @author CWDS API Team
  */
+@Api(value = RESOURCE_APPLICATION )
+@Path(RESOURCE_APPLICATION)
+@Produces(gov.ca.cwds.rest.core.Api.MEDIA_TYPE_JSON_V1)
+@Consumes(gov.ca.cwds.rest.core.Api.MEDIA_TYPE_JSON_V1)
 public interface ApplicationResource {
 	
 	@ApiOperation("Get the application name")
