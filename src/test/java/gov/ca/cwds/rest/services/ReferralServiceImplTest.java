96a4ec0f17443cb363b28cfcc674572fe58dc5dd
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-22 14:55:03 -0700

    removed unused import

--- src/test/java/gov/ca/cwds/rest/services/ReferralServiceImplTest.java
+++ src/test/java/gov/ca/cwds/rest/services/ReferralServiceImplTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import gov.ca.cwds.rest.api.domain.ReferralSummary;
 import gov.ca.cwds.rest.api.persistence.Referral;
 
 import java.util.Date;
