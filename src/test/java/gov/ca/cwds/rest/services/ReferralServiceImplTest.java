8d789a5ed36111ef2d97ab329157193ae398d579
Richard Bach <richard.bach@tabordasolutions.com>
2016-07-22 10:23:58 -0700

    cobertura to create html along with xml now.  Also implemented a couple tests that cobertura let me know were missing

--- src/test/java/gov/ca/cwds/rest/services/ReferralServiceImplTest.java
+++ src/test/java/gov/ca/cwds/rest/services/ReferralServiceImplTest.java
@@ -1,12 +1,16 @@
 package gov.ca.cwds.rest.services;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import gov.ca.cwds.rest.api.domain.ReferralSummary;
+import gov.ca.cwds.rest.api.persistence.Referral;
 
 import java.util.Date;
-
-import gov.ca.cwds.rest.api.persistence.Referral;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,11 +20,26 @@ public class ReferralServiceImplTest {
 	
 	private CrudsService<Referral> crudsService;
 	
+	private Referral FOUND_REFERRAL = new Referral("found", "name", new Date());
+	
 	@SuppressWarnings("unchecked")
 	@Before
 	public void setup() {
 		crudsService = mock(CrudsService.class);
+		
+		when(crudsService.find(FOUND_REFERRAL.getId())).thenReturn(FOUND_REFERRAL);
+		when(crudsService.find("notfound")).thenReturn(null);
 		referralService = new ReferralServiceImpl(crudsService);
+	}
+	
+	@Test 
+	public void findReferralSummaryReturnsCorrectObjectOnSuccess() {
+		assertThat(referralService.findReferralSummary(FOUND_REFERRAL.getId()).getId(), is(equalTo(FOUND_REFERRAL.getId())));
+	}
+
+	@Test 
+	public void findReferralSummaryReturnsNullOnNotFound() {
+		assertThat(referralService.findReferralSummary("notfound"), is(equalTo(null)));
 	}
 
 	@Test
