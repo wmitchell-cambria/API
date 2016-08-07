7a7b59c733c55a343d1d8651a3c8114c2f273164
Richard Bach <richard.bach@tabordasolutions.com>
2016-08-06 20:41:38 -0700

    https://www.pivotaltracker.com/story/show/126651345 - added a DateValidator

---
+++ src/main/java/gov/ca/cwds/rest/validation/Date.java
@@ -0,0 +1,36 @@
+package gov.ca.cwds.rest.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * Checks to see that the value is in a valid date format.
+ */
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = DateValidator.class)
+public @interface Date {
+    String message() default "must be in the format of {format}";
+
+    Class<?>[] groups() default {};
+    
+    Class<? extends Payload>[] payload() default {};
+    
+    /**
+     * The set of valid values.
+     */
+    String format() default "MM/dd/yyyy";
+}
