0a6395206184667339f0cdf5789fb7065a0b853a
Richard Bach <richard.bach@tabordasolutions.com>
2016-08-19 15:03:31 -0700

    https://www.pivotaltracker.com/story/show/126651345 - moved current daos to a new package ( legacy )

--- src/main/java/gov/ca/cwds/rest/jdbi/legacy/AllegationDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/legacy/AllegationDao.java
@@ -1,8 +1,9 @@
-package gov.ca.cwds.rest.jdbi;
+package gov.ca.cwds.rest.jdbi.legacy;
 
 import org.hibernate.SessionFactory;
 
 import gov.ca.cwds.rest.api.persistence.legacy.Allegation;
+import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
 
 public class AllegationDao extends CrudsDaoImpl<Allegation> {
 
--- src/main/java/gov/ca/cwds/rest/jdbi/legacy/ReferralDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/legacy/ReferralDao.java
@@ -1,9 +1,10 @@
-package gov.ca.cwds.rest.jdbi;
+package gov.ca.cwds.rest.jdbi.legacy;
 
 import org.hibernate.SessionFactory;
 
 import gov.ca.cwds.rest.api.persistence.legacy.Referral;
 import gov.ca.cwds.rest.api.persistence.legacy.StaffPerson;
+import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
 
 public class ReferralDao extends CrudsDaoImpl<Referral> {
 
