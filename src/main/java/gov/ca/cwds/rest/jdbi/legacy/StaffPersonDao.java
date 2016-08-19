0c51c84a1e21a67c4a2557a656b4f214c583b0cf
Richard Bach <richard.bach@tabordasolutions.com>
2016-08-19 15:03:31 -0700

    https://www.pivotaltracker.com/story/show/126651345 - moved current daos to a new package ( legacy )

--- src/main/java/gov/ca/cwds/rest/jdbi/legacy/StaffPersonDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/legacy/StaffPersonDao.java
@@ -1,8 +1,9 @@
-package gov.ca.cwds.rest.jdbi;
+package gov.ca.cwds.rest.jdbi.legacy;
 
 import org.hibernate.SessionFactory;
 
 import gov.ca.cwds.rest.api.persistence.legacy.StaffPerson;
+import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
 
 public class StaffPersonDao extends CrudsDaoImpl<StaffPerson> {
 
