bacaa72f3e8d2205c148ca1b16cbe73ac00dbd7f
Richard Bach <richard.bach@tabordasolutions.com>
2016-08-19 22:13:37 -0700

    https://www.pivotaltracker.com/story/show/126651345 - integrate new services and resources into the app

--- src/main/java/gov/ca/cwds/rest/jdbi/legacy/ReferralClientDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/legacy/ReferralClientDao.java
@@ -2,10 +2,10 @@ package gov.ca.cwds.rest.jdbi.legacy;
 
 import org.hibernate.SessionFactory;
 
-import gov.ca.cwds.rest.api.persistence.legacy.StaffPerson;
+import gov.ca.cwds.rest.api.persistence.legacy.ReferralClient;
 import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
 
-public class ReferralClientDao extends CrudsDaoImpl<StaffPerson> {
+public class ReferralClientDao extends CrudsDaoImpl<ReferralClient> {
 
 	public ReferralClientDao(SessionFactory sessionFactory) {
 		super(sessionFactory);
