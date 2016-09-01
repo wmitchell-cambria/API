193bde7919e43c4fd10f4dfb68310098c6a03188
AVandana-OSI <CWS-NS2@ip-192-168-10-21.us-west-2.compute.internal>
2016-09-01 09:35:57 -0700

    https://www.pivotaltracker.com/story/show/128091051 - Integration Testing for AllegationDao

--- src/main/java/gov/ca/cwds/rest/api/persistence/legacy/Allegation.java
+++ src/main/java/gov/ca/cwds/rest/api/persistence/legacy/Allegation.java
@@ -253,4 +253,219 @@ public class Allegation extends PersistentObject {
 	public Integer getPlacementFacilityType() {
 		return placementFacilityType;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setAbuseEndDate(Date abuseEndDate) {
+		this.abuseEndDate = abuseEndDate;
+	}
+
+	public void setAbuseStartDate(Date abuseStartDate) {
+		this.abuseStartDate = abuseStartDate;
+	}
+
+	public void setAbuseFrequency(Integer abuseFrequency) {
+		this.abuseFrequency = abuseFrequency;
+	}
+
+	public void setAbuseFrequencyPeriodCode(String abuseFrequencyPeriodCode) {
+		this.abuseFrequencyPeriodCode = abuseFrequencyPeriodCode;
+	}
+
+	public void setAbuseLocationDescription(String abuseLocationDescription) {
+		this.abuseLocationDescription = abuseLocationDescription;
+	}
+
+	public void setAllegationDispositionType(Integer allegationDispositionType) {
+		this.allegationDispositionType = allegationDispositionType;
+	}
+
+	public void setAllegationType(Integer allegationType) {
+		this.allegationType = allegationType;
+	}
+
+	public void setDispositionDescription(String dispositionDescription) {
+		this.dispositionDescription = dispositionDescription;
+	}
+
+	public void setDispositionDate(Date dispositionDate) {
+		this.dispositionDate = dispositionDate;
+	}
+
+	public void setInjuryHarmDetailIndicator(String injuryHarmDetailIndicator) {
+		this.injuryHarmDetailIndicator = injuryHarmDetailIndicator;
+	}
+
+	public void setNonProtectingParentCode(String nonProtectingParentCode) {
+		this.nonProtectingParentCode = nonProtectingParentCode;
+	}
+
+	public void setStaffPersonAddedIndicator(String staffPersonAddedIndicator) {
+		this.staffPersonAddedIndicator = staffPersonAddedIndicator;
+	}
+
+	public void setFkClientT(String fkClientT) {
+		this.fkClientT = fkClientT;
+	}
+
+	public void setFkClient0(String fkClient0) {
+		this.fkClient0 = fkClient0;
+	}
+
+	public void setFkReferralT(String fkReferralT) {
+		this.fkReferralT = fkReferralT;
+	}
+
+	public void setCountySpecificCode(String countySpecificCode) {
+		this.countySpecificCode = countySpecificCode;
+	}
+
+	public void setZippyCrestedIndicator(String zippyCrestedIndicator) {
+		this.zippyCrestedIndicator = zippyCrestedIndicator;
+	}
+
+	public void setPlacementFacilityType(Integer placementFacilityType) {
+		this.placementFacilityType = placementFacilityType;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((abuseEndDate == null) ? 0 : abuseEndDate.hashCode());
+		result = prime * result + ((abuseFrequency == null) ? 0 : abuseFrequency.hashCode());
+		result = prime * result + ((abuseFrequencyPeriodCode == null) ? 0 : abuseFrequencyPeriodCode.hashCode());
+		result = prime * result + ((abuseLocationDescription == null) ? 0 : abuseLocationDescription.hashCode());
+		result = prime * result + ((abuseStartDate == null) ? 0 : abuseStartDate.hashCode());
+		result = prime * result + ((allegationDispositionType == null) ? 0 : allegationDispositionType.hashCode());
+		result = prime * result + ((allegationType == null) ? 0 : allegationType.hashCode());
+		result = prime * result + ((countySpecificCode == null) ? 0 : countySpecificCode.hashCode());
+		result = prime * result + ((dispositionDate == null) ? 0 : dispositionDate.hashCode());
+		result = prime * result + ((dispositionDescription == null) ? 0 : dispositionDescription.hashCode());
+		result = prime * result + ((fkClient0 == null) ? 0 : fkClient0.hashCode());
+		result = prime * result + ((fkClientT == null) ? 0 : fkClientT.hashCode());
+		result = prime * result + ((fkReferralT == null) ? 0 : fkReferralT.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((injuryHarmDetailIndicator == null) ? 0 : injuryHarmDetailIndicator.hashCode());
+		result = prime * result + ((nonProtectingParentCode == null) ? 0 : nonProtectingParentCode.hashCode());
+		result = prime * result + ((placementFacilityType == null) ? 0 : placementFacilityType.hashCode());
+		result = prime * result + ((staffPersonAddedIndicator == null) ? 0 : staffPersonAddedIndicator.hashCode());
+		result = prime * result + ((zippyCrestedIndicator == null) ? 0 : zippyCrestedIndicator.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allegation other = (Allegation) obj;
+		if (abuseEndDate == null) {
+			if (other.abuseEndDate != null)
+				return false;
+		} else if (!abuseEndDate.equals(other.abuseEndDate))
+			return false;
+		if (abuseFrequency == null) {
+			if (other.abuseFrequency != null)
+				return false;
+		} else if (!abuseFrequency.equals(other.abuseFrequency))
+			return false;
+		if (abuseFrequencyPeriodCode == null) {
+			if (other.abuseFrequencyPeriodCode != null)
+				return false;
+		} else if (!abuseFrequencyPeriodCode.equals(other.abuseFrequencyPeriodCode))
+			return false;
+		if (abuseLocationDescription == null) {
+			if (other.abuseLocationDescription != null)
+				return false;
+		} else if (!abuseLocationDescription.trim().equals(other.abuseLocationDescription.trim()))
+			return false;
+		if (abuseStartDate == null) {
+			if (other.abuseStartDate != null)
+				return false;
+		} else if (!abuseStartDate.equals(other.abuseStartDate))
+			return false;
+		if (allegationDispositionType == null) {
+			if (other.allegationDispositionType != null)
+				return false;
+		} else if (!allegationDispositionType.equals(other.allegationDispositionType))
+			return false;
+		if (allegationType == null) {
+			if (other.allegationType != null)
+				return false;
+		} else if (!allegationType.equals(other.allegationType))
+			return false;
+		if (countySpecificCode == null) {
+			if (other.countySpecificCode != null)
+				return false;
+		} else if (!countySpecificCode.equals(other.countySpecificCode))
+			return false;
+		if (dispositionDate == null) {
+			if (other.dispositionDate != null)
+				return false;
+		} else if (!dispositionDate.equals(other.dispositionDate))
+			return false;
+		if (dispositionDescription == null) {
+			if (other.dispositionDescription != null)
+				return false;
+		} else if (!dispositionDescription.trim().equals(other.dispositionDescription.trim()))
+			return false;
+		if (fkClient0 == null) {
+			if (other.fkClient0 != null)
+				return false;
+		} else if (!fkClient0.equals(other.fkClient0))
+			return false;
+		if (fkClientT == null) {
+			if (other.fkClientT != null)
+				return false;
+		} else if (!fkClientT.equals(other.fkClientT))
+			return false;
+		if (fkReferralT == null) {
+			if (other.fkReferralT != null)
+				return false;
+		} else if (!fkReferralT.equals(other.fkReferralT))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (injuryHarmDetailIndicator == null) {
+			if (other.injuryHarmDetailIndicator != null)
+				return false;
+		} else if (!injuryHarmDetailIndicator.equals(other.injuryHarmDetailIndicator))
+			return false;
+		if (nonProtectingParentCode == null) {
+			if (other.nonProtectingParentCode != null)
+				return false;
+		} else if (!nonProtectingParentCode.equals(other.nonProtectingParentCode))
+			return false;
+		if (placementFacilityType == null) {
+			if (other.placementFacilityType != null)
+				return false;
+		} else if (!placementFacilityType.equals(other.placementFacilityType))
+			return false;
+		if (staffPersonAddedIndicator == null) {
+			if (other.staffPersonAddedIndicator != null)
+				return false;
+		} else if (!staffPersonAddedIndicator.equals(other.staffPersonAddedIndicator))
+			return false;
+		if (zippyCrestedIndicator == null) {
+			if (other.zippyCrestedIndicator != null)
+				return false;
+		} else if (!zippyCrestedIndicator.equals(other.zippyCrestedIndicator))
+			return false;
+		return true;
+	}
 }
---
+++ src/test/java/gov/ca/cwds/rest/jdbi/legacy/AllegationDaoTest.java
@@ -0,0 +1,120 @@
+package gov.ca.cwds.rest.jdbi.legacy;
+
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import gov.ca.cwds.rest.api.persistence.legacy.Allegation;
+
+public class AllegationDaoTest {
+	private SessionFactory sessionFactory;
+	private AllegationDao allegationDao;
+
+	@Before
+	public void setup() {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		sessionFactory.getCurrentSession().beginTransaction();
+		allegationDao = new AllegationDao(sessionFactory);
+	}
+
+	@After
+	public void tearndown() {
+		sessionFactory.close();
+	}
+
+
+	@Test
+	public void testFind() {
+		String id = "Aaeae9r0F4";
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F4", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F4", null, "8mu1E710F4", "19", 
+				false, null);
+		Allegation expected = new Allegation (allegation, "0F4");
+		Allegation found = allegationDao.find(id);
+		assert found.equals(expected);
+	}
+
+	@Test
+	public void testCreate() {
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F6", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F6", null, "8mu1E710F4", "19", 
+				false, null);
+		Allegation expected = new Allegation (allegation, "0F6");
+		Allegation create = allegationDao.create(expected);
+		assert expected.equals(create);
+		//allegationDao.delete("Aaeae9r0F6");
+		
+	}
+	
+	@Test
+	public void testCreateExistingEntityExpection() {
+		boolean gotException = false;
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F4", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F4", null, "8mu1E710F4", "19", 
+				false, null);
+		
+		Allegation expected = new Allegation (allegation, "0F4");
+		try{
+		Allegation create = allegationDao.create(expected);
+		} catch(EntityExistsException entityExistsExp){
+			gotException = true;
+		}
+		assert gotException;
+	}
+	
+	@Test
+	public void testDelete() {
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F6", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F6", null, "8mu1E710F4", "19", 
+				false, null);
+		Allegation expected = new Allegation (allegation, "0F6");
+		Allegation create = allegationDao.create(expected);
+		assert expected.equals(create);
+	}
+	
+	@Test
+	public void testUpdate() {
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F4", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F4", null, "8mu1E710F4", "20", 
+				false, null);
+		Allegation expected = new Allegation (allegation, "0F4");
+		Allegation update = allegationDao.update(expected);
+		assert expected.equals(update);
+	}
+
+	@Test
+	public void testUpdateEntityNotFoundException() {
+		boolean gotException = false;
+		gov.ca.cwds.rest.api.domain.Allegation allegation = new gov.ca.cwds.rest.api.domain.Allegation(
+				"Aaeae9r0F7", null, 2, "M", "  ", null,
+				0, 2180, "  ", null, false, 
+				"N", false, "AHooKwN0F7", null, "8mu1E710F7", "20", 
+				false, null);
+		
+		Allegation expected = new Allegation (allegation, "0F7");
+		try{
+		Allegation update = allegationDao.update(expected) ;
+		} catch(EntityNotFoundException entityNotFoundException){
+			gotException = true;
+		}
+		assert gotException;
+	}
+
+}
