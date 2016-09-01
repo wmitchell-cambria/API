d5f25ae7373d05b9a26a183b7d359846c70145de
AVandana-OSI <CWS-NS2@ip-192-168-10-21.us-west-2.compute.internal>
2016-09-01 09:35:57 -0700

    https://www.pivotaltracker.com/story/show/128091051 - Integration Testing for AllegationDao

--- src/main/java/gov/ca/cwds/rest/jdbi/legacy/AllegationDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/legacy/AllegationDao.java
@@ -1,14 +1,17 @@
 package gov.ca.cwds.rest.jdbi.legacy;
 
 import org.hibernate.SessionFactory;
-
 import gov.ca.cwds.rest.api.persistence.legacy.Allegation;
 import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
 
 public class AllegationDao extends CrudsDaoImpl<Allegation> {
 
+	SessionFactory sessionFactory;
+
 	public AllegationDao(SessionFactory sessionFactory) {
 		super(sessionFactory);
+		this.sessionFactory = sessionFactory;
 	}
+
 
 }
