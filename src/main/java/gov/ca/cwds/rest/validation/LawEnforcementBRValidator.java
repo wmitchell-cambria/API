3f287b3d0b2ed3b9582c0c4000303c12d3f917cf
AVandana-OSI <CWS-NS2@Tabordas-MacBook-Pro.local>
2016-09-20 14:12:45 -0700

    https://www.pivotaltracker.com/story/show/128243881 LawEnforcementID Validator Change

--- src/main/java/gov/ca/cwds/rest/validation/LawEnforcementBRValidator.java
+++ src/main/java/gov/ca/cwds/rest/validation/LawEnforcementBRValidator.java
@@ -13,39 +13,48 @@ public class LawEnforcementBRValidator implements ConstraintValidator<LawEnforce
 
 	private String lawEnforcementId;
 	private String badgeNumber;
+	private String employerName;
 
 	@Override
 	public void initialize(final LawEnforcementBR constraintAnnotation) {
 		this.lawEnforcementId = constraintAnnotation.fieldName();
 		this.badgeNumber = constraintAnnotation.dependentFieldName();
+		this.employerName = constraintAnnotation.SecondFieldName();
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public boolean isValid(final Object value, final ConstraintValidatorContext context) {
 
 		try {
 			final String lawEnforcementIdValue = BeanUtils.getProperty(value, lawEnforcementId);
 			final String badgeNumberValue = BeanUtils.getProperty(value, badgeNumber);
+			final String employerNameValue = BeanUtils.getProperty(value, employerName);
 
 			if (StringUtils.isNotBlank(lawEnforcementIdValue) && badgeNumberValue == null) {
 				context.disableDefaultConstraintViolation();
 				context.buildConstraintViolationWithTemplate("can not be null when lawEnfrocementId is entered")
-						.addNode(badgeNumber).addConstraintViolation();
+						.addPropertyNode(badgeNumber).addConstraintViolation();
 				return false;
 			}
 
 			if (StringUtils.isNotBlank(badgeNumberValue) && lawEnforcementIdValue == null) {
 				context.disableDefaultConstraintViolation();
 				context.buildConstraintViolationWithTemplate("is null then badgeNumber should not be entered")
-						.addNode(lawEnforcementId).addConstraintViolation();
+						.addPropertyNode(lawEnforcementId).addConstraintViolation();
 				return false;
 			}
+
+			if (StringUtils.isNotBlank(lawEnforcementIdValue) && StringUtils.isNotBlank(employerNameValue)) {
+				context.disableDefaultConstraintViolation();
+				context.buildConstraintViolationWithTemplate("and lawEnforcmentId both can not be entered at a time")
+						.addPropertyNode(employerName).addConstraintViolation();
+				return false;
+			}
+
 		} catch (Exception e) {
 			LOGGER.info("Exception in BusinessRule Validator {} ", e.toString());
 			return false;
 		}
-
 		return true;
 	}
 }
