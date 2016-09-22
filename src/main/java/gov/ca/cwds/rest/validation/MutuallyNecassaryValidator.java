e6f9f742ae8166d91ee214992b9852441613b368
Richard Bach <richard.bach@tabordasolutions.com>
2016-09-22 15:37:10 -0700

    #128243881 - update a MutuallyNecassary validation

---
+++ src/main/java/gov/ca/cwds/rest/validation/MutuallyNecassaryValidator.java
@@ -0,0 +1,66 @@
+package gov.ca.cwds.rest.validation;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Validator which confirms all the given properties on a bean have been set.
+ *
+ * @author CWDS API Team
+ */
+public class MutuallyNecassaryValidator implements ConstraintValidator<MutuallyNecassary, Object> {
+
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(MutuallyNecassaryValidator.class);
+
+	private String[] properties;
+	private boolean required;
+
+	@Override
+	public void initialize(MutuallyNecassary constraintAnnotation) {
+		this.properties = constraintAnnotation.properties();
+		this.required = constraintAnnotation.required();
+	}
+
+	@Override
+	public boolean isValid(final Object bean, ConstraintValidatorContext context) {
+		boolean valid = true;
+		int countNotEmpty = 0;
+		for( String property : properties ) {
+			String value = readBeanValue(bean, property);
+			if( StringUtils.isNotBlank(value)) {
+				countNotEmpty++;
+			}
+		}
+		
+		if( required && countNotEmpty < properties.length ) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(MessageFormat.format("{0} all must have their values set", Arrays.toString(properties)))
+					.addPropertyNode("Properties").addConstraintViolation();
+			valid = false;
+		}
+		if( countNotEmpty > 0 && countNotEmpty < properties.length ) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(MessageFormat.format("{0} are mutually necassary but not all values are set", Arrays.toString(properties)))
+					.addPropertyNode("Properties").addConstraintViolation();	
+			valid = false;
+		}
+		return valid;
+	}
+	
+	private String readBeanValue(Object bean, String property) {
+		try {
+			return BeanUtils.getProperty(bean, property);
+		} catch (Throwable e) {
+			throw new ValidationException(MessageFormat.format("Unable to read '{0}' from bean:{1}", property, bean), e);
+		}
+	}
+}
