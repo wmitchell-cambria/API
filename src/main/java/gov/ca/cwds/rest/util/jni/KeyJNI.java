f04357d789c56105b682b04105fa55735b405cee
Richard Bach <richard.bach@tabordasolutions.com>
2016-10-07 17:15:32 -0700

    updates to javadoc in KeyJNI.java

--- src/main/java/gov/ca/cwds/rest/util/jni/KeyJNI.java
+++ src/main/java/gov/ca/cwds/rest/util/jni/KeyJNI.java
@@ -1,5 +1,6 @@
 package gov.ca.cwds.rest.util.jni;
 
+import gov.ca.cwds.rest.api.persistence.legacy.StaffPerson;
 
 //
 // http://docs.oracle.com/javase/8/docs/technotes/guides/jni/spec/functions.html
@@ -14,96 +15,92 @@ package gov.ca.cwds.rest.util.jni;
 // JAVA EXECUTE: OS X:
 // java -Djava.library.path=.:/usr/lib/ gov.ca.cwds.rest.util.jni.KeyJNI
 
-
 /**
  * Demonstrates calling native CWDS key generation library via JNI.
  * 
- * <p>
  * <h2>Steps to build and run</h2>
- * </p>
  * 
- * @author David R. Smith, Taborda Solutions, Inc., 2016
+ * @author CWDS API Team
  */
 public class KeyJNI {
+	static {
+		System.out.println("user.dir=" + System.getProperty("user.dir"));
+		System.out.println("java.library.path=" + System.getProperty("java.library.path"));
 
-  static {
-    System.out.println("user.dir=" + System.getProperty("user.dir"));
-    System.out.println("java.library.path=" + System.getProperty("java.library.path"));
+		// keyJNI.dll (Windows), libKeyJNI.dylib (Mac), libKeyJNI.so (Unix)
+		// Load native library at runtime.
+		System.loadLibrary("KeyJNI");
+	}
 
-    // keyJNI.dll (Windows), libKeyJNI.dylib (Mac), libKeyJNI.so (Unix)
-    // Load native library at runtime.
-    System.loadLibrary("KeyJNI");
-  }
+	/**
+	 * Track memory to hunt memory leaks and overall memory consumption.
+	 * 
+	 * @return free memory in MB
+	 */
+	public static long calcMemory() {
+		Runtime runtime = Runtime.getRuntime();
+		long maxMemory = runtime.maxMemory();
+		long allocatedMemory = runtime.totalMemory();
+		long freeMemory = runtime.freeMemory();
+		return (freeMemory + (maxMemory - allocatedMemory)) / 1024L;
+	}
 
-  /**
-   * Track memory to hunt memory leaks and overall memory consumption.
-   * 
-   * @return free memory in MB
-   */
-  public static long calcMemory() {
-    Runtime runtime = Runtime.getRuntime();
-    long maxMemory = runtime.maxMemory();
-    long allocatedMemory = runtime.totalMemory();
-    long freeMemory = runtime.freeMemory();
-    return (freeMemory + (maxMemory - allocatedMemory)) / 1024L;
-  }
+	/**
+	 * Utility struct class stores details of CWDS key decomposition.
+	 */
+	public static final class KeyDetail {
+		public String key;
+		public String staffId;
+		public String UITimestamp;
+		public String PTimestamp;
+	}
 
-  /**
-   * Utility struct class stores details of CWDS key decomposition.
-   */
-  public static final class KeyDetail {
-    public String key;
-    public String staffId;
-    public String UITimestamp;
-    public String PTimestamp;
-  }
+	/**
+	 * Generates a unique key for use within CWDS CMS based on the given staff person id.
+	 * 
+	 * @param staffId	the {@link StaffPerson}
+	 * @return	The generated key
+	 */
+	public native String generateKey(String staffId);
 
-  /**
-   * 
-   * @param staffId
-   * @return
-   */
-  public native String generateKey(String staffId);
+	/**
+	 * Decomposes a generated key.
+	 * 
+	 * @param key	the key
+	 * @param kd	the key detail
+	 */
+	public native void decomposeKey(String key, KeyDetail kd);
 
-  /**
-   * 
-   * @param key
-   * @param kd
-   */
-  public native void decomposeKey(String key, KeyDetail kd);
+	// Test Driver
+	public static void main(String[] args) {
+		KeyJNI inst = new KeyJNI();
 
-  // Test Driver
-  public static void main(String[] args) {
-    KeyJNI inst = new KeyJNI();
+		// ===================
+		// GENERATE KEY:
+		// ===================
 
-    // ===================
-    // GENERATE KEY:
-    // ===================
+		{ // Generate a key from a staff id.
+			System.out.println("Java: Call JNI generateKey ... ");
+			final String key = inst.generateKey("0X5");
+			System.out.println("Java: key=" + key);
+		}
 
-    { // Generate a key from a staff id.
-      System.out.println("Java: Call JNI generateKey ... ");
-      final String key = inst.generateKey("0X5");
-      System.out.println("Java: key=" + key);
-    }
+		// ===================
+		// DECOMPOSE KEY:
+		// ===================
 
-    // ===================
-    // DECOMPOSE KEY:
-    // ===================
+		final long startingMemory = calcMemory();
 
-    final long startingMemory = calcMemory();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("current memory = " + calcMemory());
+			System.out.println("Java: Call JNI decomposeKey ... " + i);
 
-    for (int i = 0; i < 10; i++) {
-      System.out.println("current memory = " + calcMemory());
-      System.out.println("Java: Call JNI decomposeKey ... " + i);
+			KeyDetail kd = new KeyDetail();
+			inst.decomposeKey("S8eScDM0X5", kd);
+			System.out.println("Java: key=" + kd.key + ", staffId=" + kd.staffId + ", UITimestamp=" + kd.UITimestamp
+					+ ", PTimestamp=" + kd.PTimestamp);
+		}
 
-      KeyDetail kd = new KeyDetail();
-      inst.decomposeKey("S8eScDM0X5", kd);
-      System.out.println("Java: key=" + kd.key + ", staffId=" + kd.staffId + ", UITimestamp="
-          + kd.UITimestamp + ", PTimestamp=" + kd.PTimestamp);
-    }
-
-    System.out.println("used memory = " + (calcMemory() - startingMemory));
-  }
+		System.out.println("used memory = " + (calcMemory() - startingMemory));
+	}
 }
-
-
