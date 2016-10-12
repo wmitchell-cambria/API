393e6ebd89035d5029bff14bc030c3d9f3fea642
jimlebeau <James.LeBeau@osi.ca.gov>
2016-10-12 07:53:33 -0700

    add serialize and deserialize test

--- src/test/java/gov/ca/cwds/rest/api/domain/PersonTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/PersonTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import gov.ca.cwds.rest.core.Api;
 import gov.ca.cwds.rest.resources.PersonResource;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.testing.junit.ResourceTestRule;
@@ -17,14 +18,16 @@ import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
 public class PersonTest {
+
+  private static final String ROOT_RESOURCE = "/" + Api.RESOURCE_PEOPLE + "/";
+
   private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
-  private static final PersonResource mockedPersonResource = mock(PersonResource.class);
+  private static final PersonResource mockedPeopleResource = mock(PersonResource.class);
 
   @ClassRule
   public static final ResourceTestRule resources =
-      ResourceTestRule.builder().addResource(mockedPersonResource).build();
-
+      ResourceTestRule.builder().addResource(mockedPeopleResource).build();
 
   /*
    * Serialization and deserialization
@@ -51,11 +54,55 @@ public class PersonTest {
   }
 
   @Test
-  public void equalsHashCodeWork() {
+  public void equalsHashCodeWork() throws Exception {
     EqualsVerifier.forClass(Person.class).suppress(Warning.NONFINAL_FIELDS).verify();
+    Address address = new Address("123 Main", "Sacramento", "CA", 95757);
+    Person expected = new Person("firstname", "lastname", "F", "20010901", "123456789", address);
+    Person serialized =
+        MAPPER.readValue(fixture("fixtures/domain/person/valid/valid.json"), Person.class);
+    assertThat(serialized, is(expected));
   }
 
-  /*
-   * validation tests
-   */
+  // @Test
+  // public void successfulWithValid() throws Exception {
+  // Address address = new Address("123 Main", "Sacramento", "CA", 95757);
+  // Person toCreate = MAPPER.readValue(fixture("fixtures/domain/people/valid/valid.json"),
+  // Person.class);
+  // assertThat(resources.client().target(ROOT_RESOURCE).request().accept(Api.Version.JSON_VERSION_1.getMediaType())
+  // .post(Entity.entity(toCreate, Api.MEDIA_TYPE_JSON_V1)).getStatus(), is(equalTo(204)));
+  //
+  //
+  // }
+
+  @Test
+  public void failedTest() {}
+
+  // @Test
+  // public void successWhenValidDateFormat () throws Exception {
+  // Person serialized = MAPPER.readValue(fixture("fixtures/domain/people/valid/valid.json"),
+  // Person.class);
+  // Response response =
+  // resources.client().target("/people/").request()
+  // .accept(MediaType.APPLICATION_JSON)
+  // .post(Entity.entity(serialized, MediaType.APPLICATION_JSON));
+  // assertThat(response.getStatus(), is(equalTo(204)));
+  // assertThat(response.readEntity(String.class).indexOf("invalid date format"),
+  // is(greaterThanOrEqualTo(0)));
+  //
+  // }
+  //
+  // public void failureWhenInvalidDateFormat() throws Exception {
+  // Person serialized = MAPPER.readValue(fixture("fixtures/domain/people/invalid.dateFormat"),
+  // Person.class);
+  // Response response =
+  // resources.client().target("/people/").request()
+  // .accept(MediaType.APPLICATION_JSON)
+  // .post(Entity.entity(serialized, MediaType.APPLICATION_JSON));
+  // assertThat()
+  //
+  //
+  // }
+
+
+  // TODO : RDB implement tests
 }
