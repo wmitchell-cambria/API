96404496500f192c8a7e5a9267505791a13cbffc
Richard Bach <richard.bach@tabordasolutions.com>
2016-10-12 13:59:13 -0700

    #131670987 - stubbed out PersonService

---
+++ src/test/java/gov/ca/cwds/rest/services/PersonServiceTest.java
@@ -0,0 +1,81 @@
+package gov.ca.cwds.rest.services;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.hamcrest.junit.ExpectedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import gov.ca.cwds.rest.api.domain.Address;
+import gov.ca.cwds.rest.api.domain.Person;
+
+public class PersonServiceTest {
+	private PersonService personService;
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Before
+	public void setup() throws Exception {
+		personService = new PersonService();
+	}
+
+	/*
+	 * find tests
+	 */
+	@Test 
+	public void findReturnsCorrectAddressWhenFoundWhenFound() throws Exception {
+		Address address = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
+		Person expected = new Person("Bart", "Simpson", "M", "04/01/1990", "1234556789", address);
+		
+		Person found = personService.find("found");
+		
+		assertThat(found, is(expected));
+	}
+
+	@Test 
+	public void findReturnsNullWhenNotFound() throws Exception {
+		Person found = personService.find("notfound");
+		
+		assertThat(found, is(nullValue()));
+	}
+
+	/*
+	 * create tests
+	 */
+	@Test
+	public void createReturnsIdOnCreate() throws Exception {
+		Address address = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
+		Person toCreate = new Person("Bart", "Simpson", "M", "04/01/1990", "1234556789", address);
+		String id = (String)personService.create(toCreate);
+		
+		assertThat(id, is(notNullValue()));
+	}
+	
+	/*
+	 * delete tests
+	 */
+	@Test
+	public void deleteThrowsNotImplementedException() throws Exception {
+		thrown.expect(NotImplementedException.class);
+		personService.delete("someid");
+	}
+	
+	/*
+	 * update tests
+	 */
+	@Test
+	public void updateThrowsNotImplementedException() throws Exception {
+		thrown.expect(NotImplementedException.class);
+		
+		Address address = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
+		Person toUpdate = new Person("Bart", "Simpson", "M", "04/01/1990", "1234556789", address);
+		personService.update(toUpdate);
+	}
+
+}
