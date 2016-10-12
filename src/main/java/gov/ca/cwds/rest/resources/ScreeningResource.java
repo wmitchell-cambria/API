bebc2d4e679ecc97a13c44f8c2403232fed6ce58
Mariam Ghori <mariam.ghori@osi.ca.gov>
2016-10-12 12:00:07 -0700

    #131670707 RESTful API for Screenings (POST, GET)

--- src/main/java/gov/ca/cwds/rest/resources/ScreeningResource.java
+++ src/main/java/gov/ca/cwds/rest/resources/ScreeningResource.java
@@ -1,8 +1,11 @@
 package gov.ca.cwds.rest.resources;
 
-import static gov.ca.cwds.rest.core.Api.RESOURCE_SCREENING;
+import static gov.ca.cwds.rest.core.Api.RESOURCE_SCREENINGS;
+
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,7 +14,6 @@ import javax.ws.rs.core.UriInfo;
 import org.apache.http.HttpStatus;
 
 import gov.ca.cwds.rest.api.domain.ApiResponse;
-import gov.ca.cwds.rest.api.domain.Person;
 import gov.ca.cwds.rest.api.domain.Screening;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,10 +26,11 @@ import io.swagger.annotations.ResponseHeader;
  * 
  * @author CWDS API Team
  */
-@Api(value = RESOURCE_SCREENING, tags = RESOURCE_SCREENING, produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
-@Path(value = RESOURCE_SCREENING)
+@Api(value = RESOURCE_SCREENINGS, tags = RESOURCE_SCREENINGS, produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+@Path(value = RESOURCE_SCREENINGS)
 public class ScreeningResource implements CrudsResource<Screening> {
 	
+	@SuppressWarnings("unused")
 	private CrudsResourceImpl<Screening> crudsResourceImpl;
 
 	/**
@@ -46,8 +49,15 @@ public class ScreeningResource implements CrudsResource<Screening> {
 	@Override
 	@ApiOperation(value = "Find Screening by id", response = Screening.class)
 	public Response get(String id, String acceptHeader) {
-		return Response.status(Response.Status.OK).entity(new ApiResponse<Screening>("1",
-			new Screening((long)101,"reference", "12/22/2001", "county", "11/22/2010", "home", "method","name","time","decision","11/22/2010","narrative"))).build();
+		if (!Arrays.asList(acceptHeader.split(",")).contains(MediaType.APPLICATION_JSON)) {
+			return Response.status(Response.Status.NOT_ACCEPTABLE).entity(null).build();
+		}
+		if ("1".equals(id)) {
+			return Response.status(Response.Status.OK)
+					.entity(new Screening((long)101,"reference", "12/22/2001", "county", "11/22/2010", "home", "method","name","time","decision","11/22/2010","narrative")).build();
+		} else {
+			return Response.status(Response.Status.NOT_FOUND).entity(null).build();
+		}
 	}
 
 	/*
@@ -59,7 +69,7 @@ public class ScreeningResource implements CrudsResource<Screening> {
 	@Override
 	@ApiOperation(hidden = true, value = "Delete Screening - not currently implemented", code = HttpStatus.SC_OK, response = Object.class)
 	public Response delete(String id, String acceptHeader) {
-		return Response.status(Response.Status.FORBIDDEN).entity(null).build();
+		return Response.status(Response.Status.NOT_IMPLEMENTED).entity(null).build();
 	}
 
 	/*
@@ -73,16 +83,22 @@ public class ScreeningResource implements CrudsResource<Screening> {
 	@Override
 	@ApiResponses(value = { @io.swagger.annotations.ApiResponse(code = 400, message = "Unable to process JSON"),
 			@io.swagger.annotations.ApiResponse(code = 406, message = "Accept Header not supported"),
-			@io.swagger.annotations.ApiResponse(code = 422, message = "Unable to process entity") })
+			@io.swagger.annotations.ApiResponse(code = 422, message = "Unable to validate Screening") })
 	@ApiOperation(value = "Create Screening", code = HttpStatus.SC_CREATED, responseHeaders = @ResponseHeader(name = "Location", description = "Link to the newly created Screening", response = Screening.class))
-	public ApiResponse<Screening> create(Screening domainObject, String acceptHeader, UriInfo uriInfo,
+	public ApiResponse<Screening> create(@Valid Screening domainObject, String acceptHeader, UriInfo uriInfo,
 			HttpServletResponse response) {
-		response.setStatus(HttpServletResponse.SC_CREATED);
-	    try {
-	        response.flushBuffer();
-	    }catch(Exception e){}
-		return new ApiResponse<Screening>("1", 
-				new Screening((long)101,"reference", "12/22/2001", "county", "11/22/2010", "home", "method","name","time","decision","11/22/2010","narrative"));
+		int status = HttpServletResponse.SC_CREATED;
+		if (!Arrays.asList(acceptHeader.split(",")).contains(MediaType.APPLICATION_JSON)) {
+			status = HttpServletResponse.SC_NOT_ACCEPTABLE;
+		}
+		response.addHeader("Location", "some_value");
+		response.setStatus(status);
+		try {
+			response.flushBuffer();
+		} catch (Exception e) {
+		}
+		Screening screening = new Screening((long)101,"reference", "12/22/2001", "county", "11/22/2010", "home", "method","name","time","decision","11/22/2010","narrative");
+		return new ApiResponse<Screening>("1", screening);
 	}
 
 	/*
