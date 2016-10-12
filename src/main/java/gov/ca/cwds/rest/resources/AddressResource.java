96e15720a88338856ef5352e8d4d6bc50ccae4a2
Richard Bach <richard.bach@tabordasolutions.com>
2016-10-12 13:17:26 -0700

    #131670987 - AddressResource is not delegating down to a CrudsResourceImpl

--- src/main/java/gov/ca/cwds/rest/resources/AddressResource.java
+++ src/main/java/gov/ca/cwds/rest/resources/AddressResource.java
@@ -2,10 +2,7 @@ package gov.ca.cwds.rest.resources;
 
 import static gov.ca.cwds.rest.core.Api.RESOURCE_ADDRESSES;
 
-import java.util.Arrays;
-
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,21 +11,22 @@ import javax.ws.rs.core.UriInfo;
 import org.apache.http.HttpStatus;
 
 import gov.ca.cwds.rest.api.domain.Address;
-import gov.ca.cwds.rest.api.domain.ApiResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ResponseHeader;
 
 /**
  * A resource providing a RESTful interface for {@link Address}. It delegates
- * CRUD functions to {@link CrudsResourceImpl}
+ * CRUD functions to {@link CrudsResourceImpl}. It decorates the
+ * {@link CrudsResourceImpl} not in functionality but with @see <a href=
+ * "https://github.com/swagger-api/swagger-core/wiki/Annotations-1.5.X">Swagger
+ * Annotations</a>
  * 
  * @author CWDS API Team
  */
 @Api(value = RESOURCE_ADDRESSES, tags = RESOURCE_ADDRESSES, produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
 @Path(value = RESOURCE_ADDRESSES)
 public class AddressResource implements CrudsResource<Address> {
-	@SuppressWarnings("unused")
 	private CrudsResource<Address> crudsResource;
 
 	/**
@@ -50,16 +48,7 @@ public class AddressResource implements CrudsResource<Address> {
 	@Override
 	@ApiOperation(value = "Find Address by id", response = Address.class)
 	public Response get(String id, String acceptHeader) {
-		// return crudsResource.get(id, acceptHeader);
-		if (!Arrays.asList(acceptHeader.split(",")).contains(MediaType.APPLICATION_JSON)) {
-			return Response.status(Response.Status.NOT_ACCEPTABLE).entity(null).build();
-		}
-		if ("1".equals(id)) {
-			return Response.status(Response.Status.OK)
-					.entity(new Address("742 Evergreen Terrace", "Springfield", "WA", 98700)).build();
-		} else {
-			return Response.status(Response.Status.NOT_FOUND).entity(null).build();
-		}
+		return crudsResource.get(id, acceptHeader);
 	}
 
 	/*
@@ -83,20 +72,9 @@ public class AddressResource implements CrudsResource<Address> {
 	 */
 	@Override
 	@ApiOperation(value = "Create Address", code = HttpStatus.SC_CREATED, responseHeaders = @ResponseHeader(name = "Location", description = "Link to the newly created Address", response = Address.class))
-	public gov.ca.cwds.rest.api.domain.ApiResponse<Address> create(@Valid Address domainObject, String acceptHeader, UriInfo uriInfo, HttpServletResponse response) {
-		int status = HttpServletResponse.SC_CREATED;
-		if (!Arrays.asList(acceptHeader.split(",")).contains(MediaType.APPLICATION_JSON)) {
-			status = HttpServletResponse.SC_NOT_ACCEPTABLE;
-		}
-		response.addHeader("Location", "some_value");
-		response.setStatus(status);
-		try {
-			response.flushBuffer();
-		} catch (Exception e) {
-		}
-		Address address = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
-		return new ApiResponse<Address>("1", address);
-		//return crudsResource.create(domainObject, acceptHeader, uriInfo, response);
+	public gov.ca.cwds.rest.api.domain.ApiResponse<Address> create(Address domainObject, String acceptHeader,
+			UriInfo uriInfo, HttpServletResponse response) {
+		return crudsResource.create(domainObject, acceptHeader, uriInfo, response);
 	}
 
 	/*
@@ -109,8 +87,6 @@ public class AddressResource implements CrudsResource<Address> {
 	@Override
 	@ApiOperation(hidden = true, value = "Update Address", code = HttpStatus.SC_NO_CONTENT, response = Object.class)
 	public Response update(Address domainObject, String acceptHeader) {
-		// return
-		// Response.status(Response.Status.FORBIDDEN).entity(null).build();
 		return Response.status(Response.Status.NOT_IMPLEMENTED).entity(null).build();
 	}
 }
