62fb0e213c6977c3ce81f8855bf4f020303cb6e9
mysorepr <prasad.mysore@osi.ca.gov>
2016-10-14 13:24:07 -0700

    #131670707 - Screening changes

--- src/test/java/gov/ca/cwds/rest/api/domain/ScreeningTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/ScreeningTest.java
@@ -19,54 +19,58 @@ import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
 public class ScreeningTest {
-    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
-    
-    private static final ScreeningResource mockedScreeningResource =
-    	      mock(ScreeningResource.class);
-    
-    @ClassRule
-    public static final ResourceTestRule resources = ResourceTestRule.builder()
-        .addResource(mockedScreeningResource).build();
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+  private static final ScreeningResource mockedScreeningResource = mock(ScreeningResource.class);
+
+  @ClassRule
+  public static final ResourceTestRule resources =
+      ResourceTestRule.builder().addResource(mockedScreeningResource).build();
 
 
-    /*
-     * Serialization and deserialization
-     */
-//    @Test
-//    public void serializesToJSON() throws Exception {
-//    	Address address = new Address("10 main st", "Sacramento", "CA", 95814);
-//    	ArrayList<Integer> involvedPersonIds = new ArrayList<Integer>();
-//		involvedPersonIds.add(1);
-//		involvedPersonIds.add(2);
-//		involvedPersonIds.add(3);
-//    	String expected = MAPPER.writeValueAsString( new Screening((long)2,"X5HNJK", "2016-10-13T01:07", "amador", "2016-10-13", "Relative's Home", "email","first screening","immediate","accept_for_investigation","2016-10-05T01:01","first narrative", address,involvedPersonIds));
-//
-//        String serialized = MAPPER.writeValueAsString(
-//        		MAPPER.readValue(fixture("fixtures/domain/screening/valid/valid.json"), Screening.class));
-//
-//        assertThat(serialized, is(expected));
-//    }
-//    
-//    @Test
-//    public void deserializesFromJSON() throws Exception {
-//    	Address address = new Address("10 main st", "Sacramento", "CA", 95814);
-//    	ArrayList<Integer> involvedPersonIds = new ArrayList<Integer>();
-//		involvedPersonIds.add(1);
-//		involvedPersonIds.add(2);
-//		involvedPersonIds.add(3);
-//        Screening expected = new Screening((long)2,"X5HNJK", "2016-10-13T01:07", "amador", "2016-10-13", "Relative's Home", "email","first screening","immediate","accept_for_investigation","2016-10-05T01:01","first narrative", address,involvedPersonIds); 
-//        Screening serialized = MAPPER.readValue(fixture("fixtures/domain/screening/valid/valid.json"), Screening.class);
-//        assertThat(serialized, is(expected));
-//    }
-//    
-//    @Test
-//    public void equalsHashCodeWork() {
-//        EqualsVerifier.forClass(Screening.class)
-//                .suppress(Warning.NONFINAL_FIELDS)
-//                .verify();
-//    }
-    
-    /*
-     * validation tests
-     */
+  /*
+   * Serialization and deserialization
+   */
+  @Test
+  public void serializesToJSON() throws Exception {
+    Address address = new Address("10 main st", "Sacramento", "CA", 95814);
+    ArrayList<Integer> involvedPersonIds = new ArrayList<Integer>();
+    involvedPersonIds.add(1);
+    involvedPersonIds.add(2);
+    involvedPersonIds.add(3);
+    String expected = MAPPER.writeValueAsString(
+        new Screening((long) 2, "X5HNJK", "2016-10-13T01:07", "amador", "2016-10-13",
+            "Relative's Home", "email", "first screening", "immediate", "accept_for_investigation",
+            "2016-10-05T01:01", "first narrative", address, involvedPersonIds));
+
+    String serialized = MAPPER.writeValueAsString(
+        MAPPER.readValue(fixture("fixtures/domain/screening/valid/valid.json"), Screening.class));
+
+    assertThat(serialized, is(expected));
+  }
+
+  @Test
+  public void deserializesFromJSON() throws Exception {
+    Address address = new Address("10 main st", "Sacramento", "CA", 95814);
+    ArrayList<Integer> involvedPersonIds = new ArrayList<Integer>();
+    involvedPersonIds.add(1);
+    involvedPersonIds.add(2);
+    involvedPersonIds.add(3);
+    Screening expected =
+        new Screening((long) 2, "X5HNJK", "2016-10-13T01:07", "amador", "2016-10-13",
+            "Relative's Home", "email", "first screening", "immediate", "accept_for_investigation",
+            "2016-10-05T01:01", "first narrative", address, involvedPersonIds);
+    Screening serialized =
+        MAPPER.readValue(fixture("fixtures/domain/screening/valid/valid.json"), Screening.class);
+    assertThat(serialized, is(expected));
+  }
+
+  @Test
+  public void equalsHashCodeWork() {
+    EqualsVerifier.forClass(Screening.class).suppress(Warning.NONFINAL_FIELDS).verify();
+  }
+
+  /*
+   * validation tests
+   */
 }
