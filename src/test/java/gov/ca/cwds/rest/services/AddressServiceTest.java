6c7c37b53749b1a35870496f83cae68155732ac7
Richard Bach <richard.bach@tabordasolutions.com>
2016-10-19 14:25:23 -0700

    fixed address service tests

--- src/test/java/gov/ca/cwds/rest/services/AddressServiceTest.java
+++ src/test/java/gov/ca/cwds/rest/services/AddressServiceTest.java
@@ -1,79 +1,157 @@
 package gov.ca.cwds.rest.services;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+import org.apache.commons.lang3.NotImplementedException;
 import org.hamcrest.junit.ExpectedException;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import gov.ca.cwds.rest.api.domain.Address;
+import gov.ca.cwds.rest.api.domain.PostedAddress;
+import gov.ca.cwds.rest.jdbi.ns.AddressDao;
 
 public class AddressServiceTest {
   private AddressService addressService;
 
+  private AddressDao addressDao = mock(AddressDao.class);
+
   @Rule
   public ExpectedException thrown = ExpectedException.none();
 
-  // @Before
-  // public void setup() throws Exception {
-  // addressService = new AddressService();
-  // }
-  //
-  // /*
-  // * find tests
-  // */
-  // @Test
-  // public void findReturnsCorrectAddressWhenFoundWhenFound() throws Exception {
-  // Address expected = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
-  //
-  // Address found = (Address) addressService.find("found");
-  //
-  // assertThat(found, is(expected));
-  // }
-  //
-  // @Test
-  // public void findReturnsNullWhenNotFound() throws Exception {
-  // Address found = (Address) addressService.find("notfound");
-  //
-  // assertThat(found, is(nullValue()));
-  // }
-  //
-  // /*
-  // * create tests
-  // */
-  // @Test
-  // public void createReturnsPrimaryKeyResponseOnCreate() throws Exception {
-  // Address tocreate = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
-  // Response response = addressService.create(tocreate);
-  //
-  // assertThat(response, is(notNullValue()));
-  // assertThat(response.getClass(), is(PostedAddress.class));
-  // }
-  //
-  // /*
-  // * delete tests
-  // */
-  // @Test
-  // public void deleteThrowsNotImplementedException() throws Exception {
-  // thrown.expect(NotImplementedException.class);
-  // addressService.delete("someid");
-  // }
-  //
-  // /*
-  // * update tests
-  // */
-  // @Test
-  // public void updateThrowsNotImplementedException() throws Exception {
-  // thrown.expect(NotImplementedException.class);
-  //
-  // addressService.update(1L, new Address("street", "city", "state", 95555));
-  // }
+  @Before
+  public void setup() throws Exception {
+    addressService = new AddressService(addressDao);
+    reset(addressDao);
+  }
 
   /*
-   * Oddness with cobertura cause the declaring class line to be not counted as run. This has to do
-   * with bridge functions. To get our coverage numbers the "test" below calls the bridge functions
-   * directly.
+   * find tests
    */
-  // @Test
-  // public void callBridgeFunctions() throws Exception {
-  // Method create = AddressService.class.getMethod("create", DomainObject.class);
-  // create.invoke(addressService, new Address("street", "city", "state", 95555));
-  // Assert.assertTrue(true);
-  // }
+  @Test
+  public void findReturnsCorrectAddressWhenFoundWhenFound() throws Exception {
+    when(addressDao.find(new Long(1))).thenReturn(new gov.ca.cwds.rest.api.persistence.ns.Address(
+        1L, "742 Evergreen Terrace", "Springfield", "WA", new Integer(98700)));
 
+    Address expected = new Address("742 Evergreen Terrace", "Springfield", "WA", 98700);
+
+    Address found = addressService.find(new Long(1));
+
+    assertThat(found, is(expected));
+  }
+
+  @Test
+  public void findReturnsNullWhenNotFound() throws Exception {
+    when(addressDao.find(new Long(-1))).thenReturn(null);
+    Address found = addressService.find(new Long(-1));
+
+    assertThat(found, is(nullValue()));
+  }
+
+  @Test
+  public void findThrowsAssertionError() throws Exception {
+    // TODO : thrown.expect not working on AssertionError???? WHY???
+    // thrown.expect(AssertionError.class);
+    try {
+      addressService.find("nonLong");
+      Assert.fail("Expected AssertionError");
+    } catch (AssertionError e) {
+    }
+  }
+
+  /*
+   * create tests
+   */
+  @Test
+  public void createReturnsPostedAddressOnCreate() throws Exception {
+    gov.ca.cwds.rest.api.persistence.ns.Address toCreate =
+        new gov.ca.cwds.rest.api.persistence.ns.Address(1L, "742 Evergreen Terrace", "Springfield",
+            "WA", new Integer(98700));
+    Address request = new Address(toCreate);
+
+    when(addressDao.create(any(gov.ca.cwds.rest.api.persistence.ns.Address.class)))
+        .thenReturn(toCreate);
+
+    PostedAddress postAddress = addressService.create(request);
+    assertThat(postAddress, is(notNullValue()));
+    assertThat(postAddress.getClass(), is(PostedAddress.class));
+  }
+
+  @Test
+  public void createReturnsReturnsCorrectIdWithAddress() throws Exception {
+    gov.ca.cwds.rest.api.persistence.ns.Address toCreate =
+        new gov.ca.cwds.rest.api.persistence.ns.Address(1L, "742 Evergreen Terrace", "Springfield",
+            "WA", new Integer(98700));
+    Address request = new Address(toCreate);
+
+    when(addressDao.create(any(gov.ca.cwds.rest.api.persistence.ns.Address.class)))
+        .thenReturn(toCreate);
+
+    PostedAddress expected =
+        new PostedAddress(1, "742 Evergreen Terrace", "Springfield", "WA", new Integer(98700));
+    PostedAddress returned = addressService.create(request);
+
+    assertThat(returned, is(expected));
+  }
+
+  @Test
+  public void createThrowsAssertionError() throws Exception {
+    // TODO : thrown.expect not working on AssertionError???? WHY???
+    // thrown.expect(AssertionError.class);
+    try {
+      addressService.find("wrong");
+      Assert.fail("Expected AssertionError");
+    } catch (AssertionError e) {
+    }
+  }
+
+  /*
+   * delete tests
+   */
+  @Test
+  public void deleteThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    addressService.delete(new Long(1));
+  }
+
+  @Test
+  public void deleteThrowsAssertionError() throws Exception {
+    // TODO : thrown.expect not working on AssertionError???? WHY???
+    // thrown.expect(AssertionError.class);
+    try {
+      addressService.delete("nonLong");
+      Assert.fail("Expected AssertionError");
+    } catch (AssertionError e) {
+    }
+  }
+
+  /*
+   * update tests
+   */
+  @Test
+  public void updateThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+
+    addressService.update(1L, new Address("street", "city", "state", 95555));
+  }
+
+  @Test
+  public void updateThrowsAssertionError() throws Exception {
+    // TODO : thrown.expect not working on AssertionError???? WHY???
+    // thrown.expect(AssertionError.class);
+    try {
+      addressService.find("wrong");
+      Assert.fail("Expected AssertionError");
+    } catch (AssertionError e) {
+    }
+  }
 }
