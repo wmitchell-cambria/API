c6c420a25ae3ba9ce7d8507b5539ac86f6e590fb
David R. Smith <drs.code.monkey@gmail.com>
2016-11-01 12:49:43 -0700

    JNI testing

---
+++ run_LZWCompressionTest.sh
@@ -0,0 +1,2 @@
+java -Djava.library.path=.:/usr/local/lib/ -cp .:/var/lib/jenkins/workspace/API/build/classes/main:/var/lib/jenkins/workspace/API/build/classes/test:/var/lib/jenkins/workspace/API/build/resources/test:/var/lib/jenkins/test_lib/junit-4.12.jar:/var/lib/jenkins/test_lib/hamcrest-core-1.3.jar:/var/lib/jenkins/test_lib/* org.junit.runner.JUnitCore gov.ca.cwds.rest.util.jni.LZWCompressionTest
+
