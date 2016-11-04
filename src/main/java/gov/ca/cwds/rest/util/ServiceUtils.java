3cfe2d1e8f891a25d8e392aa15be8fabe928431b
jimlebeau <James.LeBeau@osi.ca.gov>
2016-11-03 17:44:39 -0700

    add Services Utility class

---
+++ src/main/java/gov/ca/cwds/rest/util/ServiceUtils.java
@@ -0,0 +1,28 @@
+package gov.ca.cwds.rest.util;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import gov.ca.cwds.rest.services.ServiceException;
+
+public class ServiceUtils {
+  public static Map<String, String> extractKeyValuePairs(Serializable nameValuePairsSerializable) {
+    Map<String, String> keyValuePairs = new HashMap<String, String>();
+    if (!(nameValuePairsSerializable instanceof String)) {
+      throw new ServiceException("Unable to read nameValuePairs as string");
+    }
+    try {
+      String primaryKeyString = (String) nameValuePairsSerializable;
+
+      for (String keyValueString : primaryKeyString.split(",")) {
+        String[] keyValuePair = keyValueString.split("=");
+        keyValuePairs.put(keyValuePair[0].trim(), keyValuePair[1].trim());
+      }
+      return keyValuePairs;
+    } catch (Throwable e) {
+      throw new ServiceException("Problem parsing name value pairs", e);
+    }
+  }
+
+}
