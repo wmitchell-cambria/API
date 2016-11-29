5945bfbc76c91df04c09f00df3c700ef52c76470
David R. Smith <drs.code.monkey@gmail.com>
2016-11-29 10:02:56 -0800

    elastic search persons

--- src/main/java/gov/ca/cwds/rest/services/PersonService.java
+++ src/main/java/gov/ca/cwds/rest/services/PersonService.java
@@ -11,8 +11,10 @@ import gov.ca.cwds.rest.jdbi.Dao;
 import gov.ca.cwds.rest.jdbi.ns.PersonDao;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import org.apache.commons.lang3.NotImplementedException;
+import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,8 +46,8 @@ public class PersonService implements CrudsService {
     this.elasticsearchDao = elasticsearchDao;
   }
 
-  /*
-   * (non-Javadoc)
+  /**
+   * {@inheritDoc}
    * 
    * @see gov.ca.cwds.rest.services.CrudsService#find(java.io.Serializable)
    */
@@ -60,20 +62,8 @@ public class PersonService implements CrudsService {
     return null;
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see gov.ca.cwds.rest.services.CrudsService#delete(java.io.Serializable)
-   */
-  @Override
-  public Response delete(Serializable primaryKey) {
-    assert (primaryKey instanceof Long);
-
-    throw new NotImplementedException("Delete is not implemented");
-  }
-
-  /*
-   * (non-Javadoc)
+  /**
+   * {@inheritDoc}
    * 
    * @see gov.ca.cwds.rest.services.CrudsService#create(gov.ca.cwds.rest.api.Request)
    */
@@ -98,9 +88,11 @@ public class PersonService implements CrudsService {
         document = mapper.writeValueAsString(esPerson);
       }
 
-      elasticsearchDao.start();
+      // Methods start/stop are now protected. Dao manages its own connections.
+      // elasticsearchDao.start();
+      // elasticsearchDao.start();
       elasticsearchDao.index(document, esPerson.getId().toString());
-      elasticsearchDao.stop();
+      // elasticsearchDao.stop();
     } catch (JsonProcessingException e) {
       throw new ApiException("Unable to convert Person to json to Index in Elasticsearch");
     } catch (Exception e) {
@@ -109,12 +101,50 @@ public class PersonService implements CrudsService {
     return postedPerson;
   }
 
+  public PostedPerson[] fetchAllPersons() throws Exception {
+    final SearchHit[] hits = this.elasticsearchDao.queryAllPersons();
 
-  /*
-   * (non-Javadoc)
+    // SAMPLE HIT:
+    // {updated_at=2016-11-23-09.09.15.930, gender=Male, date_of_birth=1990-04-01,
+    // created_at=2016-11-23-09.09.15.953, last_name=Simpson, id=100, first_name=Bart,
+    // ssn=999551111}
+
+    PostedPerson[] persons = new PostedPerson[hits.length];
+    int counter = -1;
+    for (SearchHit hit : hits) {
+      System.out.println("------------------------------");
+      final Map<String, Object> m = hit.getSource();
+      System.out.println(m);
+
+      persons[++counter] = new PostedPerson(Long.parseLong(m.getOrDefault("id", "0").toString()),
+          (String) m.getOrDefault("first_name", ""), (String) m.getOrDefault("last_name", ""),
+          (String) m.getOrDefault("gender", null), (String) m.getOrDefault("date_of_birth", null),
+          (String) m.getOrDefault("ssn", null), null);
+    }
+
+    return persons;
+  }
+
+  // ===================
+  // NOT IMPLEMENTED:
+  // ===================
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see gov.ca.cwds.rest.services.CrudsService#delete(java.io.Serializable)
+   */
+  @Override
+  public Response delete(Serializable primaryKey) {
+    assert (primaryKey instanceof Long);
+    throw new NotImplementedException("Delete is not implemented");
+  }
+
+  /**
+   * {@inheritDoc}
    * 
    * @see gov.ca.cwds.rest.services.CrudsService#update(java.io.Serializable,
-   * gov.ca.cwds.rest.api.Request)
+   *      gov.ca.cwds.rest.api.Request)
    */
   @Override
   public Response update(Serializable primaryKey, Request request) {
