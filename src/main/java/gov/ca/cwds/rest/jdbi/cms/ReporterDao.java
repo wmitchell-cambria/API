ed16ea39b944a2096f60447ab5003ec8d9fb6fd7
Gregg Hill <gregg.hill@tabordasolutions.com>
2016-12-07 09:56:39 -0800

    Fixed ReporterDao to extend BaseDaoImpl instead of CrudsDaoImpl

--- src/main/java/gov/ca/cwds/rest/jdbi/cms/ReporterDao.java
+++ src/main/java/gov/ca/cwds/rest/jdbi/cms/ReporterDao.java
@@ -1,11 +1,11 @@
 package gov.ca.cwds.rest.jdbi.cms;
 
+import gov.ca.cwds.rest.api.persistence.cms.Reporter;
+import gov.ca.cwds.rest.jdbi.BaseDaoImpl;
+
 import org.hibernate.SessionFactory;
 
-import gov.ca.cwds.rest.api.persistence.cms.Reporter;
-import gov.ca.cwds.rest.jdbi.CrudsDaoImpl;
-
-public class ReporterDao extends CrudsDaoImpl<Reporter> {
+public class ReporterDao extends BaseDaoImpl<Reporter> {
 
   public ReporterDao(SessionFactory sessionFactory) {
     super(sessionFactory);
