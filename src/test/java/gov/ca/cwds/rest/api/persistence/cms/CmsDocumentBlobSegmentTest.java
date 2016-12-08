2a8385e1c4df3af8af4b95a4e9119e9495dd4907
jimlebeau <James.LeBeau@osi.ca.gov>
2016-12-08 10:11:44 -0800

    #135352187 - Tech Debt : Increase Testing Coverage

---
+++ src/test/java/gov/ca/cwds/rest/api/persistence/cms/CmsDocumentBlobSegmentTest.java
@@ -0,0 +1,48 @@
+package gov.ca.cwds.rest.api.persistence.cms;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+import gov.ca.cwds.rest.api.persistence.cms.ReferralClient.PrimaryKey;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+public class CmsDocumentBlobSegmentTest {
+  private String docHandle = "doc1handle";
+  private String segmentSequence = "10";
+  private String docBlob = "test document blob";
+
+  /*
+   * Constructor test
+   */
+  @Test
+  public void emtpyConstructorIsNotNull() throws Exception {
+    assertThat(CmsDocumentBlobSegment.class.newInstance(), is(notNullValue()));
+  }
+
+  @Test
+  public void persistenttContructorTest() throws Exception {
+
+    CmsDocumentBlobSegment blob = new CmsDocumentBlobSegment(docHandle, segmentSequence, docBlob);
+    assertThat(blob.getDocBlob(), is(equalTo(docBlob)));
+    assertThat(blob.getSegmentSequence(), is(equalTo(segmentSequence)));
+    assertThat(blob.getDocHandle(), is(equalTo(docHandle)));
+  }
+
+  @Test
+  public void equalsHashCodeWork() {
+    EqualsVerifier.forClass(CmsDocumentBlobSegment.class).suppress(Warning.NONFINAL_FIELDS)
+        .verify();
+  }
+
+  @Test
+  public void successGetPrimaryKey() throws Exception {
+    PrimaryKey pk = new PrimaryKey(docHandle, segmentSequence);
+    CmsDocumentBlobSegment blob = new CmsDocumentBlobSegment(docHandle, segmentSequence, docBlob);
+    assertThat(blob.getPrimaryKey().toString(), is(equalTo(pk.toString())));
+  }
+}
