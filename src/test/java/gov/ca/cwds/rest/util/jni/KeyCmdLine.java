4f97dd4a616889e1a27c744227afd6801304b5dd
David R. Smith <drs.code.monkey@gmail.com>
2016-12-16 14:03:54 -0800

    test LZW Gradle config

--- src/test/java/gov/ca/cwds/rest/util/jni/KeyCmdLine.java
+++ src/test/java/gov/ca/cwds/rest/util/jni/KeyCmdLine.java
@@ -9,19 +9,6 @@ import gov.ca.cwds.rest.util.jni.KeyJNI.KeyDetail;
  */
 public class KeyCmdLine {
 
-  /**
-   * Track memory to hunt memory leaks and record overall memory consumption.
-   * 
-   * @return free memory in MB
-   */
-  public static long calcMemory() {
-    Runtime runtime = Runtime.getRuntime();
-    long maxMemory = runtime.maxMemory();
-    long allocatedMemory = runtime.totalMemory();
-    long freeMemory = runtime.freeMemory();
-    return (freeMemory + (maxMemory - allocatedMemory)) / 1024L;
-  }
-
   // Test Driver
   public static void main(String[] args) {
     KeyJNI inst = new KeyJNI();
@@ -31,22 +18,22 @@ public class KeyCmdLine {
       // DECOMPOSE KEY:
       // ===================
 
-      final long startingMemory = calcMemory();
+      final long startingMemory = CWDSCompressionUtils.calcMemory();
       KeyDetail kd = new KeyDetail();
       inst.decomposeKey(args[1], kd);
       System.out.println("Java: key=" + kd.key + ", staffId=" + kd.staffId + ", UITimestamp="
           + kd.UITimestamp + ", PTimestamp=" + kd.PTimestamp);
-      System.out.println("used memory = " + (calcMemory() - startingMemory));
+      System.out.println("used memory = " + (CWDSCompressionUtils.calcMemory() - startingMemory));
     } else {
       // ===================
       // GENERATE KEY:
       // ===================
 
-      final long startingMemory = calcMemory();
+      final long startingMemory = CWDSCompressionUtils.calcMemory();
       System.out.println("Java: Call JNI generateKey ... ");
       final String key = inst.generateKey(args[1]);
       System.out.println("Java: key=" + key);
-      System.out.println("used memory = " + (calcMemory() - startingMemory));
+      System.out.println("used memory = " + (CWDSCompressionUtils.calcMemory() - startingMemory));
     }
   }
 
