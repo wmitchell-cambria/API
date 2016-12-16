f1f6facddc97400e06eeb02edadde010f35ff4f9
David R. Smith <drs.code.monkey@gmail.com>
2016-12-16 11:52:59 -0800

    more document testing

--- src/test/java/gov/ca/cwds/rest/util/jni/LZWCmdLine.java
+++ src/test/java/gov/ca/cwds/rest/util/jni/LZWCmdLine.java
@@ -7,31 +7,18 @@ package gov.ca.cwds.rest.util.jni;
  */
 public class LZWCmdLine {
 
-  /**
-   * Track memory to hunt memory leaks and record overall memory consumption.
-   * 
-   * @return free memory in MB
-   */
-  public static long calcMemory() {
-    Runtime runtime = Runtime.getRuntime();
-    long maxMemory = runtime.maxMemory();
-    long allocatedMemory = runtime.totalMemory();
-    long freeMemory = runtime.freeMemory();
-    return (freeMemory + (maxMemory - allocatedMemory)) / 1024L;
-  }
-
   // Test Driver
   public static void main(String[] args) {
     LZWEncoder inst = new LZWEncoder();
 
     if (args[0].startsWith("-d")) { // De-compress target file.
-      final long startingMemory = calcMemory();
+      final long startingMemory = CWDSCompressionUtils.calcMemory();
       inst.fileCopyUncompress(args[1], args[2]);
-      System.out.println("used memory = " + (calcMemory() - startingMemory));
+      System.out.println("used memory = " + (CWDSCompressionUtils.calcMemory() - startingMemory));
     } else { // Compress target file.
-      final long startingMemory = calcMemory();
+      final long startingMemory = CWDSCompressionUtils.calcMemory();
       inst.fileCopyCompress(args[1], args[2]);
-      System.out.println("used memory = " + (calcMemory() - startingMemory));
+      System.out.println("used memory = " + (CWDSCompressionUtils.calcMemory() - startingMemory));
     }
   }
 
