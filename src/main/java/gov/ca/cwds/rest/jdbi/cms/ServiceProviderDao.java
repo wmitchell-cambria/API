5bf8e37b883c3664a38f627059ed8fd743ea9692
mysorepr <prasad.mysore@osi.ca.gov>
2016-12-16 09:55:27 -0800

    added serviceprovider

---
+++ src/main/java/gov/ca/cwds/rest/jdbi/cms/ServiceProviderDao.java
@@ -0,0 +1,22 @@
+package gov.ca.cwds.rest.jdbi.cms;
+
+import org.hibernate.SessionFactory;
+
+import com.google.inject.Inject;
+
+import gov.ca.cwds.inject.CmsSessionFactory;
+import gov.ca.cwds.rest.api.persistence.cms.ServiceProvider;
+import gov.ca.cwds.rest.jdbi.BaseDaoImpl;
+
+public class ServiceProviderDao extends BaseDaoImpl<ServiceProvider> {
+
+  /**
+   * Constructor
+   * 
+   * @param sessionFactory The sessionFactory
+   */
+  @Inject
+  public ServiceProviderDao(@CmsSessionFactory SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
+}
