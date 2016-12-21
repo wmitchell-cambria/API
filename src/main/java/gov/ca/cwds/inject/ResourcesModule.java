d1efb3a4330425c38b52759e6161e6a16a71fdda
Richard Bach <richard.bach@tabordasolutions.com>
2016-12-21 13:28:22 -0800

    added PersonSearchResource to guide

--- src/main/java/gov/ca/cwds/inject/ResourcesModule.java
+++ src/main/java/gov/ca/cwds/inject/ResourcesModule.java
@@ -10,6 +10,7 @@ import gov.ca.cwds.rest.SwaggerConfiguration;
 import gov.ca.cwds.rest.resources.AddressResource;
 import gov.ca.cwds.rest.resources.ApplicationResource;
 import gov.ca.cwds.rest.resources.PersonResource;
+import gov.ca.cwds.rest.resources.PersonSearchResource;
 import gov.ca.cwds.rest.resources.ResourceDelegate;
 import gov.ca.cwds.rest.resources.ScreeningResource;
 import gov.ca.cwds.rest.resources.ServiceBackedResourceDelegate;
@@ -56,6 +57,7 @@ public class ResourcesModule extends AbstractModule {
     bind(ReferralResource.class);
     bind(ReporterResource.class);
     bind(StaffPersonResource.class);
+    bind(PersonSearchResource.class);
 
 
   }
