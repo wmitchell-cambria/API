6fdc834d7b0b940b19e5697570bb5e51cdd74f9d
David R. Smith <drs.code.monkey@gmail.com>
2017-01-06 15:28:15 -0800

    restore address validation constant

--- src/main/java/gov/ca/cwds/rest/core/Api.java
+++ src/main/java/gov/ca/cwds/rest/core/Api.java
@@ -88,6 +88,11 @@ public interface Api {
   public static final String RESOURCE_USER_AUTHORIZATION = "user_authorization";
 
   /**
+   * A {@code String} constant representing {@value #RESOURCE_ADDRESS_VALIDATION} API.
+   */
+  public static final String RESOURCE_ADDRESS_VALIDATION = "address_validation";
+
+  /**
    * A {@code String} constant representing {@value #RESOURCE_AUTOCOMPLETE_PERSON} API.
    */
   public static final String RESOURCE_AUTOCOMPLETE_PERSON = "autocomplete_person";
