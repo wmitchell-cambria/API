05bdaaba98be4ce7cd678e6a0df3bc184466d697
mysorepr <prasad.mysore@osi.ca.gov>
2017-01-19 11:29:27 -0800

    #136823559 add logging to ssn mask

--- src/main/java/gov/ca/cwds/rest/validation/MaskString.java
+++ src/main/java/gov/ca/cwds/rest/validation/MaskString.java
@@ -4,18 +4,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * Masks the Social Security Number to the last 4 digits
+ * 
  * @author CWDS API Team
  *
  */
 public class MaskString {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(SmartyStreet.class);
+  private static final String ssnWithHyphen = "^(?!000)[0-9]{3}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$";
+  private static final String ssnWithOutHyphen = "^(?!000)[0-9]{3}(?!00)[0-9]{2}(?!0000)[0-9]{4}$";
+  private static final Pattern pattern1 = Pattern.compile(ssnWithHyphen);
+  private static final Pattern pattern2 = Pattern.compile(ssnWithOutHyphen);
+
   public String maskSsn(String ssn) {
     Integer length;
     Integer startPos;
     String returnSsn;
-
 
     if (StringUtils.isBlank(ssn)) {
       returnSsn = "";
@@ -23,12 +32,6 @@ public class MaskString {
     }
 
     String actualString = ssn.trim();
-
-    String ssnWithHyphen = "^(?!000)[0-9]{3}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$";
-    String ssnWithOutHyphen = "^(?!000)[0-9]{3}(?!00)[0-9]{2}(?!0000)[0-9]{4}$";
-
-    Pattern pattern1 = Pattern.compile(ssnWithHyphen);
-    Pattern pattern2 = Pattern.compile(ssnWithOutHyphen);
 
     Matcher matcher1 = pattern1.matcher(actualString);
     Matcher matcher2 = pattern2.matcher(actualString);
@@ -41,6 +44,7 @@ public class MaskString {
       returnSsn = actualString.substring(startPos, length);
     } else {
       returnSsn = "";
+      LOGGER.error("Invalid SSN format or SSN is blank");
     }
 
     return returnSsn;
