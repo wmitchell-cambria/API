3e942a407e0c14c1a11781a9c256d59af815b061
jimlebeau <James.LeBeau@osi.ca.gov>
2017-01-23 09:43:21 -0800

    #137548119 - Translate system codes to text before loading into ES

--- src/test/java/gov/ca/cwds/data/persistence/cms/ClientTest.java
+++ src/test/java/gov/ca/cwds/data/persistence/cms/ClientTest.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 package gov.ca.cwds.data.persistence.cms;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -19,171 +19,8 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.ca.cwds.data.ILanguageAware;
-import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
-
-public class ClientTest {
-
-  private static final ObjectMapper MAPPER = SystemCodeTestHarness.MAPPER;
-
-  private Client validBean() throws JsonParseException, JsonMappingException, IOException {
-    return MAPPER.readValue(fixture("fixtures/persistence/legacy/Client/old_style_valid.json"),
-        Client.class);
-  }
-
-  @Test
-  public void testSerializeAndDeserialize() throws Exception {
-    final Client tgt = validBean();
-
-    // Serialize to JSON.
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    try (PrintStream ps = new PrintStream(baos)) {
-      MAPPER.writerWithDefaultPrettyPrinter().writeValue(ps, tgt);
-    } finally {
-    }
-    final String json = baos.toString(java.nio.charset.StandardCharsets.UTF_8.name());
-
-    // Deserialize from JSON just written.
-    final Client actual = MAPPER.readValue(json, Client.class);
-
-    // Does it match exactly?
-    assertThat(actual, is(equalTo(validBean())));
-  }
-
-  /*
-   * Constructor test
-   */
-  @Test
-  public void emtpyConstructorIsNotNull() throws Exception {
-    assertThat(Client.class.newInstance(), is(notNullValue()));
-  }
-
-  @Test
-  public void type() throws Exception {
-    assertThat(Client.class, notNullValue());
-  }
-
-  @Test
-  public void instantiation() throws Exception {
-    final Client target = validBean();
-    assertThat(target, notNullValue());
-  }
-
-  @Test
-  public void getPrimaryKey_Args$() throws Exception {
-    final Client target = validBean();
-    // given
-    // e.g. : given(mocked.called()).willReturn(1);
-    // when
-    final String actual = target.getPrimaryKey();
-    // then
-    // e.g. : verify(mocked).called();
-    final String expected = "B3ucP1K07n";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getMiddleName_Args$() throws Exception {
-    final Client target = validBean();
-    // given
-    // e.g. : given(mocked.called()).willReturn(1);
-    // when
-    String actual = target.getMiddleName();
-    // then
-    // e.g. : verify(mocked).called();
-    final String expected = "";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getFirstName_Args$() throws Exception {
-    final Client target = validBean();
-    // given
-    // e.g. : given(mocked.called()).willReturn(1);
-    // when
-    String actual = target.getFirstName();
-    // then
-    // e.g. : verify(mocked).called();
-    final String expected = "Chicken";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getLastName_Args$() throws Exception {
-    final Client target = validBean();
-    // given
-    // e.g. : given(mocked.called()).willReturn(1);
-    // when
-    String actual = target.getLastName();
-    // then
-    // e.g. : verify(mocked).called();
-    final String expected = "Little";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getSsn_Args$() throws Exception {
-    final Client target = validBean();
-    // given
-    // e.g. : given(mocked.called()).willReturn(1);
-    // when
-    String actual = target.getSsn();
-    // then
-    // e.g. : verify(mocked).called();
-    final String expected = "";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getNameSuffix_Args$() throws Exception {
-    final Client target = validBean();
-    final String actual = target.getNameSuffix();
-    final String expected = "";
-    assertThat(actual, is(equalTo(expected)));
-  }
-
-  @Test
-  public void getLanguages_Args$() throws Exception {
-    final Client target = validBean();
-    final ILanguageAware[] actual = target.getLanguages();
-    assertTrue(actual.length > 0);
-  }
-
-  @Test
-  public void hashCode_Args$() throws Exception {
-    EqualsVerifier.forClass(Client.class).suppress(Warning.NONFINAL_FIELDS).verify();
-  }
-
-  @Test
-  public void equals_Args$Object() throws Exception {
-    EqualsVerifier.forClass(Client.class).suppress(Warning.NONFINAL_FIELDS).verify();
-  }
-
-}
-=======
-package gov.ca.cwds.data.persistence.cms;
-
-import static io.dropwizard.testing.FixtureHelpers.fixture;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-
-import java.io.IOException;
-
-import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-
-import gov.ca.cwds.data.CmsSystemCodeSerializer;
 import gov.ca.cwds.data.persistence.junit.template.PersistentTestTemplate;
 import gov.ca.cwds.rest.api.domain.DomainChef;
-import io.dropwizard.jackson.Jackson;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
@@ -193,21 +30,7 @@ import nl.jqno.equalsverifier.Warning;
  */
 public class ClientTest implements PersistentTestTemplate {
 
-  private static final ObjectMapper MAPPER;
-
-  /**
-   * Auto-magically translate CMS system codes when serializing JSON.
-   */
-  static {
-    // Inject system code cache.
-    ObjectMapper mapper = Jackson.newObjectMapper();
-    SimpleModule module = new SimpleModule("SystemCodeModule",
-        new Version(1, 0, 24, "alpha", "ca.gov.data.persistence.cms", "syscode"));
-    module.addSerializer(Short.class,
-        new CmsSystemCodeSerializer(new CmsSystemCodeCacheService(new SystemCodeDaoFileImpl())));
-    mapper.registerModule(module);
-    MAPPER = mapper;
-  }
+  private static final ObjectMapper MAPPER = SystemCodeTestHarness.MAPPER;
 
   /*
    * Constructor test
@@ -497,5 +320,136 @@ public class ClientTest implements PersistentTestTemplate {
             gov.ca.cwds.rest.api.domain.cms.Client.class);
     return validDomainClient;
   }
+
+  @Test
+  public void testSerializeAndDeserialize() throws Exception {
+    final Client tgt = validBean();
+
+    // Serialize to JSON.
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    try (PrintStream ps = new PrintStream(baos)) {
+      MAPPER.writerWithDefaultPrettyPrinter().writeValue(ps, tgt);
+    } finally {
+    }
+    final String json = baos.toString(java.nio.charset.StandardCharsets.UTF_8.name());
+
+    // Deserialize from JSON just written.
+    final Client actual = MAPPER.readValue(json, Client.class);
+
+    // Does it match exactly?
+    assertThat(actual, is(equalTo(validBean())));
+  }
+
+  /*
+   * Constructor test
+   */
+  @Test
+  public void emtpyConstructorIsNotNull() throws Exception {
+    assertThat(Client.class.newInstance(), is(notNullValue()));
+  }
+
+  @Test
+  public void type() throws Exception {
+    assertThat(Client.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    final Client target = validBean();
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void getPrimaryKey_Args$() throws Exception {
+    final Client target = validBean();
+    // given
+    // e.g. : given(mocked.called()).willReturn(1);
+    // when
+    final String actual = target.getPrimaryKey();
+    // then
+    // e.g. : verify(mocked).called();
+    final String expected = "B3ucP1K07n";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getMiddleName_Args$() throws Exception {
+    final Client target = validBean();
+    // given
+    // e.g. : given(mocked.called()).willReturn(1);
+    // when
+    String actual = target.getMiddleName();
+    // then
+    // e.g. : verify(mocked).called();
+    final String expected = "";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getFirstName_Args$() throws Exception {
+    final Client target = validBean();
+    // given
+    // e.g. : given(mocked.called()).willReturn(1);
+    // when
+    String actual = target.getFirstName();
+    // then
+    // e.g. : verify(mocked).called();
+    final String expected = "Chicken";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getLastName_Args$() throws Exception {
+    final Client target = validBean();
+    // given
+    // e.g. : given(mocked.called()).willReturn(1);
+    // when
+    String actual = target.getLastName();
+    // then
+    // e.g. : verify(mocked).called();
+    final String expected = "Little";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getSsn_Args$() throws Exception {
+    final Client target = validBean();
+    // given
+    // e.g. : given(mocked.called()).willReturn(1);
+    // when
+    String actual = target.getSsn();
+    // then
+    // e.g. : verify(mocked).called();
+    final String expected = "";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getNameSuffix_Args$() throws Exception {
+    final Client target = validBean();
+    final String actual = target.getNameSuffix();
+    final String expected = "";
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getLanguages_Args$() throws Exception {
+    final Client target = validBean();
+    final ILanguageAware[] actual = target.getLanguages();
+    assertTrue(actual.length > 0);
+  }
+
+  @Test
+  public void hashCode_Args$() throws Exception {
+    EqualsVerifier.forClass(Client.class).suppress(Warning.NONFINAL_FIELDS).verify();
+  }
+
+  @Test
+  public void equals_Args$Object() throws Exception {
+    EqualsVerifier.forClass(Client.class).suppress(Warning.NONFINAL_FIELDS).verify();
+  }
+
+  private Client validBean() throws JsonParseException, JsonMappingException, IOException {
+    return MAPPER.readValue(fixture("fixtures/persistence/legacy/Client/valid.json"), Client.class);
+  }
 }
->>>>>>> #137548119 - Translate system codes to text before loading into ES
