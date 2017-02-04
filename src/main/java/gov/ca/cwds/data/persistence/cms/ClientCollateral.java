3a9f3585bd21b46c74c979156c40e63c6538195f
jimlebeau <James.LeBeau@osi.ca.gov>
2017-02-04 08:35:51 -0800

    #138160209 - Search: Identify Person - Filter out Sealed and Sensitive clients

---
+++ src/main/java/gov/ca/cwds/data/persistence/cms/ClientCollateral.java
@@ -0,0 +1,189 @@
+package gov.ca.cwds.data.persistence.cms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import gov.ca.cwds.data.CmsSystemCodeDeserializer;
+import gov.ca.cwds.data.SystemCodeSerializer;
+import gov.ca.cwds.data.persistence.PersistentObject;
+
+
+/**
+ * {@link PersistentObject} representing a ClientCollateral
+ * 
+ * @author CWDS API Team
+ * 
+ */
+@Entity
+@Table(name = "CLN_COLT")
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClientCollateral extends CmsPersistentObject {
+
+  private static final long serialVersionUID = 1L;
+
+  @Column(name = "ACTIVE_IND")
+  private String activeIndicator;
+
+  @SystemCodeSerializer(logical = true, description = true)
+  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
+  @Type(type = "short")
+  @Column(name = "COL_RELC")
+  private Short collateralClientReporterRelationshipType;
+
+  @Column(name = "COMNT_DSC")
+  private String commentDescription;
+
+  @Column(name = "FKCLIENT_T")
+  private String clientId;
+
+  @Column(name = "FKCOLTRL_T")
+  private String collateralIndividualId;
+
+  @Id
+  @Column(name = "THIRD_ID", length = CMS_ID_LEN)
+  private String thirdId;
+
+  /**
+   * Default constructor
+   * 
+   * Required for Hibernate
+   */
+  public ClientCollateral() {
+    super();
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param activeIndicator = Indicates if relationship to CollateralIndividual is active (Y)
+   * @param collateralClientReporterRelationshipType = Collateral Client Rptr Relationship Type in
+   *        System Codes
+   * @param commentDescription - description
+   * @param clientId - foreign key Client table
+   * @param collateralIndividualId - foreign key to CollateralIndividual table
+   * @param thirdId - Unique key
+   * 
+   */
+  public ClientCollateral(String activeIndicator, Short collateralClientReporterRelationshipType,
+      String commentDescription, String clientId, String collateralIndividualId, String thirdId) {
+    super();
+    this.activeIndicator = activeIndicator;
+    this.collateralClientReporterRelationshipType = collateralClientReporterRelationshipType;
+    this.commentDescription = commentDescription;
+    this.clientId = clientId;
+    this.collateralIndividualId = collateralIndividualId;
+    this.thirdId = thirdId;
+  }
+
+  /**
+   * @return serialVersionUID
+   */
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+
+  @SuppressWarnings("javadoc")
+  public String getActiveIndicator() {
+    return activeIndicator;
+  }
+
+  @SuppressWarnings("javadoc")
+  public Short getCollateralClientReporterRelationshipType() {
+    return collateralClientReporterRelationshipType;
+  }
+
+  /**
+   * @return commentDescription
+   */
+  public String getCommentDescription() {
+    return commentDescription;
+  }
+
+  @SuppressWarnings("javadoc")
+  public String getClientId() {
+    return clientId;
+  }
+
+  @SuppressWarnings("javadoc")
+  public String getCollateralIndividualId() {
+    return collateralIndividualId;
+  }
+
+  @SuppressWarnings("javadoc")
+  public String getThirdId() {
+    return thirdId;
+  }
+
+  @Override
+  public String getPrimaryKey() {
+    return getThirdId();
+  }
+
+  @Override
+  public final int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((activeIndicator == null) ? 0 : activeIndicator.hashCode());
+    result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+    result = prime * result + ((collateralClientReporterRelationshipType == null) ? 0
+        : collateralClientReporterRelationshipType.hashCode());
+    result =
+        prime * result + ((collateralIndividualId == null) ? 0 : collateralIndividualId.hashCode());
+    result = prime * result + ((commentDescription == null) ? 0 : commentDescription.hashCode());
+    result = prime * result + ((thirdId == null) ? 0 : thirdId.hashCode());
+    return result;
+  }
+
+  @Override
+  public final boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ClientCollateral other = (ClientCollateral) obj;
+    if (activeIndicator == null) {
+      if (other.activeIndicator != null)
+        return false;
+    } else if (!activeIndicator.equals(other.activeIndicator))
+      return false;
+    if (clientId == null) {
+      if (other.clientId != null)
+        return false;
+    } else if (!clientId.equals(other.clientId))
+      return false;
+    if (collateralClientReporterRelationshipType == null) {
+      if (other.collateralClientReporterRelationshipType != null)
+        return false;
+    } else if (!collateralClientReporterRelationshipType
+        .equals(other.collateralClientReporterRelationshipType))
+      return false;
+    if (collateralIndividualId == null) {
+      if (other.collateralIndividualId != null)
+        return false;
+    } else if (!collateralIndividualId.equals(other.collateralIndividualId))
+      return false;
+    if (commentDescription == null) {
+      if (other.commentDescription != null)
+        return false;
+    } else if (!commentDescription.equals(other.commentDescription))
+      return false;
+    if (thirdId == null) {
+      if (other.thirdId != null)
+        return false;
+    } else if (!thirdId.equals(other.thirdId))
+      return false;
+    return true;
+  }
+
+}
---
+++ src/test/java/gov/ca/cwds/data/persistence/cms/ClientCollateralTest.java
@@ -0,0 +1,92 @@
+package gov.ca.cwds.data.persistence.cms;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.ca.cwds.data.persistence.junit.template.PersistentTestTemplate;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public class ClientCollateralTest implements PersistentTestTemplate {
+
+  private static final ObjectMapper MAPPER = SystemCodeTestHarness.MAPPER;
+
+  @Override
+  public void testEqualsHashCodeWorks() {
+    // EqualsVerifier.forClass(ClientCollateral.class).suppress(Warning.NONFINAL_FIELDS).verify();
+  }
+
+  /*
+   * Constructor test
+   */
+  @Override
+  @Test
+  public void testEmptyConstructor() throws Exception {
+    assertThat(ClientCollateral.class.newInstance(), is(notNullValue()));
+  }
+
+  @Override
+  @Test
+  public void testPersistentConstructor() throws Exception {
+    ClientCollateral ci = validClientCollateral();
+
+    ClientCollateral persistent = new ClientCollateral(ci.getActiveIndicator(),
+        ci.getCollateralClientReporterRelationshipType(), ci.getCommentDescription(),
+        ci.getClientId(), ci.getCollateralIndividualId(), ci.getThirdId());
+
+
+    assertThat(persistent.getActiveIndicator(), is(equalTo(ci.getActiveIndicator())));
+    assertThat(persistent.getCollateralClientReporterRelationshipType(),
+        is(equalTo(ci.getCollateralClientReporterRelationshipType())));
+    assertThat(persistent.getCommentDescription(), is(equalTo(ci.getCommentDescription())));
+    assertThat(persistent.getClientId(), is(equalTo(ci.getClientId())));
+    assertThat(persistent.getCollateralIndividualId(), is(equalTo(ci.getCollateralIndividualId())));
+    assertThat(persistent.getThirdId(), is(equalTo(ci.getThirdId())));
+    // MAPPER.writerWithDefaultPrettyPrinter().writeValue(System.out, persistent);
+  }
+
+  @Override
+  public void testConstructorUsingDomain() throws Exception {
+    // no domain class.
+
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void testSerializeAndDeserialize() throws Exception {
+    ClientCollateral ci = validClientCollateral();
+
+    ClientCollateral persistent = new ClientCollateral(ci.getActiveIndicator(),
+        ci.getCollateralClientReporterRelationshipType(), ci.getCommentDescription(),
+        ci.getClientId(), ci.getCollateralIndividualId(), ci.getThirdId());
+    final String expected = MAPPER.writeValueAsString((MAPPER.readValue(
+        fixture("fixtures/persistent/ClientCollateral/valid/validWithSysCodes.json"),
+        ClientCollateral.class)));
+
+    assertThat(MAPPER.writeValueAsString(persistent)).isEqualTo(expected);
+  }
+
+  private ClientCollateral validClientCollateral()
+      throws JsonParseException, JsonMappingException, IOException {
+
+    ClientCollateral validClientCollateral = MAPPER.readValue(
+        fixture("fixtures/persistent/ClientCollateral/valid/valid.json"), ClientCollateral.class);
+
+    return validClientCollateral;
+  }
+
+}
