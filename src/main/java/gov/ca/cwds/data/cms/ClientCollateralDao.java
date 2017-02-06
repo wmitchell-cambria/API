5d5e8f16d9ba310ed101256e93a9af88148992c2
David R. Smith <drs.code.monkey@gmail.com>
2017-02-06 09:25:21 -0800

    #138163381: DAO for client collateral

---
+++ src/main/java/gov/ca/cwds/data/cms/ClientCollateralDao.java
@@ -0,0 +1,27 @@
+package gov.ca.cwds.data.cms;
+
+import org.hibernate.SessionFactory;
+
+import com.google.inject.Inject;
+
+import gov.ca.cwds.data.BaseDaoImpl;
+import gov.ca.cwds.data.persistence.cms.ClientCollateral;
+import gov.ca.cwds.inject.CmsSessionFactory;
+
+/**
+ * DAO for {@link ClientCollateral}.
+ * 
+ * @author CWDS API Team
+ */
+public class ClientCollateralDao extends BaseDaoImpl<ClientCollateral> {
+
+  /**
+   * Constructor
+   * 
+   * @param sessionFactory The sessionFactory
+   */
+  @Inject
+  public ClientCollateralDao(@CmsSessionFactory SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
+}
