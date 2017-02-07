9f3229b2b720441147785a87b319b1e13c3f3acc
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2017-02-07 11:56:57 -0800

    ESPerson sourceObj added with Transient Annotation

--- src/main/java/gov/ca/cwds/rest/api/domain/es/ESPerson.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/es/ESPerson.java
@@ -3,6 +3,8 @@ package gov.ca.cwds.rest.api.domain.es;
 import java.io.Serializable;
 import java.util.Map;
 
+import javax.persistence.Transient;
+
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
@@ -301,6 +303,7 @@ public class ESPerson extends Person {
    * Class type {@link #sourceType}.
    */
   @JsonProperty("source_object")
+  @Transient
   private Object sourceObj;
 
   /**
