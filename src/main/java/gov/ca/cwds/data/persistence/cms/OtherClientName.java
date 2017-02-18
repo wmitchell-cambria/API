bf5ebf0e90005dd72a915f24d69ee69f0f3ce8a6
David R. Smith <drs.code.monkey@gmail.com>
2017-02-18 10:47:33 -0800

    #138438305: native query against replication

--- src/main/java/gov/ca/cwds/data/persistence/cms/OtherClientName.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/OtherClientName.java
@@ -35,21 +35,25 @@ import gov.ca.cwds.data.persistence.PersistentObject;
  * @author CWDS API Team
  */
 @SuppressWarnings("serial")
-@NamedQueries({
-    @NamedQuery(name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findAll",
-        query = "FROM OtherClientName WHERE clientId IN (SELECT id FROM Client WHERE sensitivityIndicator = 'N' AND soc158SealedClientIndicator = 'N')"),
-    @NamedQuery(name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findAllUpdatedAfter",
-        query = "FROM OtherClientName WHERE lastUpdatedTime > :after AND clientId IN (SELECT id FROM Client WHERE sensitivityIndicator = 'N' AND soc158SealedClientIndicator = 'N')")})
-@NamedNativeQueries({@NamedNativeQuery(
-    name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findPartitionedBuckets",
-    query = "select z.THIRD_ID, z.FIRST_NM, z.LAST_NM, z.MIDDLE_NM, z.NMPRFX_DSC, "
-        + "z.NAME_TPC, z.SUFX_TLDSC, z.LST_UPD_ID, z.LST_UPD_TS, z.FKCLIENT_T "
-        + "from ( select mod(y.rn, CAST(:total_buckets AS INTEGER)) + 1 as bucket, y.* "
-        + "from ( select row_number() over (order by 1) as rn, x.* "
-        + "from ( select c.* from {h-schema}OCL_NM_T c "
-        + "WHERE c.FKCLIENT_T >= :min_id AND c.FKCLIENT_T < :max_id AND THIRD_ID < '9999999999' "
-        + ") x ) y ) z where z.bucket = :bucket_num for read only",
-    resultClass = OtherClientName.class)})
+@NamedQueries({@NamedQuery(name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findAll",
+    query = "FROM OtherClientName WHERE clientId IN (SELECT id FROM Client WHERE sensitivityIndicator = 'N' AND soc158SealedClientIndicator = 'N')"),})
+@NamedNativeQueries({
+    @NamedNativeQuery(name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findAllUpdatedAfter",
+        query = "select x.THIRD_ID, x.FIRST_NM, x.LAST_NM, x.MIDDLE_NM, x.NMPRFX_DSC, "
+            + "x.NAME_TPC, x.SUFX_TLDSC, x.LST_UPD_ID, x.LST_UPD_TS, x.FKCLIENT_T "
+            + "select c.* from {h-schema}OCL_NM_T x WHERE c.IBMSNAP_LOGMARKER >= :after "
+            + "for read only",
+        resultClass = OtherClientName.class),
+    @NamedNativeQuery(
+        name = "gov.ca.cwds.data.persistence.cms.OtherClientName.findPartitionedBuckets",
+        query = "select z.THIRD_ID, z.FIRST_NM, z.LAST_NM, z.MIDDLE_NM, z.NMPRFX_DSC, "
+            + "z.NAME_TPC, z.SUFX_TLDSC, z.LST_UPD_ID, z.LST_UPD_TS, z.FKCLIENT_T "
+            + "from ( select mod(y.rn, CAST(:total_buckets AS INTEGER)) + 1 as bucket, y.* "
+            + "from ( select row_number() over (order by 1) as rn, x.* "
+            + "from ( select c.* from {h-schema}OCL_NM_T c "
+            + "WHERE c.FKCLIENT_T >= :min_id AND c.FKCLIENT_T < :max_id AND THIRD_ID < '9999999999' "
+            + ") x ) y ) z where z.bucket = :bucket_num for read only",
+        resultClass = OtherClientName.class)})
 @Entity
 @Table(name = "OCL_NM_T")
 @JsonPropertyOrder(alphabetic = true)
@@ -59,6 +63,7 @@ public class OtherClientName extends CmsPersistentObject implements IPersonAware
   @Column(name = "FIRST_NM")
   private String firstName;
 
+  // TODO: correct unit test and enable the composite key.
   // @Id
   @Column(name = "FKCLIENT_T", length = CMS_ID_LEN)
   private String clientId;
