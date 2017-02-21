2a7a51748a0c8b833ed84eaaff2dd7e803ed62ef
David R. Smith <drs.code.monkey@gmail.com>
2017-02-21 14:16:10 -0800

    #138438305: replication: Substitute Service Provider

--- src/main/java/gov/ca/cwds/data/persistence/cms/SubstituteCareProvider.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/SubstituteCareProvider.java
@@ -3,31 +3,21 @@ package gov.ca.cwds.data.persistence.cms;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.NamedNativeQueries;
 import org.hibernate.annotations.NamedNativeQuery;
 import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
-import org.hibernate.annotations.Type;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-import gov.ca.cwds.data.CmsSystemCodeDeserializer;
-import gov.ca.cwds.data.SystemCodeSerializer;
 import gov.ca.cwds.data.persistence.PersistentObject;
-import gov.ca.cwds.data.std.ApiPersonAware;
-
 
 /**
- * {@link PersistentObject} representing a SubstituteCareProvider
+ * {@link PersistentObject} representing a SubstituteCareProvider.
  * 
  * @author CWDS API Team
  */
@@ -56,143 +46,12 @@ import gov.ca.cwds.data.std.ApiPersonAware;
 @Table(name = "SB_PVDRT")
 @JsonPropertyOrder(alphabetic = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SubstituteCareProvider extends CmsPersistentObject implements ApiPersonAware {
+public class SubstituteCareProvider extends BaseSubstituteCareProvider {
 
   /**
-   * Base serialization version. Increment per change.
+   * 
    */
   private static final long serialVersionUID = 1L;
-
-  @Id
-  @Column(name = "IDENTIFIER", length = CMS_ID_LEN)
-  private String id;
-
-  @Column(name = "ADD_TEL_NO")
-  private BigDecimal additionalPhoneNumber;
-
-  @Type(type = "integer")
-  @Column(name = "ADD_EXT_NO")
-  private Integer additionlPhoneExtensionNumber;
-
-  @Column(name = "YR_INC_AMT")
-  private BigDecimal annualIncomeAmount;
-
-  @Type(type = "date")
-  @Column(name = "BIRTH_DT")
-  private Date birthDate;
-
-  @Column(name = "CA_DLIC_NO")
-  private String caDriverLicenseNumber;
-
-  @Column(name = "CITY_NM")
-  private String cityName;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "EDUCATION")
-  private Short educationType;
-
-  @Column(name = "EMAIL_ADDR")
-  private String emailAddress;
-
-  @Column(name = "EMPLYR_NM")
-  private String employerName;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "EMPL_STAT")
-  private Short employmentStatusType;
-
-  @Column(name = "ETH_UD_CD")
-  private String ethUnableToDetReasonCode;
-
-  @Column(name = "FIRST_NM")
-  private String firstName;
-
-  @Column(name = "FRG_ADRT_B")
-  private String foreignAddressIndicatorVar;
-
-  @Column(name = "GENDER_IND")
-  private String genderIndicator;
-
-  @Column(name = "HISP_UD_CD")
-  private String hispUnableToDetReasonCode;
-
-  @Column(name = "HISP_CD")
-  private String hispanicOriginCode;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "IND_TRBC")
-  private Short indianTribeType;
-
-  @Column(name = "LAST_NM")
-  private String lastName;
-
-  @Column(name = "LISOWNIND")
-  private String lisOwnershipIndicator;
-
-  @Column(name = "LIS_PER_ID")
-  private String lisPersonId;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "MRTL_STC")
-  private Short maritalStatusType;
-
-  @Column(name = "MID_INI_NM")
-  private String middleInitialName;
-
-  @Column(name = "NMPRFX_DSC")
-  private String namePrefixDescription;
-
-  @Column(name = "PASSBC_CD")
-  private String passedBackgroundCheckCode;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "PRIM_INC")
-  private Short primaryIncomeType;
-
-  @Column(name = "RESOST_IND")
-  private String residedOutOfStateIndicator;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "SEC_INC")
-  private Short secondaryIncomeType;
-
-  @Column(name = "SS_NO")
-  private String socialSecurityNumber;
-
-  @SystemCodeSerializer(logical = true, description = true)
-  @JsonDeserialize(using = CmsSystemCodeDeserializer.class)
-  @Type(type = "short")
-  @Column(name = "STATE_C")
-  private Short stateCodeType;
-
-  @Column(name = "STREET_NM")
-  private String streetName;
-
-  @Column(name = "STREET_NO")
-  private String streetNumber;
-
-  @Column(name = "SUFX_TLDSC")
-  private String suffixTitleDescription;
-
-  @Type(type = "integer")
-  @Column(name = "ZIP_NO")
-  private Integer zipNumber;
-
-  @Type(type = "short")
-  @Column(name = "ZIP_SFX_NO")
-  private Short zipSuffixNumber;
 
   /**
    * Default constructor
@@ -292,264 +151,6 @@ public class SubstituteCareProvider extends CmsPersistentObject implements ApiPe
   /**
    * {@inheritDoc}
    * 
-   * @see gov.ca.cwds.data.persistence.PersistentObject#getPrimaryKey()
-   */
-  @Override
-  public String getPrimaryKey() {
-    return getId();
-  }
-
-  /**
-   * @return the id
-   */
-  public String getId() {
-    return StringUtils.trimToEmpty(id);
-  }
-
-  /**
-   * @return the additionalPhoneNumber
-   */
-  public BigDecimal getAdditionalPhoneNumber() {
-    return additionalPhoneNumber;
-  }
-
-  /**
-   * @return the additionlPhoneExtensionNumber
-   */
-  public Integer getAdditionlPhoneExtensionNumber() {
-    return additionlPhoneExtensionNumber;
-  }
-
-  /**
-   * @return the annualIncomeAmount
-   */
-  public BigDecimal getAnnualIncomeAmount() {
-    return annualIncomeAmount;
-  }
-
-  /**
-   * @return the birthDate
-   */
-  @Override
-  public Date getBirthDate() {
-    return birthDate;
-  }
-
-  /**
-   * @return the caDriverLicenseNumber
-   */
-  public String getCaDriverLicenseNumber() {
-    return StringUtils.trimToEmpty(caDriverLicenseNumber);
-  }
-
-  /**
-   * @return the cityName
-   */
-  public String getCityName() {
-    return StringUtils.trimToEmpty(cityName);
-  }
-
-  /**
-   * @return the educationType
-   */
-  public Short getEducationType() {
-    return educationType;
-  }
-
-  /**
-   * @return the emailAddress
-   */
-  public String getEmailAddress() {
-    return StringUtils.trimToEmpty(emailAddress);
-  }
-
-  /**
-   * @return the employerName
-   */
-  public String getEmployerName() {
-    return StringUtils.trimToEmpty(employerName);
-  }
-
-  /**
-   * @return the employmentStatusType
-   */
-  public Short getEmploymentStatusType() {
-    return employmentStatusType;
-  }
-
-  /**
-   * @return the ethUnableToDetReasonCode
-   */
-  public String getEthUnableToDetReasonCode() {
-    return StringUtils.trimToEmpty(ethUnableToDetReasonCode);
-  }
-
-  /**
-   * @return the firstName
-   */
-  @Override
-  public String getFirstName() {
-    return StringUtils.trimToEmpty(firstName);
-  }
-
-  /**
-   * @return the foreignAddressIndicatorVar
-   */
-  public String getForeignAddressIndicatorVar() {
-    return StringUtils.trimToEmpty(foreignAddressIndicatorVar);
-  }
-
-  /**
-   * @return the genderIndicator
-   */
-  public String getGenderIndicator() {
-    return StringUtils.trimToEmpty(genderIndicator);
-  }
-
-  /**
-   * @return the hispUnableToDetReasonCode
-   */
-  public String getHispUnableToDetReasonCode() {
-    return StringUtils.trimToEmpty(hispUnableToDetReasonCode);
-  }
-
-  /**
-   * @return the hispanicOriginCode
-   */
-  public String getHispanicOriginCode() {
-    return StringUtils.trimToEmpty(hispanicOriginCode);
-  }
-
-  /**
-   * @return the indianTribeType
-   */
-  public Short getIndianTribeType() {
-    return indianTribeType;
-  }
-
-  /**
-   * @return the lastName
-   */
-  @Override
-  public String getLastName() {
-    return StringUtils.trimToEmpty(lastName);
-  }
-
-  /**
-   * @return the lisOwnershipIndicator
-   */
-  public String getLisOwnershipIndicator() {
-    return StringUtils.trimToEmpty(lisOwnershipIndicator);
-  }
-
-  /**
-   * @return the lisPersonId
-   */
-  public String getLisPersonId() {
-    return StringUtils.trimToEmpty(lisPersonId);
-  }
-
-  /**
-   * @return the maritalStatusType
-   */
-  public Short getMaritalStatusType() {
-    return maritalStatusType;
-  }
-
-  /**
-   * @return the middleInitialName
-   */
-  public String getMiddleInitialName() {
-    return StringUtils.trimToEmpty(middleInitialName);
-  }
-
-  /**
-   * @return the namePrefixDescription
-   */
-  public String getNamePrefixDescription() {
-    return StringUtils.trimToEmpty(namePrefixDescription);
-  }
-
-  /**
-   * @return the passedBackgroundCheckCode
-   */
-  public String getPassedBackgroundCheckCode() {
-    return StringUtils.trimToEmpty(passedBackgroundCheckCode);
-  }
-
-  /**
-   * @return the primaryIncomeType
-   */
-  public Short getPrimaryIncomeType() {
-    return primaryIncomeType;
-  }
-
-  /**
-   * @return the residedOutOfStateIndicator
-   */
-  public String getResidedOutOfStateIndicator() {
-    return StringUtils.trimToEmpty(residedOutOfStateIndicator);
-  }
-
-  /**
-   * @return the secondaryIncomeType
-   */
-  public Short getSecondaryIncomeType() {
-    return secondaryIncomeType;
-  }
-
-  /**
-   * @return the socialSecurityNumber
-   */
-  public String getSocialSecurityNumber() {
-    return StringUtils.trimToEmpty(socialSecurityNumber);
-  }
-
-  /**
-   * @return the stateCodeType
-   */
-  public Short getStateCodeType() {
-    return stateCodeType;
-  }
-
-  /**
-   * @return the streetName
-   */
-  public String getStreetName() {
-    return StringUtils.trimToEmpty(streetName);
-  }
-
-  /**
-   * @return the streetNumber
-   */
-  public String getStreetNumber() {
-    return StringUtils.trimToEmpty(streetNumber);
-  }
-
-  /**
-   * @return the suffixTitleDescription
-   */
-  public String getSuffixTitleDescription() {
-    return StringUtils.trimToEmpty(suffixTitleDescription);
-  }
-
-  /**
-   * @return the zipNumber
-   */
-  public Integer getZipNumber() {
-    return zipNumber;
-  }
-
-  /**
-   * @return the zipSuffixNumber
-   */
-  public Short getZipSuffixNumber() {
-    return zipSuffixNumber;
-  }
-
-  /**
-   * {@inheritDoc}
-   * 
    * @see java.lang.Object#hashCode()
    */
   @Override
@@ -610,30 +211,6 @@ public class SubstituteCareProvider extends CmsPersistentObject implements ApiPe
     result = prime * result
         + ((super.getLastUpdatedTime() == null) ? 0 : super.getLastUpdatedTime().hashCode());
     return result;
-  }
-
-  @JsonIgnore
-  @Override
-  public String getMiddleName() {
-    return this.getMiddleInitialName();
-  }
-
-  @JsonIgnore
-  @Override
-  public String getGender() {
-    return this.getGenderIndicator();
-  }
-
-  @JsonIgnore
-  @Override
-  public String getSsn() {
-    return this.getSocialSecurityNumber();
-  }
-
-  @JsonIgnore
-  @Override
-  public String getNameSuffix() {
-    return this.getSuffixTitleDescription();
   }
 
   /**
