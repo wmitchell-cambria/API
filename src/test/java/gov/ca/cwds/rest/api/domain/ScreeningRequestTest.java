79898e1aa1cdfeddc706d0e0e4e0e2df51839ed3
jimlebeau <James.LeBeau@osi.ca.gov>
2017-02-22 13:51:40 -0800

    junits for Screening classes

--- src/test/java/gov/ca/cwds/rest/api/domain/ScreeningRequestTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/ScreeningRequestTest.java
@@ -4,19 +4,21 @@ import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
-import gov.ca.cwds.rest.resources.ScreeningResource;
-import gov.ca.cwds.rest.resources.cms.JerseyGuiceRule;
-import io.dropwizard.jackson.Jackson;
-import io.dropwizard.testing.junit.ResourceTestRule;
-import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
 
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
+
+import gov.ca.cwds.rest.resources.ScreeningResource;
+import gov.ca.cwds.rest.resources.cms.JerseyGuiceRule;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 
 /**
  * @author CWDS API Team
@@ -38,8 +40,26 @@ public class ScreeningRequestTest {
   public static JerseyGuiceRule rule = new JerseyGuiceRule();
 
   @ClassRule
-  public static final ResourceTestRule resources = ResourceTestRule.builder().addResource(resource)
-      .build();
+  public static final ResourceTestRule resources =
+      ResourceTestRule.builder().addResource(resource).build();
+
+  /*
+   * constructor test
+   * 
+   */
+  @Test
+  public void testConstructor() throws Exception {
+
+    Address address = new Address("10 main st", "Sacramento", "CA", 95814, null);
+    ScreeningRequest screeningRequest = new ScreeningRequest("X5HNJK", "1973-11-22", "Amador",
+        "1973-11-22", "Home", "email", "First screening", "immediate", "accept_for_investigation",
+        "2016-10-11", "first narrative", address);
+
+    ScreeningRequest serialized = MAPPER.readValue(
+        fixture("fixtures/domain/screeningRequest/valid/valid.json"), ScreeningRequest.class);
+
+    assertThat(screeningRequest, is(serialized));
+  }
 
   /*
    * Serialization and deserialization
@@ -50,15 +70,13 @@ public class ScreeningRequestTest {
   @Test
   public void serializesToJSON() throws Exception {
     Address address = new Address("10 main st", "Sacramento", "CA", 95814, null);
-    ScreeningRequest screeningRequest =
-        new ScreeningRequest("X5HNJK", "1973-11-22", "Amador", "1973-11-22", "Home", "email",
-            "First screening", "immediate", "accept_for_investigation", "2016-10-11",
-            "first narrative", address);
+    ScreeningRequest screeningRequest = new ScreeningRequest("X5HNJK", "1973-11-22", "Amador",
+        "1973-11-22", "Home", "email", "First screening", "immediate", "accept_for_investigation",
+        "2016-10-11", "first narrative", address);
 
     String expected = MAPPER.writeValueAsString(screeningRequest);
-    String serialized =
-        MAPPER.writeValueAsString(MAPPER.readValue(
-            fixture("fixtures/domain/screeningRequest/valid/valid.json"), ScreeningRequest.class));
+    String serialized = MAPPER.writeValueAsString(MAPPER.readValue(
+        fixture("fixtures/domain/screeningRequest/valid/valid.json"), ScreeningRequest.class));
 
     assertThat(serialized, is(expected));
   }
@@ -69,13 +87,13 @@ public class ScreeningRequestTest {
   @Test
   public void deserializesFromJSON() throws Exception {
     Address address = new Address("10 main st", "Sacramento", "CA", 95814, null);
-    ScreeningRequest expected =
-        new ScreeningRequest("X5HNJK", "11/22/1973", "Amador", "11/22/1973", "Home", "email",
-            "First screening", "immediate", "accept_for_investigation", "10/11/2016",
-            "first narrative", address);
-    ScreeningRequest serialized =
-        MAPPER.readValue(fixture("fixtures/domain/screeningRequest/valid/valid.json"),
-            ScreeningRequest.class);
+    @SuppressWarnings("unused")
+    ImmutableList.Builder<Long> builder = ImmutableList.builder();
+    ScreeningRequest expected = new ScreeningRequest("X5HNJK", "11/22/1973", "Amador", "11/22/1973",
+        "Home", "email", "First screening", "immediate", "accept_for_investigation", "10/11/2016",
+        "first narrative", address);
+    ScreeningRequest serialized = MAPPER.readValue(
+        fixture("fixtures/domain/screeningRequest/valid/valid.json"), ScreeningRequest.class);
     assertThat(serialized, is(expected));
   }
 
