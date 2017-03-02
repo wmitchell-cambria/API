68e2a42d58d99976e6968f6b84126448aa8286ac
David R. Smith <drs.code.monkey@gmail.com>
2017-03-02 11:46:36 -0800

    #138438305: Attorney batch job

--- src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedAttorney.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedAttorney.java
@@ -23,16 +23,29 @@ import gov.ca.cwds.data.persistence.cms.BaseAttorney;
  * 
  * @author CWDS API Team
  */
-@NamedNativeQueries({@NamedNativeQuery(
-    name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedAttorney.findAllUpdatedAfter",
-    query = "select z.IDENTIFIER, z.CITY_NM, z.CWATRY_IND, z.FAX_NO, z.FIRST_NM, "
-        + "z.GVR_ENTC, z.LANG_TPC, z.LAST_NM, z.MSG_EXT_NO, z.MSG_TEL_NO, "
-        + "z.MID_INI_NM, z.NMPRFX_DSC, z.POSTIL_DSC, z.PRM_EXT_NO, z.PRM_TEL_NO, "
-        + "z.STATE_C, z.STREET_NM, z.STREET_NO, z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, "
-        + "z.LST_UPD_TS, z.BUSNSS_NM, z.ZIP_SFX_NO, z.END_DT, z.ARCASS_IND, z.EMAIL_ADDR "
-        + ", z.IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER "
-        + "from {h-schema}ATTRNY_T z WHERE z.IBMSNAP_LOGMARKER >= :after FOR READ ONLY",
-    resultClass = ReplicatedAttorney.class)})
+@NamedNativeQueries({
+    @NamedNativeQuery(
+        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedAttorney.findAllUpdatedAfter",
+        query = "select z.IDENTIFIER, z.CITY_NM, z.CWATRY_IND, z.FAX_NO, z.FIRST_NM, "
+            + "z.GVR_ENTC, z.LANG_TPC, z.LAST_NM, z.MSG_EXT_NO, z.MSG_TEL_NO, "
+            + "z.MID_INI_NM, z.NMPRFX_DSC, z.POSTIL_DSC, z.PRM_EXT_NO, z.PRM_TEL_NO, "
+            + "z.STATE_C, z.STREET_NM, z.STREET_NO, z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, "
+            + "z.LST_UPD_TS, z.BUSNSS_NM, z.ZIP_SFX_NO, z.END_DT, z.ARCASS_IND, z.EMAIL_ADDR "
+            + ", z.IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER "
+            + "from {h-schema}ATTRNY_T z WHERE z.IBMSNAP_LOGMARKER >= :after FOR READ ONLY",
+        resultClass = ReplicatedAttorney.class),
+    @NamedNativeQuery(
+        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedAttorney.findAllByBucket",
+        query = "select z.IDENTIFIER, z.CITY_NM, z.CWATRY_IND, z.FAX_NO, z.FIRST_NM, "
+            + "z.GVR_ENTC, z.LANG_TPC, z.LAST_NM, z.MSG_EXT_NO, z.MSG_TEL_NO, "
+            + "z.MID_INI_NM, z.NMPRFX_DSC, z.POSTIL_DSC, z.PRM_EXT_NO, z.PRM_TEL_NO, "
+            + "z.STATE_C, z.STREET_NM, z.STREET_NO, z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, "
+            + "z.LST_UPD_TS, z.BUSNSS_NM, z.ZIP_SFX_NO, z.END_DT, z.ARCASS_IND, z.EMAIL_ADDR "
+            + ", z.IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER "
+            + "from ( select mod(y.rn, CAST(:total_buckets AS INTEGER)) + 1 as bucket, y.* "
+            + "from ( select row_number() over (order by 1) as rn, x.* "
+            + "from {h-schema}ATTRNY_T x ) y ) z where z.bucket = :bucket_num for read only",
+        resultClass = ReplicatedAttorney.class)})
 @Entity
 @Table(name = "ATTRNY_T")
 @JsonPropertyOrder(alphabetic = true)
