3c7b4e686ffe04a2d35103eb20de320546448114
David R. Smith <drs.code.monkey@gmail.com>
2017-03-03 10:09:06 -0800

    #140456485: hibernate distinct with native query

--- src/main/java/gov/ca/cwds/data/persistence/cms/ClientAddress.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/ClientAddress.java
@@ -1,13 +1,10 @@
 package gov.ca.cwds.data.persistence.cms;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -31,9 +28,10 @@ public final class ClientAddress extends BaseClientAddress {
    */
   private static final long serialVersionUID = 1L;
 
-  @OneToMany(fetch = FetchType.EAGER)
+  // @Transient
+  @OneToOne(fetch = FetchType.EAGER)
   @JoinColumns({@JoinColumn(name = "IDENTIFIER", referencedColumnName = "FKADDRS_T")})
-  private Set<Address> addresses = new LinkedHashSet<>();
+  private Address address;
 
   /**
    * Default constructor
@@ -44,16 +42,12 @@ public final class ClientAddress extends BaseClientAddress {
     super();
   }
 
-  public Set<Address> getAddresses() {
-    return addresses;
+  public Address getAddress() {
+    return address;
   }
 
-  public void setAddresses(Set<Address> addresses) {
-    this.addresses = addresses;
-  }
-
-  public void addAddress(final Address address) {
-    this.addresses.add(address);
+  public void setAddress(Address address) {
+    this.address = address;
   }
 
 }
