a996871a6096aec314302ef96e6581d8d11c628c
mysorepr <prasad.mysore@osi.ca.gov>
2017-03-06 15:40:10 -0800

    #140098157 junits for client service and resource

---
+++ src/test/java/gov/ca/cwds/rest/resources/cms/ClientResourceTest.java
@@ -0,0 +1,80 @@
+package gov.ca.cwds.rest.resources.cms;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.core.Response;
+
+import org.hamcrest.junit.ExpectedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import gov.ca.cwds.rest.api.domain.cms.Client;
+import gov.ca.cwds.rest.resources.ServiceBackedResourceDelegate;
+import gov.ca.cwds.rest.resources.TypedResourceDelegate;
+import gov.ca.cwds.rest.services.cms.ClientService;
+
+/**
+ * NOTE : The CWDS API Team has taken the pattern of delegating Resource functions to
+ * {@link ServiceBackedResourceDelegate}. As such the tests in here reflect that assumption.
+ * 
+ * @author CWDS API Team
+ */
+public class ClientResourceTest {
+
+  private static final String FOUND_RESOURCE = "/_client/abc";
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Mock
+  private ClientService service;
+
+  @Mock
+  // private gov.ca.cwds.data.persistence.cms.Client persClient;
+  private gov.ca.cwds.data.std.ApiMultipleAddressesAware persClient;
+
+
+  @Mock
+  private Client domainClient;
+
+  @Mock
+  private TypedResourceDelegate<String, Client> resourceDelegate;
+
+  @InjectMocks
+  @Spy
+  private ClientResource target; // "Class Under Test"
+
+  @Before
+  public void initMocks() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void type() throws Exception {
+    assertThat(ClientResource.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void get_A$String() throws Exception {
+    final String id = "1234";
+    when(resourceDelegate.get(any())).thenReturn(null);
+    Response actual = target.get(id);
+    Response expected = null;
+    assertTrue(actual == null);
+  }
+
+}
---
+++ src/test/java/gov/ca/cwds/rest/services/cms/ClientServiceTest.java
@@ -0,0 +1,59 @@
+package gov.ca.cwds.rest.services.cms;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.mockito.Mockito.mock;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.ca.cwds.data.cms.ClientDao;
+import gov.ca.cwds.rest.api.domain.cms.Client;
+import io.dropwizard.jackson.Jackson;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public class ClientServiceTest {
+
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+  private ClientService clientService;
+  private ClientDao clientDao;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setup() throws Exception {
+    clientDao = mock(ClientDao.class);
+    clientService = new ClientService(clientDao);
+  }
+
+  @Test
+  public void createThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    Client clientDomain =
+        MAPPER.readValue(fixture("fixtures/domain/cms/Client/valid/valid.json"), Client.class);
+    clientService.create(clientDomain);
+  }
+
+  @Test
+  public void updateThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    Client clientDomain =
+        MAPPER.readValue(fixture("fixtures/domain/cms/Client/valid/valid.json"), Client.class);
+    clientService.update("testkey", clientDomain);
+  }
+
+  @Test
+  public void deleteThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    clientService.delete("testkey");
+  }
+
+}
