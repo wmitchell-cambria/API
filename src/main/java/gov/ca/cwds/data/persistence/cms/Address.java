0faa43d27114b6eef58ae134f579dc8c23a3faa8
David R. Smith <drs.code.monkey@gmail.com>
2017-03-08 09:04:12 -0800

    #140456485: separate client address from replicated version

--- src/main/java/gov/ca/cwds/data/persistence/cms/Address.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/Address.java
@@ -1,6 +1,8 @@
 package gov.ca.cwds.data.persistence.cms;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -18,8 +20,19 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public final class Address extends BaseAddress {
 
+  @OneToOne(fetch = FetchType.EAGER, mappedBy = "address")
+  private ReplicatedClientAddress clientAddress;
+
   public Address() {
     super();
+  }
+
+  public ReplicatedClientAddress getClientAddress() {
+    return clientAddress;
+  }
+
+  public void setClientAddress(ReplicatedClientAddress clientAddress) {
+    this.clientAddress = clientAddress;
   }
 
 }
--- src/main/java/gov/ca/cwds/data/persistence/cms/ReplicatedClientAddress.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/ReplicatedClientAddress.java
@@ -1,6 +1,9 @@
 package gov.ca.cwds.data.persistence.cms;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -13,36 +16,33 @@ import gov.ca.cwds.data.persistence.PersistentObject;
  * 
  * @author CWDS API Team
  */
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "CL_ADDRT")
 @JsonPropertyOrder(alphabetic = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public final class ClientAddress extends BaseClientAddress {
+public final class ReplicatedClientAddress extends BaseClientAddress {
 
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
-
-  // @OneToOne(fetch = FetchType.LAZY)
-  // @JoinColumns({@JoinColumn(name = "IDENTIFIER", referencedColumnName = "FKADDRS_T")})
-  // private Address address;
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "FKADDRS_T", referencedColumnName = "IDENTIFIER", insertable = false,
+      updatable = false)
+  protected Address address;
 
   /**
    * Default constructor
    * 
    * Required for Hibernate
    */
-  public ClientAddress() {
+  public ReplicatedClientAddress() {
     super();
   }
 
-  // public Address getAddress() {
-  // return address;
-  // }
-  //
-  // public void setAddress(Address address) {
-  // this.address = address;
-  // }
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
 
 }
