fbc1221cc5c13f40bbf7f0f6fee780cf71d04aea
jimlebeau <James.LeBeau@osi.ca.gov>
2017-03-13 08:16:20 -0700

    #140452203 - Highlight matched data from search query

--- src/test/java/gov/ca/cwds/rest/api/domain/es/AutoCompletePersonTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/es/AutoCompletePersonTest.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 
 import gov.ca.cwds.data.es.ElasticSearchPerson;
+import gov.ca.cwds.data.std.ApiAddressAware;
 import gov.ca.cwds.rest.resources.cms.JerseyGuiceRule;
 import io.dropwizard.jackson.Jackson;
 
@@ -18,6 +19,7 @@ import io.dropwizard.jackson.Jackson;
  * @author CWDS API Team
  *
  */
+@SuppressWarnings("javadoc")
 public class AutoCompletePersonTest {
 
   String id = "1234567ABC";
@@ -31,6 +33,14 @@ public class AutoCompletePersonTest {
       "{\"adjudicatedDelinquentIndicator\":\"\",\"adoptionStatusCode\":\"N\",\"alienRegistrationNumber\":\"\",\"birthCity\":\"\",\"birthCountryCodeType\":0,\"birthDate\":\"1953-03-12\",\"birthFacilityName\":\"\",\"birthStateCodeType\":0,\"birthplaceVerifiedIndicator\":\"N\",\"childClientIndicatorVar\":\"N\",\"clientIndexNumber\":\"\",\"commentDescription\":\"\",\"commonFirstName\":\"Louis\",\"commonLastName\":\"Smith\",\"commonMiddleName\":\"\",\"confidentialityActionDate\":null,\"confidentialityInEffectIndicator\":\"N\",\"creationDate\":\"2003-03-12\",\"currCaChildrenServIndicator\":\"N\",\"currentlyOtherDescription\":\"\",\"currentlyRegionalCenterIndicator\":\"N\",\"deathDate\":null,\"deathDateVerifiedIndicator\":\"N\",\"deathPlace\":\"\",\"deathReasonText\":\"\",\"driverLicenseNumber\":\"\",\"driverLicenseStateCodeType\":0,\"emailAddress\":\"\",\"estimatedDobCode\":\"Y\",\"ethUnableToDetReasonCode\":\"\",\"fatherParentalRightTermDate\":null,\"genderCode\":\"M\",\"healthSummaryText\":\"\",\"hispUnableToDetReasonCode\":\"\",\"hispanicOriginCode\":\"U\",\"id\":\"LDSPRcl0I3\",\"immigrationCountryCodeType\":0,\"immigrationStatusType\":0,\"incapacitatedParentCode\":\"U\",\"individualHealthCarePlanIndicator\":\"N\",\"lastUpdatedId\":\"0I3\",\"lastUpdatedTime\":1047479557134,\"limitationOnScpHealthIndicator\":\"N\",\"literateCode\":\"U\",\"maritalCohabitatnHstryIndicatorVar\":\"N\",\"maritalStatusType\":0,\"militaryStatusCode\":\"U\",\"motherParentalRightTermDate\":null,\"namePrefixDescription\":\"\",\"nameType\":{\"sys_id\":1313,\"short_description\":\"Legal\",\"logical_id\":\"0003\"},\"outstandingWarrantIndicator\":\"N\",\"prevCaChildrenServIndicator\":\"N\",\"prevOtherDescription\":\"\",\"prevRegionalCenterIndicator\":\"N\",\"primaryEthnicityType\":{\"sys_id\":839,\"short_description\":\"White*\",\"logical_id\":\"01\"},\"primaryKey\":\"LDSPRcl0I3\",\"primaryLanguageType\":{\"sys_id\":1253,\"short_description\":\"English\",\"logical_id\":\"07\"},\"religionType\":0,\"replicationDate\":1047479557134,\"replicationOperation\":\"U\",\"secondaryLanguageType\":0,\"sensitiveHlthInfoOnFileIndicator\":\"N\",\"sensitivityIndicator\":\"N\",\"soc158PlacementCode\":\"N\",\"soc158SealedClientIndicator\":\"N\",\"socialSecurityNumChangedCode\":\"N\",\"socialSecurityNumber\":\"\",\"suffixTitleDescription\":\"\",\"tribalAncestryClientIndicatorVar\":\"N\",\"tribalMembrshpVerifctnIndicatorVar\":\"N\",\"unemployedParentCode\":\"U\",\"zippyCreatedIndicator\":\"Y\"}";
   String highlight = "{\"firstName\":\"<em>mik</em>e\", \"lastName\":\"sm<em>ith</em>\"}";
 
+  String city = "San Jose";
+  String county = "Santa Clara";
+  String state = "CA";
+  String address = "123 first street";
+  String zip = "98765";
+
+  String testGet = "test get";
+
 
   private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
@@ -38,6 +48,7 @@ public class AutoCompletePersonTest {
   public ExpectedException thrown = ExpectedException.none();
 
 
+  @SuppressWarnings("javadoc")
   @After
   public void ensureServiceLocatorPopulated() {
     JerseyGuiceUtils.reset();
@@ -49,6 +60,41 @@ public class AutoCompletePersonTest {
   @Before
   public void setup() {}
 
+  // @Test
+  // public void testAutoCompletePersonAddressConstuctor() {
+  // ApiAddressAware add = new Address(city, county, state, address, zip);
+  //
+  // AutoCompletePerson.AutoCompletePersonAddress acpa =
+  // new AutoCompletePerson.AutoCompletePersonAddress(add);
+  // assertThat(acpa, notNullValue());
+  // }
+  //
+  // @Test
+  // public void testAutoCompletePersonAddressGetters() {
+  // ApiAddressAware add = new Address(city, county, state, address, zip);
+  //
+  // AutoCompletePerson.AutoCompletePersonAddress acpa =
+  // new AutoCompletePerson.AutoCompletePersonAddress(add);
+  //
+  // assertThat(acpa.getCity(), is(equalTo(city)));
+  // assertThat(acpa.getCounty(), is(equalTo(county)));
+  // assertThat(acpa.getState(), is(equalTo(state)));
+  // assertThat(acpa.getAddressType(), is(equalTo(address)));
+  // assertThat(acpa.getZip(), is(equalTo(zip)));
+  //
+  // }
+  //
+  // public void testAutoCompletePersonAddressSetters() {
+  // ApiAddressAware add = new Address(city, county, state, address, zip);
+  //
+  // AutoCompletePerson.AutoCompletePersonAddress acpa =
+  // new AutoCompletePerson.AutoCompletePersonAddress(add);
+  //
+  // acpa.setCity(testGet);
+  // assertThat(acpa.getCity(), is(equalTo(testGet)));
+  // acpa.setStreetAddress(testGet);
+  //
+  // }
   // @Test
   // public void testConstuctorSuccess() {
   //
@@ -145,5 +191,53 @@ public class AutoCompletePersonTest {
     // System.out.println("json = " + json);
     //
     return esp;
+  }
+
+  public static final class Address implements ApiAddressAware {
+
+    private String city;
+    private String county;
+    private String state;
+    private String streetAddress;
+    private String zip;
+
+    public Address(String city, String county, String state, String address, String zip) {
+      this.city = city;
+      this.county = county;
+      this.state = state;
+      this.streetAddress = address;
+      this.zip = zip;
+    }
+
+    @Override
+    public String getCity() {
+      // TODO Auto-generated method stub
+      return this.city;
+    }
+
+    @Override
+    public String getCounty() {
+      // TODO Auto-generated method stub
+      return this.county;
+    }
+
+    @Override
+    public String getState() {
+      // TODO Auto-generated method stub
+      return this.state;
+    }
+
+    @Override
+    public String getStreetAddress() {
+      // TODO Auto-generated method stub
+      return this.streetAddress;
+    }
+
+    @Override
+    public String getZip() {
+      // TODO Auto-generated method stub
+      return this.zip;
+    }
+
   }
 }
