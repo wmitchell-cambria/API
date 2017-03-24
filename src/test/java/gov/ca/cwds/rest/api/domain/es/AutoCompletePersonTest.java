1162ee5d3908ad72a83ee1bbece7aa9fa5f4d7ba
David R. Smith <drs.code.monkey@gmail.com>
2017-03-24 15:28:52 -0700

    #141437175: person search JSON

--- src/test/java/gov/ca/cwds/rest/api/domain/es/AutoCompletePersonTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/es/AutoCompletePersonTest.java
@@ -136,8 +136,8 @@ public class AutoCompletePersonTest {
 
   private ElasticSearchPerson validESP() {
 
-    ElasticSearchPerson esp = new ElasticSearchPerson(id, firstName, lastName, gender, birthDate,
-        ssn, sourceType, sourceJson, highlight);
+    ElasticSearchPerson esp = new ElasticSearchPerson(id, firstName, lastName, null, null,
+        gender, birthDate, ssn, sourceType, sourceJson, highlight, null, null, null);
 
     final String json = esp.getHighlightFields().replaceAll("\\s+\",", "\",");
     //
