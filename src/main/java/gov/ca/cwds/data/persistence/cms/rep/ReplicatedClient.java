f45579ada19a4c7ebae1d15a272bb591aa8cfa6f
David R. Smith <drs.code.monkey@gmail.com>
2017-03-27 12:49:40 -0700

    #141437175: person search JSON

--- src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedClient.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedClient.java
@@ -84,8 +84,8 @@ import gov.ca.cwds.data.std.ApiPhoneAware;
     @NamedNativeQuery(
         name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedClient.findPartitionedBuckets",
         query = "select {a.*}, {b.*}, {c.*}  from {h-schema}CLIENT_T a "
-            + "JOIN {h-schema}CL_ADDRT b ON a.IDENTIFIER = b.FKCLIENT_T and b.EFF_END_DT is null "
-            + "JOIN {h-schema}ADDRS_T c ON b.FKADDRS_T = c.IDENTIFIER "
+            + "LEFT OUTER JOIN {h-schema}CL_ADDRT b ON a.IDENTIFIER = b.FKCLIENT_T and b.EFF_END_DT is null "
+            + "LEFT OUTER JOIN {h-schema}ADDRS_T c ON b.FKADDRS_T = c.IDENTIFIER "
             + "where a.SOC158_IND ='N' and a.SENSTV_IND = 'N' "
             + "AND a.IDENTIFIER >= :min_id and a.IDENTIFIER < :max_id for read only",
         resultClass = ReplicatedClient.class, readOnly = true,
