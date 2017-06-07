a051c73969bb774515efadb5d76b94a9ce34fdfa
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2017-06-07 10:28:20 -0700

    https://www.pivotaltracker.com/story/show/146181701 BUSINESS RULE: "R - 05360” - StreetAddress is set City is required and JavaDoc

--- src/main/java/gov/ca/cwds/rest/messages/MessageBuilder.java
+++ src/main/java/gov/ca/cwds/rest/messages/MessageBuilder.java
@@ -1,42 +1,62 @@
 package gov.ca.cwds.rest.messages;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
 import gov.ca.cwds.rest.api.domain.DomainObject;
 import gov.ca.cwds.rest.api.domain.error.ErrorMessage;
 import gov.ca.cwds.rest.api.domain.error.ErrorMessage.ErrorType;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import javax.validation.ConstraintViolation;
 
+/**
+ * @author CWDS API Team
+ *
+ */
 public class MessageBuilder {
   ArrayList<ErrorMessage> messages;
 
-  public MessageBuilder(){
+  /**
+   * messageBuilder - messageBuilder
+   */
+  public MessageBuilder() {
     messages = new ArrayList<>();
   }
 
+  /**
+   * @param message - message
+   */
   public void addError(String message) {
     messages.add(new ErrorMessage(ErrorMessage.ErrorType.VALIDATION, message, ""));
   }
 
+  /**
+   * @param message - message
+   * @param type - type
+   */
   public void addError(String message, ErrorType type) {
     messages.add(new ErrorMessage(type, message, ""));
   }
 
-  public void addDomainValidationError( Set<ConstraintViolation<DomainObject>> errors) {
+  /**
+   * @param errors - errors
+   */
+  public void addDomainValidationError(Set<ConstraintViolation<DomainObject>> errors) {
     if (!errors.isEmpty()) {
       errors.forEach(error -> {
         final String message = error.getMessage();
-//            " getRootBean: " + error.getRootBean() + " getLeafBean: "
-//            + error.getLeafBean() + " getConstraintDescriptor: " + error.getConstraintDescriptor()
-//            + " ERROR PROP PATH: " + error.getPropertyPath() + " " + error.getMessage();
+        // " getRootBean: " + error.getRootBean() + " getLeafBean: "
+        // + error.getLeafBean() + " getConstraintDescriptor: " + error.getConstraintDescriptor()
+        // + " ERROR PROP PATH: " + error.getPropertyPath() + " " + error.getMessage();
         messages.add(new ErrorMessage(ErrorMessage.ErrorType.VALIDATION, message, ""));
       });
     }
   }
 
-  public ArrayList<ErrorMessage> getMessages(){
-    return messages == null ? new ArrayList() : messages;
+  /**
+   * @return the error message
+   */
+  public ArrayList<ErrorMessage> getMessages() {
+    return messages == null ? new ArrayList<>() : messages;
   }
 }
