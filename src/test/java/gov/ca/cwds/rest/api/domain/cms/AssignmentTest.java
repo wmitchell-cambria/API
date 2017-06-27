d05cc1f63e3d1e3ae053c277749f86aefcbf722b
jimlebeau <James.LeBeau@osi.ca.gov>
2017-06-27 08:02:07 -0700

    #146713651 - BARNEY: Referrals require a default assignment

--- src/test/java/gov/ca/cwds/rest/api/domain/cms/AssignmentTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/cms/AssignmentTest.java
@@ -12,6 +12,8 @@ import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.junit.After;
@@ -96,16 +98,16 @@ public class AssignmentTest {
         Assignment.class), is(equalTo(validAssignment())));
   }
 
-  // @SuppressWarnings("javadoc")
-  // @Test
-  // public void testWithValidSuccess() throws Exception {
-  // Assignment toCreate = MAPPER
-  // .readValue(fixture("fixtures/domain/legacy/Assignment/valid/valid.json"), Assignment.class);
-  // Response response =
-  // resources.client().target(ROOT_RESOURCE).request().accept(MediaType.APPLICATION_JSON)
-  // .post(Entity.entity(toCreate, MediaType.APPLICATION_JSON));
-  // assertThat(response.getStatus(), is(equalTo(204)));
-  // }
+  @SuppressWarnings("javadoc")
+  @Test
+  public void testWithValidSuccess() throws Exception {
+    Assignment toCreate = MAPPER
+        .readValue(fixture("fixtures/domain/legacy/Assignment/valid/valid.json"), Assignment.class);
+    Response response =
+        resources.client().target(ROOT_RESOURCE).request().accept(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(toCreate, MediaType.APPLICATION_JSON));
+    assertThat(response.getStatus(), is(equalTo(204)));
+  }
 
   @SuppressWarnings("javadoc")
   @Test
