6b7b396669850de3d702365937b00d3edd421126
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2017-07-19 09:44:12 -0700

    Removed the Mapper an replaced with Builders and added few more test cases

--- src/test/java/gov/ca/cwds/rest/business/rules/NonLACountyTriggersTest.java
+++ src/test/java/gov/ca/cwds/rest/business/rules/NonLACountyTriggersTest.java
@@ -1,6 +1,5 @@
 package gov.ca.cwds.rest.business.rules;
 
-import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -16,17 +15,16 @@ import org.junit.rules.ExpectedException;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import gov.ca.cwds.data.cms.CountyOwnershipDao;
 import gov.ca.cwds.data.cms.ReferralClientDao;
 import gov.ca.cwds.data.cms.ReferralDao;
 import gov.ca.cwds.data.persistence.cms.Client;
 import gov.ca.cwds.data.persistence.cms.ClientAddress;
 import gov.ca.cwds.data.persistence.cms.CountyOwnership;
-import gov.ca.cwds.data.persistence.cms.SystemCodeTestHarness;
+import gov.ca.cwds.data.rules.TriggerTableException;
 import gov.ca.cwds.fixture.AssignmentResourceBuilder;
 import gov.ca.cwds.fixture.ClientAddressResourceBuilder;
+import gov.ca.cwds.fixture.ClientResourceBuilder;
 import gov.ca.cwds.fixture.ReferralClientResourceBuilder;
 import gov.ca.cwds.fixture.ReferralResourceBuilder;
 import gov.ca.cwds.rest.api.domain.cms.Assignment;
@@ -38,8 +36,6 @@ import gov.ca.cwds.rest.api.domain.cms.ReferralClient;
  *
  */
 public class NonLACountyTriggersTest {
-
-  private static final ObjectMapper MAPPER = SystemCodeTestHarness.MAPPER;
 
   private CountyOwnershipDao countyOwnershipDao;
   private ReferralDao referralDao;
@@ -65,19 +61,19 @@ public class NonLACountyTriggersTest {
     countyOwnership = null;
   }
 
-  /*
+  /**
    * Test for checking the referral Client CountyOnwership created or updated through the PUT method
+   * 
+   * @throws Exception - TriggerTableException
    */
-  @SuppressWarnings("javadoc")
   @Test
   public void testChecksReferralClientCreatedCountyOwnership() throws Exception {
-    ReferralClient referralClientDomain = MAPPER.readValue(
-        fixture("fixtures/legacy/business/rules/nonLaCountyTrigger/referralClientValid.json"),
-        ReferralClient.class);
+    ReferralClient referralClientDomain =
+        new ReferralClientResourceBuilder().setCountySpecificCode("54").buildReferralClient();
+
     gov.ca.cwds.data.persistence.cms.ReferralClient toCreate =
         new gov.ca.cwds.data.persistence.cms.ReferralClient(referralClientDomain, "ABC");
 
-    // ReferralClient request = new ReferralClient(toCreate);
     when(countyOwnershipDao.find(any(String.class))).thenReturn(null);
     when(countyOwnershipDao.create(any(CountyOwnership.class)))
         .thenAnswer(new Answer<CountyOwnership>() {
@@ -95,19 +91,48 @@ public class NonLACountyTriggersTest {
     assertThat(countyOwnership.getEntityCode(), is(equalTo("C")));
   }
 
-  /*
-   * Test for checking the referral Client CountyOnwership updated
+  /**
+   * Test for checking the referral Client CountyOnwership create throws exception
+   * 
+   * @throws Exception - TriggerTableException
    */
-  @SuppressWarnings("javadoc")
-  @Test
-  public void testForReferralClientUpdatedCountyOwnership() throws Exception {
-    ReferralClient referralClientDomain = MAPPER.readValue(
-        fixture("fixtures/legacy/business/rules/nonLaCountyTrigger/referralClientValid.json"),
-        ReferralClient.class);
+  @Test(expected = TriggerTableException.class)
+  public void testChecksReferralClientCreatedCountyOwnershipThrowsTriggerTableException()
+      throws Exception {
+    ReferralClient referralClientDomain =
+        new ReferralClientResourceBuilder().setCountySpecificCode("100").buildReferralClient();
+
     gov.ca.cwds.data.persistence.cms.ReferralClient toCreate =
         new gov.ca.cwds.data.persistence.cms.ReferralClient(referralClientDomain, "ABC");
 
-    // ReferralClient request = new ReferralClient(toCreate);
+    when(countyOwnershipDao.find(any(String.class))).thenReturn(null);
+    when(countyOwnershipDao.create(any(CountyOwnership.class)))
+        .thenAnswer(new Answer<CountyOwnership>() {
+
+          @Override
+          public CountyOwnership answer(InvocationOnMock invocation) throws Throwable {
+
+            CountyOwnership report = (CountyOwnership) invocation.getArguments()[0];
+            countyOwnership = report;
+            return report;
+          }
+        });
+    nonLaCountyTriggers.createAndUpdateReferralClientCoutyOwnership(toCreate);
+  }
+
+  /**
+   * Test for checking the referral Client CountyOnwership updated
+   * 
+   * @throws Exception - TriggerTableException
+   */
+  @Test
+  public void testForReferralClientUpdatedCountyOwnership() throws Exception {
+    ReferralClient referralClientDomain =
+        new ReferralClientResourceBuilder().setCountySpecificCode("62").buildReferralClient();
+
+    gov.ca.cwds.data.persistence.cms.ReferralClient toCreate =
+        new gov.ca.cwds.data.persistence.cms.ReferralClient(referralClientDomain, "ABC");
+
     when(countyOwnershipDao.find(any(String.class))).thenReturn(new CountyOwnership());
     when(countyOwnershipDao.update(any(CountyOwnership.class)))
         .thenAnswer(new Answer<CountyOwnership>() {
@@ -125,15 +150,15 @@ public class NonLACountyTriggersTest {
     assertThat(countyOwnership.getCounty62Flag(), is(equalTo("Y")));
   }
 
-  /*
+  /**
    * Test for checking the Client CountyOnwership created
+   * 
+   * @throws Exception - TriggerTableException
    */
-  @SuppressWarnings("javadoc")
   @Test
   public void testForClientCreatedCountyOwnership() throws Exception {
-    gov.ca.cwds.rest.api.domain.cms.Client ClientDomain = MAPPER.readValue(
-        fixture("fixtures/legacy/business/rules/nonLaCountyTrigger/clientValid.json"),
-        gov.ca.cwds.rest.api.domain.cms.Client.class);
+    gov.ca.cwds.rest.api.domain.cms.Client ClientDomain = new ClientResourceBuilder().build();
+
     Client toCreate = new Client("ABC1234567", ClientDomain, "ABC");
     when(countyOwnershipDao.create(any(CountyOwnership.class)))
         .thenAnswer(new Answer<CountyOwnership>() {
@@ -154,7 +179,7 @@ public class NonLACountyTriggersTest {
   /**
    * Test for ClientAddress CountyOwnership created
    * 
-   * @throws Exception - exception
+   * @throws Exception - TriggerTableException
    */
   @Test
   public void testForClientAddressCreatedCountyOwnership() throws Exception {
@@ -186,10 +211,44 @@ public class NonLACountyTriggersTest {
     assertThat(countyOwnership.getCounty62Flag(), is(equalTo("Y")));
   }
 
+
+  /**
+   * Test for clientAddress CountyOwnership creation throws TriggerTableException
+   * 
+   * @throws Exception - TriggerTableException
+   */
+  @Test(expected = TriggerTableException.class)
+  public void testForClientAddressCreatedCountyOwnershipThrowsTriggerTableException()
+      throws Exception {
+
+    gov.ca.cwds.rest.api.domain.cms.ClientAddress clientAddressDomain =
+        new ClientAddressResourceBuilder().buildClientAddress();
+    ClientAddress toCreate = new ClientAddress("ABC1234567", clientAddressDomain, "ABC");
+
+    ReferralClient referralClientDomain =
+        new ReferralClientResourceBuilder().setCountySpecificCode("100").buildReferralClient();
+    gov.ca.cwds.data.persistence.cms.ReferralClient referralClient =
+        new gov.ca.cwds.data.persistence.cms.ReferralClient(referralClientDomain, "ABC");
+
+    when(referralClientDao.find(any(String.class))).thenReturn(referralClient);
+    when(countyOwnershipDao.create(any(CountyOwnership.class)))
+        .thenAnswer(new Answer<CountyOwnership>() {
+
+          @Override
+          public CountyOwnership answer(InvocationOnMock invocation) throws Throwable {
+
+            CountyOwnership report = (CountyOwnership) invocation.getArguments()[0];
+            countyOwnership = report;
+            return report;
+          }
+        });
+    nonLaCountyTriggers.createAndUpdateClientAddressCoutyOwnership(toCreate);
+  }
+
   /**
    * Test for updating the countyOwnership for the exisiting Address Id
    * 
-   * @throws Exception - exception
+   * @throws Exception - TriggerTableException
    */
   @Test
   public void testForClientAddressUpdatedCountyOwnership() throws Exception {
@@ -227,7 +286,7 @@ public class NonLACountyTriggersTest {
   /**
    * Test for Referral CountyOwnership created
    * 
-   * @throws Exception - exception
+   * @throws Exception - TriggerTableException
    */
   @Test
   public void testForReferralCreatedCountyOwnership() throws Exception {
@@ -260,9 +319,39 @@ public class NonLACountyTriggersTest {
   }
 
   /**
+   * @throws Exception - TriggerTableException
+   */
+  @Test(expected = TriggerTableException.class)
+  public void testForReferralCreatedCountyOwnershipThrowsTriggerTableException() throws Exception {
+
+    Assignment assignmentDomain = new AssignmentResourceBuilder().buildAssignment();
+    gov.ca.cwds.data.persistence.cms.Assignment toCreate =
+        new gov.ca.cwds.data.persistence.cms.Assignment("ABC1234567", assignmentDomain, "ABC");
+
+    Referral referralClientDomain =
+        new ReferralResourceBuilder().setCountySpecificCode("100").build();
+    gov.ca.cwds.data.persistence.cms.Referral referral =
+        new gov.ca.cwds.data.persistence.cms.Referral("ABC1234567", referralClientDomain, "0X5");
+
+    when(referralDao.find(any(String.class))).thenReturn(referral);
+    when(countyOwnershipDao.create(any(CountyOwnership.class)))
+        .thenAnswer(new Answer<CountyOwnership>() {
+
+          @Override
+          public CountyOwnership answer(InvocationOnMock invocation) throws Throwable {
+
+            CountyOwnership report = (CountyOwnership) invocation.getArguments()[0];
+            countyOwnership = report;
+            return report;
+          }
+        });
+    nonLaCountyTriggers.createAndUpdateReferralCoutyOwnership(toCreate);
+  }
+
+  /**
    * Test for updating the countyOwnership for the exisiting Referral Id
    * 
-   * @throws Exception - exception
+   * @throws Exception - TriggerTableException
    */
   @Test
   public void testForReferralUpdatedCountyOwnership() throws Exception {
