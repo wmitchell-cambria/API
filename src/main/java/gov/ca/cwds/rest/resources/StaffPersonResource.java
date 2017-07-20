99fbc1bc2d644e9705a99dbbc8d3687357bcf789
Mariam Ghori <mariam.ghori@osi.ca.gov>
2017-07-20 15:21:13 -0700

    # 146172991 E2E LINK - Display user's name and county and use user's Staff ID for saving referrals and #148802767 Refactor /_staffingPerson to return staff person information that is not part of authorization info.

---
+++ src/main/java/gov/ca/cwds/rest/resources/StaffPersonResource.java
@@ -0,0 +1,71 @@
+package gov.ca.cwds.rest.resources;
+
+import static gov.ca.cwds.rest.core.Api.RESOURCE_STAFF_PERSONS;
+import gov.ca.cwds.inject.StaffPersonsServiceBackedResource;
+import gov.ca.cwds.rest.api.domain.PostedStaffPerson;
+import gov.ca.cwds.rest.api.domain.StaffPerson;
+import io.dropwizard.hibernate.UnitOfWork;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.inject.Inject;
+
+/**
+ * A resource providing a RESTful interface for {@link StaffPerson}. It delegates functions to
+ * {@link ResourceDelegate}. It decorates the {@link ResourceDelegate} not in functionality but with @see
+ * <a href= "https://github.com/swagger-api/swagger-core/wiki/Annotations-1.5.X">Swagger
+ * Annotations</a> and <a
+ * href="https://jersey.java.net/documentation/latest/user-guide.html#jaxrs-resources">Jersey
+ * Annotations</a>
+ * 
+ * @author CWDS API Team
+ */
+@Api(value = RESOURCE_STAFF_PERSONS)
+@Path(value = RESOURCE_STAFF_PERSONS)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class StaffPersonResource {
+  private ResourceDelegate resourceDelegate;
+
+  /**
+   * Constructor
+   * 
+   * @param resourceDelegate The resourceDelegate to delegate to.
+   */
+  @Inject
+  public StaffPersonResource(@StaffPersonsServiceBackedResource ResourceDelegate resourceDelegate) {
+    this.resourceDelegate = resourceDelegate;
+  }
+
+
+  /**
+   * Finds an {@link StaffPerson} by id.
+   * 
+   * @param id the id
+   * 
+   * @return the response
+   */
+  @UnitOfWork(value = "cms")
+  @GET
+  @Path("/{id}")
+  @ApiResponses(value = {@ApiResponse(code = 401, message = "Not Authorized"),
+      @ApiResponse(code = 404, message = "Not found"),
+      @ApiResponse(code = 406, message = "Accept Header not supported")})
+  @ApiOperation(value = "Find staffperson by id", response = PostedStaffPerson.class, code = 200)
+  public Response get(@PathParam("id") @ApiParam(required = true, name = "id",
+      value = "The id of the StaffPerson to find") String id) {
+    return resourceDelegate.get(id);
+  }
+
+}
---
+++ src/test/java/gov/ca/cwds/rest/services/StaffPersonServiceTest.java
@@ -0,0 +1,112 @@
+package gov.ca.cwds.rest.services;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import gov.ca.cwds.data.cms.StaffPersonDao;
+import gov.ca.cwds.rest.api.Response;
+import gov.ca.cwds.rest.api.domain.DomainChef;
+import gov.ca.cwds.rest.api.domain.PostedStaffPerson;
+import gov.ca.cwds.rest.api.domain.StaffPerson;
+import gov.ca.cwds.rest.services.cms.StaffPersonIdRetriever;
+import io.dropwizard.jackson.Jackson;
+
+import java.math.BigDecimal;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public class StaffPersonServiceTest {
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+  private StaffPersonService staffPersonService;
+  private StaffPersonDao staffPersonDao;
+  private StaffPersonIdRetriever staffPersonIdRetriever;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @SuppressWarnings("javadoc")
+  @Before
+  public void setup() throws Exception {
+    staffPersonDao = mock(StaffPersonDao.class);
+    staffPersonIdRetriever = mock(StaffPersonIdRetriever.class);
+
+    staffPersonService = new StaffPersonService(staffPersonDao, staffPersonIdRetriever);
+
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void findThrowsAssertionError() {
+    thrown.expect(AssertionError.class);
+    try {
+      staffPersonService.find(new Long(1));
+    } catch (AssertionError e) {
+      assertEquals("Expected AssertionError", e.getMessage());
+    }
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void findReturnsCorrectStaffPersonWhenFound() throws Exception {
+    String id = "ABC";
+    StaffPerson domainStaffPerson =
+        new StaffPerson("2016-10-31", "John", "CEO", "Doe", "C", "Mr", new BigDecimal(9165551212L),
+            22, "2016-10-31", "III", true, "MIZN02k11B", "abc", "def", "99", false, "3XPCP92b24",
+            "john.doe@anyco.com");
+    PostedStaffPerson expected = new PostedStaffPerson(domainStaffPerson, "ABC");
+    gov.ca.cwds.data.persistence.cms.StaffPerson staffPerson =
+        new gov.ca.cwds.data.persistence.cms.StaffPerson(id,
+            DomainChef.uncookDateString("2016-10-31"), "John", "CEO", "Doe", "C", "Mr",
+            new BigDecimal(9165551212L), 22, DomainChef.uncookDateString("2016-10-31"), "III",
+            DomainChef.cookBoolean(true), "MIZN02k11B", "abc", "def", "99",
+            DomainChef.cookBoolean(false), "3XPCP92b24", "john.doe@anyco.com");
+
+    when(staffPersonDao.find(id)).thenReturn(staffPerson);
+
+    StaffPerson found = staffPersonService.find(id);
+
+    assertThat(found, is(expected));
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void findReturnsNullWhenNotFound() throws Exception {
+    Response found = staffPersonService.find("0XA");
+    assertThat(found, is(nullValue()));
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void deleteThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    staffPersonService.delete("ABC");
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void updateThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    staffPersonService.update("ABC", mock(StaffPerson.class));
+  }
+
+  @SuppressWarnings("javadoc")
+  @Test
+  public void createThrowsNotImplementedException() throws Exception {
+    thrown.expect(NotImplementedException.class);
+    staffPersonService.create(mock(StaffPerson.class));
+  }
+
+}
