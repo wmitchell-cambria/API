e695d04ea17a639c73502852e906b9cadfc5a357
David R. Smith <drs.code.monkey@gmail.com>
2017-07-28 10:17:00 -0700

    sprint 28

--- src/main/java/gov/ca/cwds/rest/api/domain/comparator/EntityChangedComparator.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/comparator/EntityChangedComparator.java
@@ -1,15 +1,5 @@
 package gov.ca.cwds.rest.api.domain.comparator;
 
-<<<<<<< HEAD
-import gov.ca.cwds.rest.api.domain.Participant;
-import gov.ca.cwds.rest.api.domain.cms.Client;
-=======
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
->>>>>>> Test coverage
-
-import gov.ca.cwds.rest.api.domain.DomainChef;
 import gov.ca.cwds.rest.api.domain.Participant;
 import gov.ca.cwds.rest.api.domain.cms.Client;
 
@@ -18,48 +8,17 @@ public class EntityChangedComparator {
   /**
    * Compares the last update time stamp of the participant and client. This comparator does NOT use
    * the standard interface since we are comparing to different objects, and thus is not
-   * safe(compatible) for use in equals and hash methods. Both participant and client are differnent
+   * safe(compatible) for use in equals and hash methods. Both participant and client are different
    * representations of the same object at different parts of application flow.
    *
-   * Joda time is used to handle comparison.
+   * Joda time is used to handle
    *
-<<<<<<< HEAD
    * @param participant The participant to compare last updated time with
    * @param client the Client to compare last updated time with
    * @return returns true if the date is equal to the second resolution
    */
-  public boolean compare(Participant participant, Client client){
-    return participant
-        .getLegacyDescriptor().getLastUpdated().getMillis() == client.getLastUpdatedTime().getMillis();
-  }
-=======
-   * @param participant participant to compare.
-   * @param client client to compare
-   * @return true if equivalent
-   */
   public boolean compare(Participant participant, Client client) {
-    DateTimeFormatter formatter =
-        DateTimeFormat.forPattern(deleteAfterSeconds(DomainChef.TIMESTAMP_FORMAT));;
-    String clientLastUpdated = client.getLastUpdatedTime();
-    DateTime dbDate = formatter.parseDateTime(deleteAfterSeconds(clientLastUpdated));
-
-    DateTimeFormatter formatter2 =
-        DateTimeFormat.forPattern(deleteAfterSeconds(DomainChef.TIMESTAMP_STRICT_FORMAT));
-    String participantLastUpdated = participant.getLegacyDescriptor().getLastUpdated();
-    DateTime incommingDate = formatter2.parseDateTime(deleteAfterSeconds(participantLastUpdated));
-
-    return dbDate.isEqual(incommingDate);
-
+    return participant.getLegacyDescriptor().getLastUpdated().getMillis() == client
+        .getLastUpdatedTime().getMillis();
   }
-
-  private String deleteAfterSeconds(String timestamp) {
-    int millisecondsLocation = timestamp.lastIndexOf(".");
-    return timestamp.substring(0, millisecondsLocation);
-
-  }
-
-  // private String zeroOutStrictTimestampMilliseconds(String timestamp){
-  // return timestamp;
-  // }
->>>>>>> Test coverage
 }
