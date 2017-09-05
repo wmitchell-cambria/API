dd8d2167a734d4162b9c24158ec0bb61b5b53d4b
jimlebeau <James.LeBeau@osi.ca.gov>
2017-09-05 12:30:09 -0700

    #149963092 - ** HOTLINE **: Perpetrator is not displayed on Allegation notebook on CWS/CMS

--- src/main/java/gov/ca/cwds/rest/services/LegacyDefaultValues.java
+++ src/main/java/gov/ca/cwds/rest/services/LegacyDefaultValues.java
@@ -7,7 +7,6 @@ package gov.ca.cwds.rest.services;
 public class LegacyDefaultValues {
   public static final short DEFAULT_CODE = 0;
   public static final short DEFAULT_APPROVAL_STATUS_CODE = 118;
-  public static final String DEFAULT_NON_PROTECTING_PARENT_CODE = "U";
   public static final short DEFAULT_ADDRESS_TYPE = 32; // residence
   public static final String DEFAULT_COUNTY_SPECIFIC_CODE = "99";
   public static final String DEFAULT_STAFF_PERSON_ID = "0X5";
