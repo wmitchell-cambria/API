431ed42a5c568289c9679278f41a4b5dee79ad38
Mariam Ghori <mariam.ghori@osi.ca.gov>
2017-09-14 14:28:46 -0700

    #150897565 INVESTIGATION: /investigations/:referral_id/screening** API [GET] stubs

--- src/main/java/gov/ca/cwds/rest/services/investigation/ScreeningSummaryService.java
+++ src/main/java/gov/ca/cwds/rest/services/investigation/ScreeningSummaryService.java
@@ -1,15 +1,17 @@
 package gov.ca.cwds.rest.services.investigation;
 
-import static io.dropwizard.testing.FixtureHelpers.fixture;
 import gov.ca.cwds.data.Dao;
 import gov.ca.cwds.data.dao.contact.DeliveredServiceDao;
 import gov.ca.cwds.rest.api.Response;
 import gov.ca.cwds.rest.api.domain.investigation.ScreeningSummary;
+import gov.ca.cwds.rest.api.domain.investigation.SimpleAllegation;
 import gov.ca.cwds.rest.filters.RequestExecutionContext;
 import gov.ca.cwds.rest.services.TypedCrudsService;
 import io.dropwizard.jackson.Jackson;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
@@ -34,6 +36,43 @@ public class ScreeningSummaryService implements
   private Date lastUpdatedTime = RequestExecutionContext.instance().getRequestStartTime();
   private DeliveredServiceDao deliveredServiceDao;
 
+  private String perpetratorId = "11";
+  private Set<String> allegationTypes = validAllegationTypes();
+  private String victimId = "22";
+
+  private String additionalInformation = "There was excessive evidence of abuse";
+  private String decision = "promote_to_referral";
+  private String decisionDetail = "immediate";
+  private String name = "Henderson Screening";
+  private Set<String> safetyAlerts = validSafetyAletrs();
+  private String startedAt = "2017-09-01T16:48:05.457Z";
+  private String safetyInformation = "The animal at residence is a lion";
+  private String id = "1";
+  private Set<SimpleAllegation> allegations = validAllegationsSet();
+
+  private Set<String> validSafetyAletrs() {
+    Set<String> safetyAlerts = new HashSet<String>();
+    safetyAlerts.add("Dangerous Animal on Premises");
+    safetyAlerts.add("Firearms in Home");
+    safetyAlerts.add("Severe Mental Health Status");
+    return safetyAlerts;
+  }
+
+  private Set<SimpleAllegation> validAllegationsSet() {
+    Set<SimpleAllegation> validAllegations = new HashSet<SimpleAllegation>();
+    SimpleAllegation allegation = new SimpleAllegation(victimId, perpetratorId, allegationTypes);
+    validAllegations.add(allegation);
+    return validAllegations;
+  }
+
+  private Set<String> validAllegationTypes() {
+    Set<String> allegationTypes = new HashSet<String>();
+    allegationTypes.add("General neglect");
+    allegationTypes.add("Physical abuse");
+    return allegationTypes;
+  }
+
+
   /**
    * @param deliveredServiceDao {@link Dao} handling
    *        {@link gov.ca.cwds.data.persistence.contact.DeliveredServiceEntity} objects
@@ -53,14 +92,10 @@ public class ScreeningSummaryService implements
 
   @Override
   public Response find(String primaryKey) {
-    ScreeningSummary serialized = new ScreeningSummary();
-    try {
-      serialized =
-          MAPPER.readValue(fixture("fixtures/domain/investigation/screening/valid/valid.json"),
-              ScreeningSummary.class);
-    } catch (Exception e) {
-      LOGGER.error("Exception In HistoryOfInvolvement " + e.getMessage());
-    }
+
+    ScreeningSummary serialized =
+        new ScreeningSummary(id, name, decision, decisionDetail, safetyAlerts, safetyInformation,
+            additionalInformation, startedAt, allegations);
     return serialized;
   }
 
