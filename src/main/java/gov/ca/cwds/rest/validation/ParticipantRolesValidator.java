7332b31a2a5744f2d0b23df8c28ef0b13a1e3825
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2017-09-17 23:30:42 -0700

    Added participant roles validator

---
+++ src/main/java/gov/ca/cwds/rest/validation/ParticipantRolesValidator.java
@@ -0,0 +1,81 @@
+package gov.ca.cwds.rest.validation;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import gov.ca.cwds.rest.api.domain.Participant;
+
+/**
+ * Validates that the {@code SystemCodeId.property} of a given bean must be a valid CMS system code
+ * id for its system code category, {@code SystemCodeId.category}.
+ * 
+ * @author CWDS API Team
+ */
+public class ParticipantRolesValidator
+    implements ConstraintValidator<ValidParticipantRoles, Collection<Participant>> {
+
+  private static final String MANDATED_REPORTER_ROLE = "Mandated Reporter";
+  private static final String NON_MANDATED_REPORTER_ROLE = "Non-mandated Reporter";
+  private static final String ANONYMOUS_REPORTER_ROLE = "Anonymous Reporter";
+  private static final String VICTIM_ROLE = "Victim";
+  private static final String SELF_REPORTED_ROLE = "Self Reported";
+
+  @Override
+  public void initialize(ValidParticipantRoles anno) {}
+
+  @Override
+  public boolean isValid(final Collection<Participant> participants,
+      ConstraintValidatorContext context) {
+    boolean valid = true;
+
+    int reporterCount = 0;
+    int victimCount = 0;
+
+    if (participants != null) {
+      for (Participant participant : participants) {
+        if (hasReporterRole(participant)) {
+          reporterCount++;
+        }
+        if (hasVictimRole(participant)) {
+          victimCount++;
+        }
+      }
+    }
+
+    // R - 00851 Reporter Creation
+    // R - 00836 Self Rep Ind Limit
+    // only one reporter is allowed on a referral
+    if (reporterCount != 1) {
+      valid = false;
+    }
+
+    // R - 00851 Reporter Creation
+    // at least one victim is required for a referral
+    if (victimCount < 1) {
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  private static Boolean hasReporterRole(Participant participant) {
+    Set<String> roles = participant.getRoles();
+    Boolean hasReporterRole = false;
+
+    if (roles != null) {
+      hasReporterRole =
+          roles.contains(ANONYMOUS_REPORTER_ROLE) || roles.contains(MANDATED_REPORTER_ROLE)
+              || roles.contains(NON_MANDATED_REPORTER_ROLE) || roles.contains(SELF_REPORTED_ROLE);
+    }
+    return hasReporterRole;
+  }
+
+  private static Boolean hasVictimRole(Participant participant) {
+    Set<String> roles = participant.getRoles();
+    return roles != null && roles.contains(VICTIM_ROLE);
+  }
+
+}
---
+++ src/main/java/gov/ca/cwds/rest/validation/ValidParticipantRoles.java
@@ -0,0 +1,32 @@
+package gov.ca.cwds.rest.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * Annotation indicating that a Set of participants must contains valid roles.
+ * 
+ * @author CWDS API Team
+ */
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = ParticipantRolesValidator.class)
+public @interface ValidParticipantRoles {
+
+  String message() default "must contain at leaset one victim and only one reporter";
+
+  Class<?>[]groups() default {};
+
+  Class<? extends Payload>[]payload() default {};
+}
