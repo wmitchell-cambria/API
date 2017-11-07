f28d1ec962c369e4cd63c77094be71d6163dd070
Tom Parker <tom.parker@tobordasolutions.com>
2017-11-06 17:19:18 -0800

    Add Interfaces for Business Rules

---
+++ src/main/java/gov/ca/cwds/rest/business/RuleAction.java
@@ -0,0 +1,10 @@
+package gov.ca.cwds.rest.business;
+
+/**
+ * A common interface for executing legacy business rules
+ *
+ * Validators perform some sort of Business Rule action and does not return a value.
+ */
+public interface RuleAction {
+  public void execute();
+}
