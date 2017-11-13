33d4587a5e7f1e2eababa2f363d2241a22061e94
Mariam Ghori <mariam.ghori@osi.ca.gov>
2017-11-13 11:48:20 -0800

    #151667712 ContactService, extract out Doc Tool Rule R05904

---
+++ src/test/java/gov/ca/cwds/rest/business/rules/R05904ContactStartedAtTest.java
@@ -0,0 +1,49 @@
+package gov.ca.cwds.rest.business.rules;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import gov.ca.cwds.rest.api.domain.DomainChef;
+
+public class R05904ContactStartedAtTest {
+
+  @Test
+  public void isValidWhenContactStartedAtBeforeReferralReceivedDateTimeReturnsFalse()
+      throws Exception {
+    Date contactStartedAtDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:29:14.000-0000");
+    Date referralReceivedDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:30:14.000-0000");
+    Boolean actual =
+        new R05904ContactStartedAt(contactStartedAtDateTime, referralReceivedDateTime).isValid();
+    assertEquals(actual, false);
+  }
+
+  @Test
+  public void isValidWhenContactStartedAtEqualsReferralReceivedDateTimeReturnsFalse()
+      throws Exception {
+    Date contactStartedAtDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:30:14.000-0000");
+    Date referralReceivedDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:30:14.000-0000");
+    Boolean actual =
+        new R05904ContactStartedAt(contactStartedAtDateTime, referralReceivedDateTime).isValid();
+    assertEquals(actual, false);
+  }
+
+  @Test
+  public void isValidWhenContactStartedAtAfterReferralReceivedDateTimeReturnsTrue()
+      throws Exception {
+    Date contactStartedAtDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:31:14.000-0000");
+    Date referralReceivedDateTime =
+        DomainChef.uncookStrictTimestampString("2010-04-27T23:30:14.000-0000");
+    Boolean actual =
+        new R05904ContactStartedAt(contactStartedAtDateTime, referralReceivedDateTime).isValid();
+    assertEquals(actual, true);
+  }
+
+}
