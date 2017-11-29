c4877521ff1734529a5216aab5d1d93e21377c44
Jeyaraman <jeyaraman.govindaraj@osi.ca.gov>
2017-11-28 16:15:51 -0800

    Rule - 04530 - Assignment End Date validation.

--- src/main/java/gov/ca/cwds/rest/business/rules/R04530AssignmentEndDateValidator.java
+++ src/main/java/gov/ca/cwds/rest/business/rules/R04530AssignmentEndDateValidator.java
@@ -11,32 +11,69 @@ public class R04530AssignmentEndDateValidator implements RuleValidatator {
   Assignment assignment = null;
 
   /**
-   * validating assignment end date
+   * Rule - R04530 - validating assignment end date
    */
   @Override
   public boolean isValid() {
 
     boolean validEndDate = false;
     // end date is optional value.. it can be null.
-    if (StringUtils.isBlank(assignment.getEndDate())
-        && StringUtils.isBlank(assignment.getEndTime())) {
+    if (isNullEndDate()) {
       return true;
-    } else {
-      Date assignemntEndDate = DomainChef.concatenateDateAndTime(
-          unCookDateString(assignment.getEndDate()), unCookTimeString(assignment.getEndTime()));
-
-      Date assignemntStartDate = DomainChef.concatenateDateAndTime(
-          unCookDateString(assignment.getStartDate()), unCookTimeString(assignment.getStartTime()));
-      Date currentDate = new Date();
-
-      if ((assignemntStartDate.before(assignemntEndDate)
-          || (assignemntStartDate.equals(assignemntEndDate)))
-          && (currentDate.before(assignemntEndDate) || currentDate.equals(assignemntEndDate))) {
-        validEndDate = true;
-      }
     }
 
+    Date assignemntStartDate =
+        this.concatenateDateAndTime(assignment.getStartDate(), assignment.getStartTime());
+    Date assignemntEndDate =
+        this.concatenateDateAndTime(assignment.getEndDate(), assignment.getEndTime());
+
+
+    if (this.isAssignmentStartDateBeforeOrEqualsEndDate(assignemntStartDate, assignemntEndDate)
+        && (this.isCurrentDateBeforeOrEqualAssignmentEndDate(assignemntEndDate))) {
+      validEndDate = true;
+    }
     return validEndDate;
+
+  }
+
+  private boolean isCurrentDateBeforeOrEqualAssignmentEndDate(Date assignemntEndDate) {
+    Date currentDate = new Date();
+    return (currentDate.before(assignemntEndDate) || currentDate.equals(assignemntEndDate));
+
+  }
+
+  /**
+   * validating start date should be before or equals start date
+   * 
+   * @param startDate - assignment start date
+   * @param endDate - assignment end date
+   * @return
+   */
+  private boolean isAssignmentStartDateBeforeOrEqualsEndDate(Date startDate, Date endDate) {
+    return (startDate.before(endDate) || (startDate.equals(endDate)));
+
+  }
+
+  /**
+   * method to combine date and time
+   * 
+   * @param date - date object
+   * @param time - time object
+   * @return - combines date and time
+   */
+  private Date concatenateDateAndTime(String date, String time) {
+    return DomainChef.concatenateDateAndTime(unCookDateString(date), unCookTimeString(time));
+
+  }
+
+  /**
+   * checking end date is null or not;
+   * 
+   * @return true if endDate is null
+   */
+  private boolean isNullEndDate() {
+    return (StringUtils.isBlank(assignment.getEndDate())
+        && StringUtils.isBlank(assignment.getEndTime()));
 
   }
 
