ec0eabb717e4f2b0f1cde49115b4dc6739d23023
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2017-11-30 18:21:23 -0800

    R - 00832

---
+++ src/test/java/gov/ca/cwds/rest/business/rules/doctool/R00832SetStaffPersonAddedIndTest.java
@@ -0,0 +1,33 @@
+package gov.ca.cwds.rest.business.rules.doctool;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import gov.ca.cwds.fixture.ScreeningToReferralResourceBuilder;
+import gov.ca.cwds.rest.api.domain.ScreeningToReferral;
+import gov.ca.cwds.rest.business.rules.R00832SetStaffPersonAddedInd;
+
+/**
+ * @author CWDS API Team
+ * 
+ * @see R00832SetStaffPersonAddedInd
+ *
+ */
+public class R00832SetStaffPersonAddedIndTest {
+
+  /**
+   * Test when referral response time is set then the staffPersonAddedInd to true
+   * 
+   * @throws Exception - Exception
+   */
+  @Test
+  public void testPassIsValidReturnTrue() throws Exception {
+    ScreeningToReferral validScreeningToreferral = new ScreeningToReferralResourceBuilder()
+        .setResponseTime((short) 1516).createScreeningToReferral();
+    R00832SetStaffPersonAddedInd r00832SetStaffPersonAddedInd =
+        new R00832SetStaffPersonAddedInd(validScreeningToreferral);
+    assertEquals(true, r00832SetStaffPersonAddedInd.isValid());
+  }
+
+}
