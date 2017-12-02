ce16b0720ea2b0a331efb608366f540bded89230
David R. Smith <drs.code.monkey@gmail.com>
2017-12-02 14:22:03 -0800

    SonarQube

--- src/main/java/gov/ca/cwds/rest/api/domain/AccessLimitation.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/AccessLimitation.java
@@ -1,8 +1,12 @@
 package gov.ca.cwds.rest.api.domain;
 
+import java.util.Date;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import gov.ca.cwds.data.std.ApiObjectIdentity;
+import gov.ca.cwds.rest.api.domain.cms.SystemCodeDescriptor;
+import gov.ca.cwds.rest.util.FerbDateUtils;
 
 /**
  * Access Limitation (sealed, sensitive, restricted).
@@ -19,16 +23,13 @@ public class AccessLimitation extends ApiObjectIdentity {
   private LimitedAccessType limitedAccessCode = DEFAULT_LIMITED_ACCESS_CODE;
 
   @JsonProperty("limited_access_date")
-  private String limitedAccessDate;
+  private Date limitedAccessDate;
 
   @JsonProperty("limited_access_description")
   private String limitedAccessDescription;
 
-  @JsonProperty("limited_access_government_entity_id")
-  private String limitedAccessGovernmentEntityId;
-
-  @JsonProperty("limited_access_government_entity_name")
-  private String limitedAccessGovernmentEntityName;
+  @JsonProperty("limited_access_government_entity")
+  private SystemCodeDescriptor limitedAccessGovernmentEntity;
 
   /**
    * No-argument constructor
@@ -45,12 +46,12 @@ public class AccessLimitation extends ApiObjectIdentity {
     this.limitedAccessCode = limitedAccessCode;
   }
 
-  public String getLimitedAccessDate() {
-    return limitedAccessDate;
+  public Date getLimitedAccessDate() {
+    return FerbDateUtils.freshDate(limitedAccessDate);
   }
 
-  public void setLimitedAccessDate(String limitedAccessDate) {
-    this.limitedAccessDate = limitedAccessDate;
+  public void setLimitedAccessDate(Date limitedAccessDate) {
+    this.limitedAccessDate = FerbDateUtils.freshDate(limitedAccessDate);
   }
 
   public String getLimitedAccessDescription() {
@@ -61,21 +62,12 @@ public class AccessLimitation extends ApiObjectIdentity {
     this.limitedAccessDescription = limitedAccessDescription;
   }
 
-  public String getLimitedAccessGovernmentEntityId() {
-    return limitedAccessGovernmentEntityId;
+  public SystemCodeDescriptor getLimitedAccessGovernmentEntity() {
+    return limitedAccessGovernmentEntity;
   }
 
-  public void setLimitedAccessGovernmentEntityId(String limitedAccessGovernmentEntityId) {
-    this.limitedAccessGovernmentEntityId = limitedAccessGovernmentEntityId;
+  public void setLimitedAccessGovernmentEntity(SystemCodeDescriptor limitedAccessGovernmentEntity) {
+    this.limitedAccessGovernmentEntity = limitedAccessGovernmentEntity;
   }
-
-  public String getLimitedAccessGovernmentEntityName() {
-    return limitedAccessGovernmentEntityName;
-  }
-
-  public void setLimitedAccessGovernmentEntityName(String limitedAccessGovernmentEntityName) {
-    this.limitedAccessGovernmentEntityName = limitedAccessGovernmentEntityName;
-  }
-
 }
 
