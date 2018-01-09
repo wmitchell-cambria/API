752e5c26e64b7f43bd9e5120e48d20a8117cd2fa
tom parker <tom.parker@osi.ca.gov>
2018-01-09 11:52:59 -0800

    SonarQube Refactor. Remove unused imports

--- src/main/java/gov/ca/cwds/rest/services/StaffPersonService.java
+++ src/main/java/gov/ca/cwds/rest/services/StaffPersonService.java
@@ -1,8 +1,6 @@
 package gov.ca.cwds.rest.services;
 
 import org.apache.commons.lang3.NotImplementedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
--- src/main/java/gov/ca/cwds/rest/services/cms/ClientAddressService.java
+++ src/main/java/gov/ca/cwds/rest/services/cms/ClientAddressService.java
@@ -2,7 +2,6 @@ package gov.ca.cwds.rest.services.cms;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
