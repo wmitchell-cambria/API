a46ed9591d7cc07ccde8afb0da88d37b6d0ced5e
OlegKorniichuk <oleg.korniichuk@osi.ca.gov>
2018-02-09 16:28:12 -0800

    Fix tests on jenkins (next try)

--- src/test/java/gov/ca/cwds/rest/services/hoi/HOICaseServiceTest.java
+++ src/test/java/gov/ca/cwds/rest/services/hoi/HOICaseServiceTest.java
@@ -1,57 +1,73 @@
 package gov.ca.cwds.rest.services.hoi;
 
-import static gov.ca.cwds.rest.core.Api.RESOURCE_CASE_HISTORY_OF_INVOLVEMENT;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
-
-import gov.ca.cwds.IntakeBaseTest;
-import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.shiro.authz.AuthorizationException;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import gov.ca.cwds.data.cms.CaseDao;
+import gov.ca.cwds.data.cms.ClientDao;
+import gov.ca.cwds.data.cms.ClientRelationshipDao;
+import gov.ca.cwds.data.cms.TestSystemCodeCache;
+import gov.ca.cwds.data.persistence.cms.Client;
+import gov.ca.cwds.data.persistence.cms.ClientRelationship;
+import gov.ca.cwds.data.persistence.cms.CmsCase;
+import gov.ca.cwds.fixture.ClientEntityBuilder;
+import gov.ca.cwds.fixture.ClientRelationshipEntityBuilder;
+import gov.ca.cwds.fixture.CmsCaseEntityBuilder;
+import gov.ca.cwds.fixture.StaffPersonEntityBuilder;
+import gov.ca.cwds.rest.api.domain.cms.SystemCodeCache;
+import gov.ca.cwds.rest.api.domain.hoi.HOICase;
+import gov.ca.cwds.rest.api.domain.hoi.HOICaseResponse;
 import gov.ca.cwds.rest.api.domain.hoi.HOIRequest;
+import gov.ca.cwds.rest.filters.TestingRequestExecutionContext;
 
 /**
  * @author CWDS API Team
  *
  */
 @SuppressWarnings("javadoc")
-@Ignore
-public class HOICaseServiceTest extends IntakeBaseTest {
+public class HOICaseServiceTest {
 
-  @Test
-  public void testHandleFindNonExistingClientId() throws Exception {
-    WebTarget target = clientTestRule.withSecurityToken("security/social-worker-only-principal.json")
-        .target(RESOURCE_CASE_HISTORY_OF_INVOLVEMENT);
+  private CaseDao caseDao;
+  private ClientDao clientDao;
+  private ClientRelationshipDao clientRelationshipDao;
+  private HOICaseService target;
+  private HOIRequest request;
 
-    HOIRequest request = new HOIRequest();
-    request.setClientIds(Stream.of("-1").collect(Collectors.toSet()));
+  /**
+   * Initialize system code cache
+   */
+  private TestSystemCodeCache testSystemCodeCache = new TestSystemCodeCache();
 
-    Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
-    Response response = invocation.post(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE));
-
-    String value = IOUtils.toString((InputStream) response.getEntity(), "UTF-8");
-
-    assertEqualJsonArrays("[ ]", value);
-  }
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   /**
    * Test setup
-   * 
+   *
    * @throws Exception - Exception
    */
   @Before
   public void setup() throws Exception {
-/*
     new TestingRequestExecutionContext("02f");
     SystemCodeCache.global().getAllSystemCodes();
     caseDao = mock(CaseDao.class);
@@ -60,10 +76,8 @@ public class HOICaseServiceTest extends IntakeBaseTest {
     target = new HOICaseService(caseDao, clientDao, clientRelationshipDao);
     request = new HOIRequest();
     request.setClientIds(Stream.of("123").collect(Collectors.toSet()));
-*/
   }
 
-/*
   @Test
   public void testHandleFind() throws Exception {
     CmsCase cmscase1 = new CmsCaseEntityBuilder().setId("TAZGOO205C").setStartDate(new Date())
@@ -82,9 +96,7 @@ public class HOICaseServiceTest extends IntakeBaseTest {
     HOICaseResponse response = target.handleFind(request);
     assertThat(response, notNullValue());
   }
-*/
 
-/*
   @Test
   public void testNotPermittedHandleFind() throws Exception {
     HOICaseService spy = spy(target);
@@ -94,9 +106,7 @@ public class HOICaseServiceTest extends IntakeBaseTest {
     HOICaseResponse response = spy.handleFind(request);
     assertEquals(response.getHoiCases().size(), 0);
   }
-*/
 
-/*
   @Test
   public void testHandleFindWhenNoClientIdsProvided() throws Exception {
     HOIRequest emptyRequest = new HOIRequest();
@@ -104,156 +114,27 @@ public class HOICaseServiceTest extends IntakeBaseTest {
     HOICaseResponse response = target.handleFind(emptyRequest);
     assertThat(response, notNullValue());
   }
-*/
-
-/*
-  @Test
-  public void testFindParentsBySecondaryRelationship254MotherSonStepSuccess() throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship((short) 254);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
 
   @Test
-  public void testFindParentsBySecondaryRelationshipType214FatherSonStepSuccess() throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship((short) 214);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsByPrimaryRelationshipTypeSonDeFactoParent282Success()
-      throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsPrimaryClientInTheRelationship((short) 282);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsBySecondaryRelationshipType6361MotherSonPresumedSuccess()
-      throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship((short) 6361);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsByPrimaryRelationshipType294SonNonCustodialParentSuccess()
-      throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsPrimaryClientInTheRelationship((short) 294);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsBySecondaryRelationshipType273NonCustodialParentSonSuccess()
-      throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship((short) 273);
-    assertEquals(1, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsByPrimaryRelationshipType254MotherSonStepFails() throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsPrimaryClientInTheRelationship((short) 254);
-    assertEquals(0, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsByPrimaryRelationshipType296UncleNephewMaternalFails()
-      throws Exception {
-    HOICaseResponse response =
-        getCaseResponseWhereChildIsPrimaryClientInTheRelationship((short) 296);
-    assertEquals(0, response.getHoiCases().get(0).getParents().size());
-  }
-
-  @Test
-  public void testFindParentsWhereChildIsSecondaryClientInTheRelationshipForAllValidTypesSuccess()
-      throws Exception {
-    List<Integer> parentToChildTypes = new ArrayList<Integer>();
-    parentToChildTypes.addAll(Arrays.asList(272, 273, 5620, 6361));
-    for (Integer i = 201; i <= 214; i++) {
-      parentToChildTypes.add(i);
-    }
-    for (Integer i = 245; i <= 254; i++) {
-      parentToChildTypes.add(i);
-    }
-    for (Integer parentToChildType : parentToChildTypes) {
-      HOICaseResponse response = getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship(
-          parentToChildType.shortValue());
-      assertEquals(1, response.getHoiCases().get(0).getParents().size());
-    }
-  }
-
-  @Test
-  public void testFindParentsWhereChildIsPrimaryClientInTheRelationshipForAllValidTypesSuccess()
-      throws Exception {
-    List<Integer> childToParentTypes = new ArrayList<Integer>();
-    childToParentTypes.addAll(Arrays.asList(6360));
-    for (Integer i = 187; i <= 200; i++) {
-      childToParentTypes.add(i);
-    }
-    for (Integer i = 282; i <= 294; i++) {
-      childToParentTypes.add(i);
-    }
-    for (Integer childToParentType : childToParentTypes) {
-      HOICaseResponse response =
-          getCaseResponseWhereChildIsPrimaryClientInTheRelationship(childToParentType.shortValue());
-      assertEquals(1, response.getHoiCases().get(0).getParents().size());
-    }
-  }
-
-
-  @Test(expected = NotImplementedException.class)
-  public void testHandleRequest() throws Exception {
-    target.handleRequest(new HOICase());
-  }
-
-  public HOICaseResponse getCaseResponseWhereChildIsPrimaryClientInTheRelationship(short type)
-      throws Exception {
+  public void testFindParentsByPrimaryRelationship() throws Exception {
     CmsCase cmscase = new CmsCaseEntityBuilder().setId("TAZGOO205C")
         .setStaffPerson(new StaffPersonEntityBuilder().build()).build();
     CmsCase[] cases = {cmscase};
     ClientRelationship relationship =
-        new ClientRelationshipEntityBuilder().setClientRelationshipType(type).build();
+        new ClientRelationshipEntityBuilder().setClientRelationshipType((short) 245).build();
     ClientRelationship[] relationships = {relationship};
-    ClientRelationship[] emptyRelationships = {};
     when(clientRelationshipDao.findByPrimaryClientId(any(String.class))).thenReturn(relationships);
-    when(clientRelationshipDao.findBySecondaryClientId(any(String.class)))
-        .thenReturn(emptyRelationships);
-    Client client = new ClientEntityBuilder().build();
-    when(caseDao.findByVictimClientIds(any(Collection.class))).thenReturn(cases);
-    when(clientDao.find(any(String.class))).thenReturn(client);
-    HOICaseResponse response = target.handleFind(request);
-    return response;
-  }
-
-  public HOICaseResponse getCaseResponseWhereChildIsSecondaryClientIdInTheRelationship(short type)
-      throws Exception {
-    CmsCase cmscase = new CmsCaseEntityBuilder().setId("TAZGOO205C")
-        .setStaffPerson(new StaffPersonEntityBuilder().build()).build();
-    CmsCase[] cases = {cmscase};
-    ClientRelationship relationship =
-        new ClientRelationshipEntityBuilder().setClientRelationshipType(type).build();
-    ClientRelationship[] relationships = {relationship};
-    ClientRelationship[] emptyRelationships = {};
-    when(clientRelationshipDao.findByPrimaryClientId(any(String.class)))
-        .thenReturn(emptyRelationships);
     when(clientRelationshipDao.findBySecondaryClientId(any(String.class)))
         .thenReturn(relationships);
     Client client = new ClientEntityBuilder().build();
     when(caseDao.findByVictimClientIds(any(Collection.class))).thenReturn(cases);
     when(clientDao.find(any(String.class))).thenReturn(client);
     HOICaseResponse response = target.handleFind(request);
-    return response;
+    assertThat(response, notNullValue());
   }
-*/
 
-/*
   @Test(expected = NotImplementedException.class)
   public void testHandleRequest() throws Exception {
     target.handleRequest(new HOICase());
   }
-*/
 }
