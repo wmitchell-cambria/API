4532ae460b2f0287b6c21dcd6610a582be6d399d
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2018-02-20 21:07:08 -0800

    added resource test (tbc)

---
+++ src/test/java/gov/ca/cwds/rest/resources/hoi/HOIScreeningResourceApplicationTest.java
@@ -0,0 +1,40 @@
+package gov.ca.cwds.rest.resources.hoi;
+
+import static gov.ca.cwds.rest.core.Api.RESOURCE_HOI_SCREENINGS;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import gov.ca.cwds.IntakeBaseTest;
+import gov.ca.cwds.rest.api.domain.hoi.HOIRequest;
+import gov.ca.cwds.rest.api.domain.hoi.HOIScreening;
+
+@Ignore
+public class HOIScreeningResourceApplicationTest extends IntakeBaseTest {
+
+  @Test
+  public void testHandleFindNonExistingClientId() throws Exception {
+    WebTarget target =
+        clientTestRule.withSecurityToken("security/social-worker-only-principal.json")
+            .target(RESOURCE_HOI_SCREENINGS);
+
+    HOIRequest request = new HOIRequest();
+    request.setClientIds(Stream.of("-1").collect(Collectors.toSet()));
+
+    Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON_TYPE);
+    HOIScreening[] response = invocation
+        .post(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE), HOIScreening[].class);
+    String actualJson = clientTestRule.getMapper().writeValueAsString(response);
+
+    JSONAssert.assertEquals("[]", actualJson, true);
+  }
+}
