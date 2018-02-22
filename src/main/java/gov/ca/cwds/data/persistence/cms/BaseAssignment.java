087324fd7335f7eaf82b4cdd9086d8d34d7abe85
jim lebeau <James.LeBeau@osi.ca.gov>
2018-02-22 08:45:15 -0800

    Sonarqube code smells

--- src/main/java/gov/ca/cwds/data/persistence/cms/BaseAssignment.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/BaseAssignment.java
@@ -13,8 +13,6 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.DiscriminatorFormula;
 import org.hibernate.annotations.DiscriminatorOptions;
 import org.hibernate.annotations.Type;
@@ -316,13 +314,4 @@ public abstract class BaseAssignment extends CmsPersistentObject {
     this.establishedForCode = establishedForCode;
   }
   
-  @Override
-  public int hashCode() {
-    return HashCodeBuilder.reflectionHashCode(this, false);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    return EqualsBuilder.reflectionEquals(this, obj, false);
-  }
 }
