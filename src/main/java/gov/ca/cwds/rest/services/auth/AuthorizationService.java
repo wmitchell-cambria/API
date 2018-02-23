3a71e3ece9e0307f1f8fa1b4eee8b404007c8ef8
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2018-02-23 10:13:41 -0800

    Fixed javadoc error

--- src/main/java/gov/ca/cwds/rest/services/auth/AuthorizationService.java
+++ src/main/java/gov/ca/cwds/rest/services/auth/AuthorizationService.java
@@ -44,7 +44,8 @@ public class AuthorizationService
    * AuthorizationException if it is not.
    * 
    * @param clientId The client id
-   * @throws AuthorizationException
+   * @throws AuthorizationException If client identified by given clientId is not accessible to
+   *         logged in user.
    */
   public void ensureClientAccessAuthorized(@Authorize("client:read:clientId") String clientId) {
     // Authorizer annotation does the work
@@ -54,8 +55,9 @@ public class AuthorizationService
    * Check if logged in user is authorized to access clients identified by given clientIds, throws
    * AuthorizationException if any client is not authorized for staff person.
    * 
-   * @param clientId The client id
-   * @throws AuthorizationException
+   * @param clientIds The client ids
+   * @throws AuthorizationException If any client identified by given clientIds is not accessible to
+   *         logged in user.
    */
   public void ensureClientAccessAuthorized(Collection<String> clientIds) {
     for (String clientId : clientIds) {
@@ -67,8 +69,8 @@ public class AuthorizationService
    * Check if logged in user is authorized to access given screening and all participants, throws
    * AuthorizationException if not authorized.
    * 
-   * @param screeningEntity
-   * @throws AuthorizationException
+   * @param screeningEntity The screening
+   * @throws AuthorizationException If given screening not accessible to logged in user.
    */
   public void ensureScreeningAccessAuthorized(
       @Authorize("screening:read:screeningEntity") ScreeningEntity screeningEntity) {
