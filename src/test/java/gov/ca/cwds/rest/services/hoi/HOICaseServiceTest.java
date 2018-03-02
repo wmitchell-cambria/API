f25c3978899a6bc2a830dc8de3a8edc991354db1
OlegKorniichuk <oleg.korniichuk@osi.ca.gov>
2018-03-02 10:23:05 -0800

    INT-1225 Add Functional tests . Integrate with new care-api and data-model versions

--- src/test/java/gov/ca/cwds/rest/services/hoi/HOICaseServiceTest.java
+++ src/test/java/gov/ca/cwds/rest/services/hoi/HOICaseServiceTest.java
@@ -15,7 +15,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
