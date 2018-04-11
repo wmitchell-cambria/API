2918b63cf7e73adaed981e1c7e829040255e2515
David R. Smith <David.Smith@osi.ca.gov>
2018-04-11 14:03:27 -0700

    INT-1592: XA transactions

--- src/main/java/gov/ca/cwds/data/cms/ClientRelationshipDao.java
+++ src/main/java/gov/ca/cwds/data/cms/ClientRelationshipDao.java
@@ -1,7 +1,9 @@
 package gov.ca.cwds.data.cms;
 
-import gov.ca.cwds.data.persistence.cms.RelationshipWrapper;
+import java.util.List;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.hibernate.type.StringType;
 
@@ -10,9 +12,8 @@ import com.google.inject.Inject;
 import gov.ca.cwds.data.BaseDaoImpl;
 import gov.ca.cwds.data.persistence.cms.ClientRelationship;
 import gov.ca.cwds.data.persistence.cms.CmsCase;
+import gov.ca.cwds.data.persistence.cms.RelationshipWrapper;
 import gov.ca.cwds.inject.CmsSessionFactory;
-
-import java.util.List;
 
 /**
  * DAO for {@link ClientRelationship}.
@@ -21,53 +22,51 @@ import java.util.List;
  */
 public class ClientRelationshipDao extends BaseDaoImpl<ClientRelationship> {
 
-	/**
-	 * Constructor
-	 * 
-	 * @param sessionFactory
-	 *            The sessionFactory
-	 */
-	@Inject
-	public ClientRelationshipDao(@CmsSessionFactory SessionFactory sessionFactory) {
-		super(sessionFactory);
-	}
+  /**
+   * Constructor
+   * 
+   * @param sessionFactory The sessionFactory
+   */
+  @Inject
+  public ClientRelationshipDao(@CmsSessionFactory SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
 
-	/**
-	 * Find by primary client id
-	 * 
-	 * @param primaryClientId
-	 *            - clientId
-	 * @return the client relationship
-	 */
-	@SuppressWarnings("unchecked")
-	public ClientRelationship[] findByPrimaryClientId(String primaryClientId) {
-		final Query<CmsCase> query = this.getSessionFactory().getCurrentSession().getNamedQuery(
-				"gov.ca.cwds.data.persistence.cms.ClientRelationship.findClientRelationshipByPrimaryClientId");
-		query.setParameter("primaryClientId", primaryClientId, StringType.INSTANCE);
-		return query.list().toArray(new ClientRelationship[0]);
-	}
+  /**
+   * Find by primary client id
+   * 
+   * @param primaryClientId - clientId
+   * @return the client relationship
+   */
+  @SuppressWarnings("unchecked")
+  public ClientRelationship[] findByPrimaryClientId(String primaryClientId) {
+    final Query<CmsCase> query = this.getSessionFactory().getCurrentSession().getNamedQuery(
+        "gov.ca.cwds.data.persistence.cms.ClientRelationship.findClientRelationshipByPrimaryClientId");
+    query.setParameter("primaryClientId", primaryClientId, StringType.INSTANCE);
+    return query.list().toArray(new ClientRelationship[0]);
+  }
 
-	/**
-	 * Find by secondary client id
-	 * 
-	 * @param secondaryClientId
-	 *            - clientId
-	 * @return the client relationship
-	 */
-	@SuppressWarnings("unchecked")
-	public ClientRelationship[] findBySecondaryClientId(String secondaryClientId) {
-		final Query<CmsCase> query = this.getSessionFactory().getCurrentSession().getNamedQuery(
-				"gov.ca.cwds.data.persistence.cms.ClientRelationship.findClientRelationshipBySecondaryClientId");
-		query.setParameter("secondaryClientId", secondaryClientId, StringType.INSTANCE);
-		return query.list().toArray(new ClientRelationship[0]);
-	}
+  /**
+   * Find by secondary client id
+   * 
+   * @param secondaryClientId - clientId
+   * @return the client relationship
+   */
+  @SuppressWarnings("unchecked")
+  public ClientRelationship[] findBySecondaryClientId(String secondaryClientId) {
+    final Query<CmsCase> query = this.getSessionFactory().getCurrentSession().getNamedQuery(
+        "gov.ca.cwds.data.persistence.cms.ClientRelationship.findClientRelationshipBySecondaryClientId");
+    query.setParameter("secondaryClientId", secondaryClientId, StringType.INSTANCE);
+    return query.list().toArray(new ClientRelationship[0]);
+  }
 
-	public List<RelationshipWrapper> findRelationshipsByClientId(String clientId){
-		final Query<RelationshipWrapper> query = this.getSessionFactory()
-				.getCurrentSession()
-				.getNamedNativeQuery("gov.ca.cwds.data.persistence.cms.RelationshipWrapper.findAllRelatedClientsByClientId")
-				.addEntity(RelationshipWrapper.class)
-		        .setParameter("clientId", clientId, StringType.INSTANCE);
-		return query.list();
-	}
+  public List<RelationshipWrapper> findRelationshipsByClientId(String clientId) {
+    final NativeQuery<RelationshipWrapper> query = this.getSessionFactory().getCurrentSession()
+        .getNamedNativeQuery(
+            "gov.ca.cwds.data.persistence.cms.RelationshipWrapper.findAllRelatedClientsByClientId")
+        .addEntity(RelationshipWrapper.class)
+        .setParameter("clientId", clientId, StringType.INSTANCE);
+    return query.list();
+  }
+
 }
--- src/test/java/gov/ca/cwds/rest/util/jni/DB2PassUserTest.java
+++ src/test/java/gov/ca/cwds/rest/util/jni/DB2PassUserTest.java
@@ -15,44 +15,54 @@ public class DB2PassUserTest {
     try {
       Class.forName("com.ibm.db2.jcc.DB2Driver");
 
-      // DOCKER:
-      // final String url = "jdbc:db2://localhost:50000/DB0TDEV";
-
       // MAINFRAME:
-      final String url =
-          "jdbc:db2://localhost:9000/DB0TSOC:retrieveMessagesFromServerOnGetMessage=true;emulateParameterMetaDataForZCalls=1;";
+      // final String url =
+      // "jdbc:db2://localhost:9000/DB0TSOC:retrieveMessagesFromServerOnGetMessage=true;emulateParameterMetaDataForZCalls=1;";
+      final String url = System.getenv("DB_CMS_JDBC_URL");
       final String user = System.getenv("DB_CMS_USER");
       final String password = System.getenv("DB_CMS_PASSWORD");
+      final String schema = System.getenv("DB_CMS_PASSWORD");
 
-      Connection conn = DriverManager.getConnection(url, user, password);
+      try (final Connection conn = DriverManager.getConnection(url, user, password)) {
+        conn.setSchema(schema);
 
-      if (conn instanceof DB2Connection) {
-        DB2Connection db2conn = (DB2Connection) conn;
-        db2conn.setDB2ClientUser("0X5"); // staff id
-        // db2conn.setDB2ClientWorkstation("192.168.9.121");
-        db2conn.setDB2ClientWorkstation("127.0.0.1");
-        conn.setAutoCommit(true);
-        conn.prepareStatement(
-            "UPDATE CWSINT.TSCNTRLT C SET C.LST_UPD_ID = CURRENT CLIENT_USERID WHERE C.DOC_HANDLE = '0001121506110220*RAMESHA 00001'")
-            .executeUpdate();
+        if (conn instanceof DB2Connection) {
+          final String userId = "0X5";
+          final DB2Connection db2conn = (DB2Connection) conn;
 
-        // Execute SQL to force extended client information to be sent to the server.
-        // Auto-close the ResultSet upon block exit.
-        try (ResultSet rs = conn.prepareStatement(
-            "SELECT C.DOC_HANDLE, C.DOC_SEGS, C.CMPRS_PRG, C.DOC_NAME, C.DOC_DATE, C.DOC_TIME, C.DOC_LEN, C.LST_UPD_ID, C.LST_UPD_TS, C.DOC_AUTH, C.DOC_SERV "
-                + "FROM CWSINT.TSCNTRLT C "
-                + "WHERE C.DOC_HANDLE = '0001121506110220*RAMESHA 00001' "
-                + "ORDER BY C.DOC_HANDLE FOR READ ONLY WITH UR")
-            .executeQuery()) {
+          // db2conn.setDB2CurrentPackagePath(arg0);
+          // db2conn.setDB2ClientCorrelationToken(arg0);
 
-          while (rs.next()) {
-            final String staffId = rs.getString("LST_UPD_ID");
-            System.out.println("staffId = " + staffId);
+          db2conn.setClientInfo("ClientUser", userId);
+          db2conn.setDB2ClientUser(userId); // staff id
+          // db2conn.setDB2ClientWorkstation("192.168.9.121");
+          db2conn.setDB2ClientWorkstation("127.0.0.1");
+          conn.setAutoCommit(true);
+          conn.prepareStatement(
+              "UPDATE CWSINT.TSCNTRLT C SET C.LST_UPD_TS = CURRENT TIMESTAMP, C.LST_UPD_ID = CURRENT CLIENT_USERID WHERE C.DOC_HANDLE = '0001121506110220*RAMESHA 00001'")
+              .executeUpdate();
+
+          // Execute SQL to force extended client information to be sent to the server.
+          // Auto-close the ResultSet upon block exit.
+          try (ResultSet rs = conn.prepareStatement(
+              "SELECT C.DOC_HANDLE, C.DOC_SEGS, C.CMPRS_PRG, C.DOC_NAME, C.DOC_DATE, C.DOC_TIME, C.DOC_LEN, C.LST_UPD_ID, C.LST_UPD_TS, C.DOC_AUTH, C.DOC_SERV "
+                  + "FROM CWSINT.TSCNTRLT C "
+                  + "WHERE C.DOC_HANDLE = '0001121506110220*RAMESHA 00001' "
+                  + "ORDER BY C.DOC_HANDLE FOR READ ONLY WITH UR")
+              .executeQuery()) {
+
+            while (rs.next()) {
+              final String staffId = rs.getString("LST_UPD_ID");
+              System.out.println("staffId = " + staffId);
+            }
           }
         }
+
+      } finally {
+        // auto-close
       }
 
-    } catch (Throwable e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
