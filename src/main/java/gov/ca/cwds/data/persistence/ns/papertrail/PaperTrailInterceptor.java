457bc920b672b7fb23c59c54f5dc0c911d67cd3e
David R. Smith <David.Smith@osi.ca.gov>
2018-04-13 15:33:22 -0700

    Guice: more hidden dimensions than String Theory

--- src/main/java/gov/ca/cwds/data/persistence/ns/papertrail/PaperTrailInterceptor.java
+++ src/main/java/gov/ca/cwds/data/persistence/ns/papertrail/PaperTrailInterceptor.java
@@ -78,4 +78,9 @@ public class PaperTrailInterceptor extends EmptyInterceptor {
     this.paperTrailDao = paperTrailDao;
   }
 
+  @Override
+  public String toString() {
+    return "PaperTrailInterceptor [paperTrailDao=" + paperTrailDao + "]";
+  }
+
 }
