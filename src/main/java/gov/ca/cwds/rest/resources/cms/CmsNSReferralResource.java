a200a2893d84f8d90de3ce7eb5c22bcc34b0ab9c
David R. Smith <David.Smith@osi.ca.gov>
2018-04-17 15:30:50 -0700

    INT-1592: XA transactions

--- src/main/java/gov/ca/cwds/rest/resources/cms/CmsNSReferralResource.java
+++ src/main/java/gov/ca/cwds/rest/resources/cms/CmsNSReferralResource.java
@@ -98,7 +98,7 @@ public class CmsNSReferralResource {
    * 
    * @return The {@link Response}
    */
-  // @UnitOfWork(value = "cms")
+  // @UnitOfWork(value = "cms") // Uses XA
   @POST
   @ApiResponses(value = {@ApiResponse(code = 400, message = "Unable to process JSON"),
       @ApiResponse(code = 401, message = "Not Authorized"),
