d79eb7420aed3a944968eac90d35e9764160f41e
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-04-19 15:50:06 -0700

    Few Functional Tests on HOI referrals end point.

---
+++ src/test/java/gov/ca/cwds/config/TokenCredentialsTest.java
@@ -0,0 +1,43 @@
+package gov.ca.cwds.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public class TokenCredentialsTest {
+
+  private String baseUrl = "https://ferbapi.preint.cwds.io";
+  private String perryLoginUrl = "https://localhost:8090/perry/login";
+  private String authLoginUrl = "https://localhost:8090/perry/authn/login";
+  private String tokenUrl = "https://localhost:8090/perry/authn/token";
+  private String callBackUrl = "https://ferbapi.preint.cwds.io/swagger";
+  private String logOutUrl = "https://localhost:8090/perry/authn/logout";
+  private String validateUrl = "https://localhost:8090/perry/authn/validate";
+
+  /**
+   * Test for TokenCredentials class getters and setters.
+   */
+  @Test
+  public void testForSettersGetters() {
+    final TokenCredentials testUrl = new TokenCredentials();
+    testUrl.setAuthLoginUrl(authLoginUrl);
+    testUrl.setBaseUrl(baseUrl);
+    testUrl.setCallBackUrl(callBackUrl);
+    testUrl.setLogOutUrl(logOutUrl);
+    testUrl.setPerryLoginUrl(perryLoginUrl);
+    testUrl.setTokenUrl(tokenUrl);
+    testUrl.setValidateUrl(validateUrl);
+
+    assertEquals(authLoginUrl, testUrl.getAuthLoginUrl());
+    assertEquals(baseUrl, testUrl.getAuthLoginUrl());
+    assertEquals(callBackUrl, testUrl.getAuthLoginUrl());
+    assertEquals(perryLoginUrl, testUrl.getAuthLoginUrl());
+    assertEquals(tokenUrl, testUrl.getAuthLoginUrl());
+    assertEquals(validateUrl, testUrl.getAuthLoginUrl());
+  }
+
+}
