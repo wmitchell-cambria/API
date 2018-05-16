c62d8e404810a0aa30e1a8d892c88c183dadf193
Mariam Ghori <mariam.ghori@osi.ca.gov>
2018-05-16 15:23:29 -0700

    HOT-1689 Implement in Ferb API the Screening endpoint method (submit) to absorb Intake API

--- src/test/java/gov/ca/cwds/rest/services/submit/ScreeningSubmitServiceTest.java
+++ src/test/java/gov/ca/cwds/rest/services/submit/ScreeningSubmitServiceTest.java
@@ -3,7 +3,12 @@ package gov.ca.cwds.rest.services.submit;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Before;
@@ -17,8 +22,15 @@ import org.mockito.MockitoAnnotations;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import gov.ca.cwds.data.cms.TestSystemCodeCache;
 import gov.ca.cwds.data.ns.IntakeLovDao;
+import gov.ca.cwds.data.persistence.ns.IntakeLov;
 import gov.ca.cwds.rest.api.Response;
+import gov.ca.cwds.rest.api.domain.PostedStaffPerson;
+import gov.ca.cwds.rest.api.domain.Screening;
+import gov.ca.cwds.rest.api.domain.ScreeningToReferral;
+import gov.ca.cwds.rest.api.domain.cms.SystemCodeCache;
+import gov.ca.cwds.rest.filters.TestingRequestExecutionContext;
 import gov.ca.cwds.rest.services.ScreeningService;
 import gov.ca.cwds.rest.services.ScreeningToReferralService;
 import gov.ca.cwds.rest.services.StaffPersonService;
@@ -33,7 +45,7 @@ import io.dropwizard.jackson.Jackson;
 public class ScreeningSubmitServiceTest {
 
   static final ObjectMapper MAPPER = Jackson.newObjectMapper();
-
+  private Screening screening;
   @Rule
   public ExpectedException thrown = ExpectedException.none();
 
@@ -49,11 +61,76 @@ public class ScreeningSubmitServiceTest {
   @InjectMocks
   private ScreeningSubmitService screeningSubmitService = new ScreeningSubmitService();
 
+  /**
+   * Initialize system code cache
+   */
+  private TestSystemCodeCache testSystemCodeCache = new TestSystemCodeCache();
+
+
+  @SuppressWarnings("javadoc")
   @Before
   public void setup() throws Exception {
-    MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
-
     MockitoAnnotations.initMocks(this);
+    MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
+    new TestingRequestExecutionContext("0X5");
+    SystemCodeCache.global().getAllSystemCodes();
+
+    PostedStaffPerson staffPerson = mock(PostedStaffPerson.class);
+    when(staffPerson.getCountyCode()).thenReturn("99");
+    when(staffPersonService.find(any())).thenReturn(staffPerson);
+    IntakeLov intakeLovInPerson = mock(IntakeLov.class);
+    when(intakeLovInPerson.getIntakeCode()).thenReturn("in_person");
+    when(intakeLovInPerson.getLegacySystemCodeId()).thenReturn(new Long(408));
+    when(intakeLovInPerson.getLegacyLogicalCode()).thenReturn("34");
+
+    IntakeLov intakeLovResponseTime = mock(IntakeLov.class);
+    when(intakeLovResponseTime.getIntakeCode()).thenReturn("evaluate_out");
+    when(intakeLovResponseTime.getLegacySystemCodeId()).thenReturn(new Long(1519));
+    when(intakeLovInPerson.getLegacyLogicalCode()).thenReturn("34");
+
+    IntakeLov intakeLovCountyCode = mock(IntakeLov.class);
+    when(intakeLovCountyCode.getIntakeCode()).thenReturn("1101");
+    when(intakeLovCountyCode.getLegacySystemCodeId()).thenReturn(new Long(1101));
+    when(intakeLovCountyCode.getLegacyLogicalCode()).thenReturn("34");
+
+    IntakeLov intakeLovStateCa = mock(IntakeLov.class);
+    when(intakeLovStateCa.getIntakeCode()).thenReturn("CA");
+    when(intakeLovStateCa.getLegacySystemCodeId()).thenReturn(new Long(1828));
+
+    List<IntakeLov> intakeLovs = new ArrayList<>();
+    intakeLovs.add(intakeLovStateCa);
+    intakeLovs.add(intakeLovInPerson);
+    intakeLovs.add(intakeLovCountyCode);
+    intakeLovs.add(intakeLovResponseTime);
+
+    when(intakeLovDao.findAll()).thenReturn(intakeLovs);
+
+    ScreeningToReferral screeningToReferral = mock(ScreeningToReferral.class);
+    when(screeningToReferral.getReferralId()).thenReturn("ABC1234567");
+    when(screeningToReferralService.create(any())).thenReturn(screeningToReferral);
+
+    screening = new Screening();
+    screening.setId("1");
+    screening.setReferralId("");
+    screening.setEndedAt("2017-01-01");
+    screening.setIncidentCounty("34");
+    screening.setIncidentDate("2017-01-01");
+    screening.setLocationType("Foster Home");
+    screening.setCommunicationMethod("in_person");
+    screening.setCurrentLocationOfChildren(null);
+    screening.setName("The Rocky Horror Show");
+    screening.setReportNarrative("Narrative 123 test");
+    screening.setReference("123ABC");
+    screening.setRestrictionsRationale("");
+    screening.setStartedAt("2017-01-01");
+    screening.setAssignee("Michael Bastow");
+    screening.setAssigneeStaffId("0X5");
+    screening.setAdditionalInformation("additional information");
+    screening.setScreeningDecision("Screening Decision");
+    screening.setScreeningDecisionDetail("evaluate_out");
+    screening.setIncidentAddress(null);
+    screening.setParticipantIntakeApis(null);
+
   }
 
   // create test
@@ -83,6 +160,17 @@ public class ScreeningSubmitServiceTest {
     Response found = screeningSubmitService.find("000");
 
     assertThat(found, is(nullValue()));
+
+  }
+
+  @Test
+  public void testFindReturnsUpdatedScreening() throws Exception {
+
+    when(screenigService.getScreening("1")).thenReturn(screening);
+    when(screenigService.update(any(), any())).thenReturn(screening);
+
+    Response found = screeningSubmitService.find("1");
+    assertThat(found, is(screening));
 
   }
 
