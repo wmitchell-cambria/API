792dbd06014584c2b1ad518fe02cad40b16d9dd5
David R. Smith <David.Smith@osi.ca.gov>
2018-05-15 17:38:04 -0700

    SNAP-49: Production: no HOI shown for client

--- src/main/java/gov/ca/cwds/rest/api/domain/RequestContextAwareResponse.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/RequestContextAwareResponse.java
@@ -1,6 +1,7 @@
 package gov.ca.cwds.rest.api.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 import gov.ca.cwds.rest.api.Response;
 import gov.ca.cwds.rest.api.domain.error.ErrorMessage;
@@ -8,23 +9,35 @@ import gov.ca.cwds.rest.filters.RequestExecutionContext;
 
 /**
  * {@link Response} interface extension that is aware of the current request's
- * {@link RequestExecutionContext}.
+ * {@link RequestExecutionContext}, which is bound to the current thread.
  * 
  * @author CWDS API Team
  */
 public interface RequestContextAwareResponse extends Response {
 
+  /**
+   * Pull error/warning messages from the current thread's {@link RequestExecutionContext}, if
+   * available, or return null.
+   * 
+   * @return messages in current request context or null
+   */
+  static List<ErrorMessage> getRequestContextMessages() {
+    final Optional<RequestExecutionContext> ctx =
+        Optional.<RequestExecutionContext>ofNullable(RequestExecutionContext.instance());
+    final List<ErrorMessage> messages =
+        ctx.isPresent() ? ctx.get().getMessageBuilder().getMessages() : null;
+    return messages;
+  }
+
   @Override
   default boolean hasMessages() {
-    final RequestExecutionContext ctx = RequestExecutionContext.instance();
-    final List<ErrorMessage> messages = ctx.getMessageBuilder().getMessages();
+    final List<ErrorMessage> messages = getRequestContextMessages();
     return messages != null && !messages.isEmpty() ? true : Response.super.hasMessages();
   }
 
   @Override
   default List<ErrorMessage> getMessages() {
-    final RequestExecutionContext ctx = RequestExecutionContext.instance();
-    final List<ErrorMessage> messages = ctx.getMessageBuilder().getMessages();
+    final List<ErrorMessage> messages = getRequestContextMessages();
     return messages != null && !messages.isEmpty() ? messages : Response.super.getMessages();
   }
 
