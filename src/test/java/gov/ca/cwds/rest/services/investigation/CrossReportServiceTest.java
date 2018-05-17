d9004c53753a47ab58bcec39bf98b7f91c987ed1
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-05-17 11:52:22 -0700

    fixed several code smells for sonar

--- src/test/java/gov/ca/cwds/rest/services/investigation/CrossReportServiceTest.java
+++ src/test/java/gov/ca/cwds/rest/services/investigation/CrossReportServiceTest.java
@@ -3,7 +3,6 @@ package gov.ca.cwds.rest.services.investigation;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.hamcrest.junit.ExpectedException;
@@ -11,7 +10,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import gov.ca.cwds.data.dao.investigation.CrossReportDao;
 import gov.ca.cwds.fixture.investigation.CrossReportEntityBuilder;
 import gov.ca.cwds.rest.api.Response;
 import gov.ca.cwds.rest.api.domain.investigation.CrossReport;
@@ -24,7 +22,6 @@ public class CrossReportServiceTest {
   private CrossReport crossReportStub;
 
   private CrossReportService crossReportService;
-  private CrossReportDao crossReportDao;
 
   @Rule
   public ExpectedException thrown = ExpectedException.none();
@@ -32,8 +29,7 @@ public class CrossReportServiceTest {
   @Before
   public void setup() throws Exception {
     new TestingRequestExecutionContext("0X5");
-    this.crossReportDao = mock(CrossReportDao.class);
-    crossReportService = new CrossReportService(crossReportDao);
+    crossReportService = new CrossReportService();
     crossReportStub = new CrossReportEntityBuilder().build();
   }
 
