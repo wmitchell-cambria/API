e7471adfd1b119a983fc9bc125939a4c23bfa213
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-05-18 13:30:44 -0700

    added more Integration Tests

--- src/test/java/gov/ca/cwds/rest/resources/hoi/InvolvementHistoryResourceIRT.java
+++ src/test/java/gov/ca/cwds/rest/resources/hoi/InvolvementHistoryResourceIRT.java
@@ -2,8 +2,11 @@ package gov.ca.cwds.rest.resources.hoi;
 
 import static gov.ca.cwds.rest.core.Api.RESOURCE_SCREENINGS;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import gov.ca.cwds.rest.api.domain.hoi.InvolvementHistory;
+import gov.ca.cwds.rest.services.hoi.InvolvementHistoryService;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +14,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -37,5 +41,33 @@ public class InvolvementHistoryResourceIRT extends HOIBaseTest {
         .target(RESOURCE_SCREENINGS + "/" + id + "/history_of_involvements");
     Response response = target.request(MediaType.APPLICATION_JSON).get();
     return IOUtils.toString((InputStream) response.getEntity(), StandardCharsets.UTF_8);
+  }
+
+  @Test
+  public void testInvolvementHistoryWhenNoSuchScreening() throws IOException {
+    final String actualJson = doGet("-1");
+    final InvolvementHistory actualInvolvementHistory = getInvolvementHistory(actualJson);
+    assertEquals("-1", actualInvolvementHistory.getId());
+    assertNotNull(actualInvolvementHistory.getCases());
+    assertEquals(0, actualInvolvementHistory.getCases().size());
+    assertNotNull(actualInvolvementHistory.getReferrals());
+    assertEquals(0, actualInvolvementHistory.getReferrals().size());
+    assertNotNull(actualInvolvementHistory.getScreenings());
+    assertEquals(0, actualInvolvementHistory.getScreenings().size());
+  }
+
+  @Test(expected = NotImplementedException.class)
+  public void testDeleteNotImplemented() {
+    new InvolvementHistoryService().delete("1");
+  }
+
+  @Test(expected = NotImplementedException.class)
+  public void testUpdateNotImplemented() {
+    new InvolvementHistoryService().update("1", null);
+  }
+
+  @Test(expected = NotImplementedException.class)
+  public void testCreateNotImplemented() {
+    new InvolvementHistoryService().create(null);
   }
 }
