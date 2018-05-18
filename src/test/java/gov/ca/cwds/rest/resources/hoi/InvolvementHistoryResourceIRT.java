8a1c861213b3969cb8da4c6d18aeac677e9f55ce
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-05-18 13:13:32 -0700

    added more Integration Tests

--- src/test/java/gov/ca/cwds/rest/resources/hoi/InvolvementHistoryResourceIRT.java
+++ src/test/java/gov/ca/cwds/rest/resources/hoi/InvolvementHistoryResourceIRT.java
@@ -2,18 +2,11 @@ package gov.ca.cwds.rest.resources.hoi;
 
 import static gov.ca.cwds.rest.core.Api.RESOURCE_SCREENINGS;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
-import static org.junit.Assert.assertEquals;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gov.ca.cwds.IntakeBaseTest;
-import gov.ca.cwds.rest.api.domain.hoi.HOICase;
-import gov.ca.cwds.rest.api.domain.hoi.HOIReferral;
 import gov.ca.cwds.rest.api.domain.hoi.InvolvementHistory;
-import io.dropwizard.jackson.Jackson;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,20 +15,21 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
-public class InvolvementHistoryResourceIRT extends IntakeBaseTest {
-
-  static final String VALID_HOI_JSON = "fixtures/gov/ca/cwds/rest/resources/hoi/history-of-involvements.valid.json";
+public class InvolvementHistoryResourceIRT extends HOIBaseTest {
 
   @Test
   public void testGet() throws Exception {
-    String actualJson = doGet("714");
-    String expectedResponse = fixture(VALID_HOI_JSON);
+    final String expectedResponse = fixture(VALID_HOI_JSON);
+    final String actualJson = doGet("714");
     JSONAssert.assertEquals(expectedResponse, actualJson, JSONCompareMode.NON_EXTENSIBLE);
 
-    InvolvementHistory actualInvolvementHistory = objectMapper
-        .readValue(actualJson.getBytes(), InvolvementHistory.class);
-    assertHOICasesAreSorted(actualInvolvementHistory);
-    assertHOIReferralsAreSorted(actualInvolvementHistory);
+    final InvolvementHistory actualInvolvementHistory = getInvolvementHistory(actualJson);
+    assertHOICasesAreSorted(new String[]{"Co8uaDi0DW", "IdQImWo0DW"},
+        actualInvolvementHistory.getCases());
+    assertHOIReferralsAreSorted(new String[]{"MYsSPHW0DW", "9OQhOAE0DW"},
+        actualInvolvementHistory.getReferrals());
+    assertHOIScreeningsAreSorted(new String[]{"750", "885", "862"},
+        actualInvolvementHistory.getScreenings());
   }
 
   private String doGet(String id) throws IOException {
@@ -43,17 +37,5 @@ public class InvolvementHistoryResourceIRT extends IntakeBaseTest {
         .target(RESOURCE_SCREENINGS + "/" + id + "/history_of_involvements");
     Response response = target.request(MediaType.APPLICATION_JSON).get();
     return IOUtils.toString((InputStream) response.getEntity(), StandardCharsets.UTF_8);
-  }
-
-  private void assertHOICasesAreSorted(InvolvementHistory actualInvolvementHistory) {
-    List<HOICase> hoiCases = actualInvolvementHistory.getCases();
-    assertEquals("Co8uaDi0DW", hoiCases.get(0).getId());
-    assertEquals("IdQImWo0DW", hoiCases.get(1).getId());
-  }
-
-  private void assertHOIReferralsAreSorted(InvolvementHistory actualInvolvementHistory) {
-    List<HOIReferral> hoiReferrals = actualInvolvementHistory.getReferrals();
-    assertEquals("MYsSPHW0DW", hoiReferrals.get(0).getId());
-    assertEquals("9OQhOAE0DW", hoiReferrals.get(1).getId());
   }
 }
