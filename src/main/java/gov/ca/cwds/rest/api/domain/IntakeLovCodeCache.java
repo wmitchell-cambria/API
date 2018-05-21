bcc4ed9dbff73302ae3c2516b02c6dc5bf72aab8
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-05-21 10:44:59 -0700

    HOT-1973 Added the cache class for intake code.

---
+++ src/main/java/gov/ca/cwds/rest/api/domain/IntakeLovCodeCache.java
@@ -0,0 +1,44 @@
+package gov.ca.cwds.rest.api.domain;
+
+import java.util.Map;
+
+import gov.ca.cwds.data.persistence.cms.DeferredRegistry;
+import gov.ca.cwds.data.persistence.ns.IntakeLov;
+import gov.ca.cwds.data.std.ApiMarker;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public interface IntakeLovCodeCache extends ApiMarker {
+
+  /**
+   * Register this intakes Code cache instance for system-wide use.
+   */
+  default void register() {
+    DeferredRegistry.<IntakeLovCodeCache>register(IntakeLovCodeCache.class, this);
+  }
+
+  /**
+   * Globally available, singleton intakes code cache.
+   * 
+   * @return singleton intakes code cache
+   */
+  static IntakeLovCodeCache global() {
+    return DeferredRegistry.<IntakeLovCodeCache>unwrap(IntakeLovCodeCache.class);
+  }
+
+  /**
+   * @param metaId - metaId
+   * @return All legacy system codes for the metaId.
+   */
+  Map<String, IntakeLov> getAllLegacySystemCodesForMeta(String metaId);
+
+  /**
+   * @param metaId
+   * @param intakeCode
+   * @return
+   */
+  IntakeLov getLegacySystemCodeForIntakeCode(String metaId, String intakeCode);
+
+}
---
+++ src/main/java/gov/ca/cwds/rest/services/CachingIntakeLovService.java
@@ -0,0 +1,196 @@
+package gov.ca.cwds.rest.services;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.inject.Inject;
+
+import gov.ca.cwds.data.ns.IntakeLovDao;
+import gov.ca.cwds.data.persistence.ns.IntakeLov;
+import gov.ca.cwds.data.std.ApiObjectIdentity;
+import gov.ca.cwds.rest.api.domain.IntakeLovCodeCache;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public class CachingIntakeLovService extends IntakeLovService implements IntakeLovCodeCache {
+
+  private transient LoadingCache<CacheKey, Object> intakeLovCodeCache;
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Default no-arg constructor.
+   */
+  @SuppressWarnings("unused")
+  private CachingIntakeLovService() {
+    // no-opt
+  }
+
+  /**
+   * @param intakeLovDao
+   * @param secondsToRefreshCache
+   * @param preloadCache
+   */
+  @Inject
+  public CachingIntakeLovService(IntakeLovDao intakeLovDao, long secondsToRefreshCache,
+      boolean preloadCache) {
+    super(intakeLovDao);
+
+    final IntakeCodeCacheLoader cacheLoader = new IntakeCodeCacheLoader(this);
+    intakeLovCodeCache = CacheBuilder.newBuilder()
+        .refreshAfterWrite(secondsToRefreshCache, TimeUnit.SECONDS).build(cacheLoader);
+
+    if (preloadCache) {
+      try {
+        Map<CacheKey, Object> intakeCode = cacheLoader.loadAll();
+        intakeLovCodeCache.putAll(intakeCode);
+      } catch (Exception e) {
+        LOGGER.error("Error loading intake codes", e);
+        throw new ServiceException(e);
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public Map<String, IntakeLov> getAllLegacySystemCodesForMeta(String metaId) {
+    Map<String, IntakeLov> intakeLov = new HashMap<>();
+    if (!StringUtils.isBlank(metaId)) {
+      CacheKey cacheKey = CacheKey.createForMeta(metaId);
+      return (Map<String, IntakeLov>) getFromCache(cacheKey);
+    }
+    return intakeLov;
+  }
+
+  @Override
+  public IntakeLov getLegacySystemCodeForIntakeCode(String metaId, String intakeCode) {
+
+    if (StringUtils.isNotBlank(metaId)) {
+      CacheKey cacheKey = CacheKey.createForMeta(metaId);
+      @SuppressWarnings("unchecked")
+      Map<String, IntakeLov> intakeLovMap = (Map<String, IntakeLov>) getFromCache(cacheKey);
+      if (intakeLovMap != null) {
+        return intakeLovMap.get(intakeCode);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Get cached object identified by given cache key.
+   * 
+   * @param cacheEntryKey Cache key
+   * @return Cached object if found, otherwise null.
+   */
+  private Object getFromCache(CacheKey cacheEntryKey) {
+    Object obj = null;
+    try {
+      obj = intakeLovCodeCache.get(cacheEntryKey);
+    } catch (Exception e) {
+      LOGGER.warn("getFromCache -> Unable to load object for key: " + cacheEntryKey, e);
+    }
+
+    return obj;
+  }
+
+  private static class IntakeCodeCacheLoader extends CacheLoader<CacheKey, Object> {
+
+    private IntakeLovService intakeLovService;
+
+    /**
+     * Construct the object
+     * 
+     * @param intakeLovService
+     */
+    IntakeCodeCacheLoader(IntakeLovService intakeLovService) {
+      this.intakeLovService = intakeLovService;
+    }
+
+    /**
+     * Loads all system code cache entries.
+     * 
+     * @return All system code cache entries.
+     * @throws Exception on disconnect, NPE, etc.
+     */
+    public Map<CacheKey, Object> loadAll() {
+      LOGGER.info("Loading all system code cache...");
+
+      Map<CacheKey, Object> intakeCodeMap = new HashMap<>();
+      Map<String, Map<String, IntakeLov>> intakeLovMap = new HashMap<>();
+      List<IntakeLov> intakeLovList = intakeLovService.findAllIntakeLov();
+      if (intakeLovList != null) {
+        for (IntakeLov intakeLov : intakeLovList) {
+          if (intakeLovMap.containsKey(intakeLov.getLegacyCategoryId())) {
+            Map<String, IntakeLov> codeMap = intakeLovMap.get(intakeLov.getLegacyCategoryId());
+            codeMap.put(intakeLov.getIntakeCode(), intakeLov);
+            intakeLovMap.put(intakeLov.getLegacyCategoryId(), codeMap);
+          } else {
+            Map<String, IntakeLov> codeMap = new HashMap<>();
+            codeMap.put(intakeLov.getIntakeCode(), intakeLov);
+            intakeLovMap.put(intakeLov.getLegacyCategoryId(), codeMap);
+          }
+        }
+      }
+
+      for (Entry<String, Map<String, IntakeLov>> entry : intakeLovMap.entrySet()) {
+        CacheKey cacheKey = CacheKey.createForMeta(entry.getKey());
+        intakeCodeMap.put(cacheKey, entry.getValue());
+      }
+      return intakeCodeMap;
+    }
+
+    @Override
+    public Object load(CacheKey key) throws Exception {
+      return null;
+    }
+
+  }
+
+  /**
+   * =============================================================================== <br>
+   * Cache key. <br>
+   * ===============================================================================
+   */
+  private static class CacheKey extends ApiObjectIdentity {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String META_ID_TYPE = "META_ID";
+
+    private Serializable value;
+    private String type;
+
+    private CacheKey(String type, Serializable value) {
+      this.type = type;
+      this.value = value;
+    }
+
+    public Serializable getValue() {
+      return value;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    private static CacheKey createForMeta(Serializable value) {
+      return new CacheKey(META_ID_TYPE, value);
+    }
+
+  }
+
+}
