96232659d72ade5b5561d7168d4493872eb71b52
Oleksandr Kuznetsov <Alex.Kuznetsov@osi.ca.gov>
2018-05-22 10:40:08 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -x /paramfolder/parameters.sh ]; then
+    source /paramfolder/parameters.sh
+fi
+
 CWDS_OPTS="-Xms128m -Xmx256m ${CWDS_OPTS}"
 
 java ${CWDS_OPTS} -jar api.jar server api.yml
